8730fea9719d21b2682b0289312e19cc505a7a43
Carlos Lopez <clshortfuse@gmail.com>
2024-03-14 16:46:38 -0400

    feat(batmanak): add color grade and presets

--- src/batman-arkhamknight/addon.cpp
+++ src/batman-arkhamknight/addon.cpp
@@ -8,15 +8,16 @@
 
 #define DEBUG_LEVEL_0
 
+#include <embed/0x3A4E0B90.h>
 #include <embed/0x978BFB09.h>
 #include <embed/0xB6B56605.h>
 #include <embed/0xF01CCC7E.h>
-#include <embed/0x3A4E0B90.h>
 
 #include "./shared.h"
 
 #include "../../external/reshade/deps/imgui/imgui.h"
 #include "../../external/reshade/include/reshade.hpp"
+#include "../common/UserSettingUtil.hpp"
 #include "../common/shaderReplaceMod.hpp"
 #include "../common/swapChainUpgradeMod.hpp"
 
@@ -32,76 +33,103 @@ ShaderReplaceMod::CustomShaders customShaders = {
 
 ShaderInjectData shaderInjection;
 
-struct {
-  int toneMapperEnum = 2u;
-  float gamePeakWhite = 1000.f;
-  float gamePaperWhite = 203.f;
-  float uiPaperWhite = 203.f;
-} userSettings;
-
-static void updateInjection() {
-  shaderInjection.toneMapperEnum = static_cast<float>(userSettings.toneMapperEnum);
-  shaderInjection.gamePeakWhite = userSettings.gamePeakWhite;
-  shaderInjection.gamePaperWhite = userSettings.gamePaperWhite;
-  shaderInjection.uiPaperWhite = userSettings.uiPaperWhite;
-}
-
-static void on_register_overlay(reshade::api::effect_runtime* runtime) {
-  bool settingsChanged = false;
-
-  static const char* toneMapperEnums[] = {
-    "Vanilla",
-    "None",
-    "ACES",
-    "OpenDRT"
-  };
-  settingsChanged |= ImGui::SliderInt(
-    "Tone Mapper",
-    &userSettings.toneMapperEnum,
-    0,
-    3,
-    toneMapperEnums[userSettings.toneMapperEnum],
-    ImGuiSliderFlags_NoInput
-  );
-
-  settingsChanged |= ImGui::SliderFloat(
-    "Peak Brightness",
-    &userSettings.gamePeakWhite,
-    48.f,
-    4000.f,
-    "%.0f"
-  );
-  ImGui::SetItemTooltip("Adjusts the peak brightness in nits.");
-
-  settingsChanged |= ImGui::SliderFloat(
-    "Game Brightness",
-    &userSettings.gamePaperWhite,
-    48.f,
-    500.f,
-    "%.0f"
-  );
-  ImGui::SetItemTooltip("Adjusts the brightness of 100%% white in nits.");
-
-  settingsChanged |= ImGui::SliderFloat(
-    "UI Brightness",
-    &userSettings.uiPaperWhite,
-    48.f,
-    500.f,
-    "%.0f"
-  );
-  ImGui::SetItemTooltip("Adjusts the peak brightness of the UI in nits.");
-
-  if (settingsChanged) {
-    updateInjection();
+// clang-format off
+UserSettingUtil::UserSettings userSettings = {
+  new UserSettingUtil::UserSetting {
+    .key = "toneMapType",
+    .binding = &shaderInjection.toneMapType,
+    .valueType = UserSettingUtil::UserSettingValueType::integer,
+    .defaultValue = 2.f,
+    .label = "Tone Mapper",
+    .section = "Tone Mapping",
+    .tooltip = "Sets the tone mapper type",
+    .labels = {"Vanilla", "None", "ACES", "OpenDRT"}
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "toneMapPeakNits",
+    .binding = &shaderInjection.toneMapPeakNits,
+    .defaultValue = 1000.f,
+    .label = "Peak Brightness",
+    .section = "Tone Mapping",
+    .tooltip = "Sets the value of peak white in nits",
+    .min = 48.f,
+    .max = 4000.f
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "toneMapGameNits",
+    .binding = &shaderInjection.toneMapGameNits,
+    .defaultValue = 203.f,
+    .label = "Game Brightness",
+    .section = "Tone Mapping",
+    .tooltip = "Sets the value of 100%% white in nits",
+    .min = 48.f,
+    .max = 500.f
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "toneMapUINits",
+    .binding = &shaderInjection.toneMapUINits,
+    .defaultValue = 203.f,
+    .label = "UI Brightness",
+    .section = "Tone Mapping",
+    .tooltip = "Sets the brightness of UI and HUD elements in nits",
+    .min = 48.f,
+    .max = 500.f
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "colorGradeHighlights",
+    .binding = &shaderInjection.colorGradeHighlights,
+    .defaultValue = 50.f,
+    .label = "Highlights",
+    .section = "Color Grading",
+    .max = 100.f,
+    .parse = [](float value) { return value * 0.02f; }
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "colorGradeShadows",
+    .binding = &shaderInjection.colorGradeShadows,
+    .defaultValue = 50.f,
+    .label = "Shadows",
+    .section = "Color Grading",
+    .max = 100.f,
+    .parse = [](float value) { return value * 0.02f; }
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "colorGradeContrast",
+    .binding = &shaderInjection.colorGradeContrast,
+    .defaultValue = 50.f,
+    .label = "Contrast",
+    .section = "Color Grading",
+    .max = 100.f,
+    .parse = [](float value) { return value * 0.02f; }
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "colorGradeSaturation",
+    .binding = &shaderInjection.colorGradeSaturation,
+    .defaultValue = 50.f,
+    .label = "Saturation",
+    .section = "Color Grading",
+    .max = 100.f,
+    .parse = [](float value) { return value * 0.02f; }
   }
+};
+
+// clang-format on
+
+static void onPresetOff() {
+  UserSettingUtil::updateUserSetting("toneMapType", 0.f);
+  UserSettingUtil::updateUserSetting("toneMapPeakNits", 203.f);
+  UserSettingUtil::updateUserSetting("toneMapGameNits", 203.f);
+  UserSettingUtil::updateUserSetting("toneMapUINits", 203.f);
+  UserSettingUtil::updateUserSetting("colorGradeHighlights", 50.f);
+  UserSettingUtil::updateUserSetting("colorGradeShadows", 50.f);
+  UserSettingUtil::updateUserSetting("colorGradeContrast", 50.f);
+  UserSettingUtil::updateUserSetting("colorGradeSaturation", 50.f);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID) {
   switch (fdwReason) {
     case DLL_PROCESS_ATTACH:
       if (!reshade::register_addon(hModule)) return FALSE;
-      updateInjection();
-      reshade::register_overlay("RenoDX", on_register_overlay);
 
       SwapChainUpgradeMod::swapChainUpgradeTargets.push_back(
         {reshade::api::format::r8g8b8a8_unorm, reshade::api::format::r16g16b16a16_float, 3}
@@ -109,10 +137,11 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID) {
 
       break;
     case DLL_PROCESS_DETACH:
-      reshade::unregister_overlay("RenoDX", on_register_overlay);
       reshade::unregister_addon(hModule);
       break;
   }
+
+  UserSettingUtil::use(fdwReason, &userSettings, &onPresetOff);
 
   SwapChainUpgradeMod::use(fdwReason);
 
--- src/batman-arkhamknight/tonemapper.hlsl
+++ src/batman-arkhamknight/tonemapper.hlsl
@@ -11,34 +11,60 @@ float3 applyUserToneMap(float3 inputColor, float3 untonemapped) {
   float3 outputColor = saturate(inputColor);
 
   const float uncharted2Scaler = 1.66289866f;
-  if (injectedData.toneMapperEnum == 0) {
+
+  if (injectedData.toneMapType == 0) {
     // noop
   } else {
-    if (injectedData.toneMapperEnum == 1) {
+    if (injectedData.toneMapType == 1.f) {
       outputColor = untonemapped;  // Untonemapped
     } else {
       // OutputColor was intended for 2.2 displays, using 2.2 numbers
       outputColor = pow(outputColor, 2.2f);  // Now in linear
+
       float inputY = yFromBT709(abs(untonemapped));
       float outputY = yFromBT709(outputColor);
       outputColor *= (outputY ? inputY / outputY : 1);
-      if (injectedData.toneMapperEnum == 2) {
+
+      if (injectedData.colorGradeSaturation != 1.f) {
+        float grayscale = yFromBT709(outputColor);
+        outputColor = lerp(grayscale, outputColor, injectedData.colorGradeSaturation);
+        outputColor = max(0, outputColor);
+      }
+
+      if (injectedData.toneMapType == 2) {
+        outputColor = apply_user_shadows(outputColor, injectedData.colorGradeShadows);
+        if (injectedData.colorGradeHighlights != 1.f) {
+          outputColor = apply_user_highlights(outputColor, injectedData.colorGradeHighlights);
+        }
+        if (injectedData.colorGradeContrast != 1.f) {
+          float3 workingColor = pow(outputColor / 0.18f, injectedData.colorGradeContrast) * 0.18f;
+          // Working in BT709 still
+          float workingColorY = yFromBT709(workingColor);
+          float outputColorY = yFromBT709(outputColor);
+          outputColor *= outputColorY ? workingColorY / outputColorY : 1.f;
+        }
+
         outputColor = aces_rrt_odt(
           outputColor * uncharted2Scaler,
           0.0001f,  // minY
-          48.f * (injectedData.gamePeakWhite / injectedData.gamePaperWhite),
+          48.f * (injectedData.toneMapPeakNits / injectedData.toneMapGameNits),
           AP1_2_BT2020_MAT
         );
       } else {
         outputColor = mul(BT709_2_BT2020_MAT, outputColor);
+        outputColor = max(0, outputColor);
         outputColor = open_drt_transform_custom(
-          outputColor * uncharted2Scaler,
-          100.f * (injectedData.gamePeakWhite / injectedData.gamePaperWhite),
+          outputColor,
+          100.f * (injectedData.toneMapPeakNits / injectedData.toneMapGameNits),
+          0,
+          injectedData.colorGradeHighlights,
+          injectedData.colorGradeShadows,
+          injectedData.colorGradeContrast,
           0
         );
       }
       outputColor = mul(BT2020_2_BT709_MAT, outputColor);
-      outputColor *= injectedData.gamePeakWhite / injectedData.uiPaperWhite;
+      outputColor *= injectedData.toneMapPeakNits / injectedData.toneMapUINits;
     }
     // Send as 2.2 numbers
     outputColor = sign(outputColor) * pow(abs(outputColor), 1.f / 2.2f);
