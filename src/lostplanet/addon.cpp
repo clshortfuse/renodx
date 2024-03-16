78517f137df6bff0d8e9e0c0acfccef638b980ff
Carlos Lopez <clshortfuse@gmail.com>
2024-03-16 11:45:59 -0400

    feat(lostplanet): add aspect correction shader

--- src/lostplanet/addon.cpp
+++ src/lostplanet/addon.cpp
@@ -12,6 +12,7 @@
 #include <embed/0x98F668B5.h>
 #include <embed/0xB2AD4F48.h>
 #include <embed/0xC1C7045A.h>
+#include <embed/0x7952DC10.h>
 
 #include "./shared.h"
 
@@ -25,6 +26,7 @@ extern "C" __declspec(dllexport) const char* NAME = "RenoDX - Lost Planet";
 extern "C" __declspec(dllexport) const char* DESCRIPTION = "RenoDX for Lost Planet";
 
 ShaderReplaceMod::CustomShaders customShaders = {
+  CustomShaderEntry(0x7952DC10),  // with aspect correction
   CustomShaderEntry(0xB2AD4F48),  // fullscreen
   CustomShaderEntry(0x7EC02107),  // output
   CustomShaderEntry(0xC1C7045A),  // matrix
@@ -140,7 +142,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID) {
       break;
   }
 
-  // Upgrade backbuffer
   UserSettingUtil::use(fdwReason, &userSettings, &onPresetOff);
 
   SwapChainUpgradeMod::use(fdwReason);
