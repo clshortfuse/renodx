fe765ac113eaec595db80cdab27f028a7a9448b5
Carlos Lopez <clshortfuse@gmail.com>
2024-03-18 17:18:47 -0400

    refactor(colorgrade): add color grade tools

--- src/common/Open_DRT.hlsl
+++ src/common/Open_DRT.hlsl
@@ -76,41 +76,6 @@ float powerptoe_fixed(float x) {
   return ((x - 1.f) * pow(pow((1.f - x) / (1.05f), 1.0f / 0.05f) + 1.0f, -0.05f) + 1.f);
 }
 
-/* Shadow Contrast
-    Invertible cubic shadow exposure function
-    https://www.desmos.com/calculator/ubgteikoke
-    https://colab.research.google.com/drive/1JT_-S96RZyfHPkZ620QUPIRfxmS_rKlx
-*/
-float3 shd_con(float3 rgb, float ex, float str) {
-  // Parameter setup
-  const float m = exp2(ex);
-  // Perf: explicit cube
-  // const float w = pow(str, 3.0f);
-  const float w = str * str * str;
-
-  const float n = max(rgb.x, max(rgb.y, rgb.z));
-  const float n2 = n * n;
-  const float dividend = n2 + w;
-  const float s = dividend ? (n2 + m * w) / dividend : 1.f;  // Implicit divide by n
-  return rgb * s;
-}
-
-/* Highlight Contrast
-    Invertible quadratic highlight contrast function. Same as ex_high without lin ext
-    https://www.desmos.com/calculator/p7j4udnwkm
-*/
-float3 hl_con(float3 rgb, float ex, float th) {
-  // Parameter setup
-  const float p = exp2(-ex);
-  const float t0 = 0.18f * exp2(th);
-  const float a = pow(t0, 1.0f - p) / p;
-  const float b = t0 * (1.0f - 1.0f / p);
-
-  const float n = max(rgb.x, max(rgb.y, rgb.z));
-  if (n == 0.0f || n < t0) return rgb;
-  return rgb * (pow((n - b) / a, 1.0f / p) / n);
-}
-
 float3 ex_high(float3 rgb, float ex, float pv, float fa) {
   // Zoned highlight exposure with falloff : https://www.desmos.com/calculator/ylq5yvkhoq
 
@@ -137,20 +102,6 @@ float3 apply_aces_highlights(float3 rgb) {
   rgb = ex_high(rgb, 0.5f, -3.0f, 0.5f);
   rgb = ex_high(rgb, 0.924f, -1.0f, 0.5f);
   rgb = ex_high(rgb, -0.15f, 2.68f, 0.19f);
-  return rgb;
-}
-
-float3 apply_user_shadows(float3 rgb, float shadows = 1.f) {
-  // Perf: explicit cube
-  // rgb = shd_con(rgb, -1.8f, pow(2.f - shadows, 3) * 0.04); // 0.04 @ 1
-  rgb = shd_con(rgb, -1.8f, pow(2.f - shadows, 4.f) * 0.025);     // 0.04 @ 1
-  rgb = shd_con(rgb, -0.50f * shadows * (1.f - shadows), 0.25f);  // 0 @ 1
-
-  return rgb;
-}
-
-float3 apply_user_highlights(float3 rgb, float highlights = 1.f) {
-  rgb = hl_con(rgb, (highlights - 1.f) * 4.f, 2.f);
   return rgb;
 }
 
@@ -383,7 +334,6 @@ float3 open_drt_transform(
   float ccf = 1.0f - overallDechroma * (pow(ts, dechromaDelay / dch) * (1.0f - ts * dechromaStrength) + ts * ts * dechromaBias);
   // float ccf = 1.0f - (powf(ts, 1.0f/dch)*(1.0f-ts) + ts*ts);
 
-
   // Apply chroma compression to RGB Ratios
   rats = rats * ccf + 1.0f - ccf;
 
@@ -447,29 +397,6 @@ float3 open_drt_transform(
   // Despite being 0-1, output range does not mean SDR range.
   // Output is still scene-linear, only compressed to output range.
   rgb = saturate(rgb);
-
-  return rgb;
-}
-
-float3 open_drt_transform_custom(
-  float3 rgb,
-  float peakNits = 203.f,
-  float midGrayAdjustment = 1.f,
-  float highlights = 1.f,
-  float shadows = 1.f,
-  float contrast = 1.f,
-  float dechroma = 1.f
-) {
-  rgb = apply_aces_highlights(rgb);
-  rgb = apply_user_shadows(rgb, shadows);
-  rgb = apply_user_highlights(rgb, highlights);
-  rgb = open_drt_transform(
-    rgb,
-    peakNits,
-    midGrayAdjustment,
-    contrast,
-    dechroma
-  );
 
   return rgb;
 }
