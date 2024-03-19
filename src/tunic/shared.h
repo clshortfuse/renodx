87650dad0049fcd5eea24636a05b5024cbe46dd3
Carlos Lopez <clshortfuse@gmail.com>
2024-03-19 14:42:42 -0400

    fix(tunic): add missing file

---
+++ src/tunic/shared.h
@@ -0,0 +1,26 @@
+#ifndef SRC_TUNIC_SHARED_H_
+#define SRC_TUNIC_SHARED_H_
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float colorGradeLUTStrength;
+  float fxScreenGlow;
+  float processingLUTCorrection;
+};
+
+#ifndef __cplusplus
+cbuffer cb13 : register(b13) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_TUNIC_SHARED_H_
