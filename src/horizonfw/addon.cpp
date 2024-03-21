9eebe160c305355d5ff0caa84fa8030dc66b8ee5
Carlos Lopez <clshortfuse@gmail.com>
2024-03-21 18:03:47 -0400

    feat(horizonfw): first commit

---
+++ src/horizonfw/addon.cpp
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2023 Carlos Lopez
+ * SPDX-License-Identifier: MIT
+ */
+
+#define IMGUI_DISABLE_INCLUDE_IMCONFIG_H
+#define ImTextureID ImU64
+
+#define DEBUG_LEVEL_0
+
+#include <embed/0x81FF83CE.h>
+
+#include "../../external/reshade/deps/imgui/imgui.h"
+#include "../../external/reshade/include/reshade.hpp"
+#include "../common/shaderReplaceMod.hpp"
+#include "../common/userSettingUtil.hpp"
+#include "./shared.h"
+
+extern "C" __declspec(dllexport) const char* NAME = "RenoDX - Horizon Forbidden West";
+extern "C" __declspec(dllexport) const char* DESCRIPTION = "RenoDX for Horizon Forbidden West";
+
+ShaderReplaceMod::CustomShaders customShaders = {
+  CustomShaderEntry(0x81FF83CE)
+  // CustomShaderEntry(0xE3DF9B3A)
+};
+
+ShaderInjectData shaderInjection;
+
+// clang-format off
+UserSettingUtil::UserSettings userSettings = {
+  new UserSettingUtil::UserSetting {
+    .key = "toneMapType",
+    .binding = &shaderInjection.toneMapType,
+    .valueType = UserSettingUtil::UserSettingValueType::integer,
+    .defaultValue = 2.f,
+    .label = "Tone Mapper",
+    .section = "Tone Mapping",
+    .tooltip = "Sets the tone mapper type",
+    .labels = {"Vanilla", "None", "ACES", "OpenDRT"}
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "toneMapPeakNits",
+    .binding = &shaderInjection.toneMapPeakNits,
+    .defaultValue = 1000.f,
+    .label = "Peak Brightness",
+    .section = "Tone Mapping",
+    .tooltip = "Sets the value of peak white in nits",
+    .min = 48.f,
+    .max = 4000.f
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "toneMapGameNits",
+    .binding = &shaderInjection.toneMapGameNits,
+    .defaultValue = 203.f,
+    .label = "Game Brightness",
+    .section = "Tone Mapping",
+    .tooltip = "Sets the value of 100%% white in nits",
+    .min = 48.f,
+    .max = 500.f
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "toneMapUINits",
+    .binding = &shaderInjection.toneMapUINits,
+    .defaultValue = 203.f,
+    .label = "UI Brightness",
+    .section = "Tone Mapping",
+    .tooltip = "Sets the brightness of UI and HUD elements in nits",
+    .min = 48.f,
+    .max = 500.f
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "colorGradeHighlights",
+    .binding = &shaderInjection.colorGradeHighlights,
+    .defaultValue = 50.f,
+    .label = "Highlights",
+    .section = "Color Grading",
+    .max = 100.f,
+    .parse = [](float value) { return value * 0.02f; }
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "colorGradeShadows",
+    .binding = &shaderInjection.colorGradeShadows,
+    .defaultValue = 50.f,
+    .label = "Shadows",
+    .section = "Color Grading",
+    .max = 100.f,
+    .parse = [](float value) { return value * 0.02f; }
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "colorGradeContrast",
+    .binding = &shaderInjection.colorGradeContrast,
+    .defaultValue = 50.f,
+    .label = "Contrast",
+    .section = "Color Grading",
+    .max = 100.f,
+    .parse = [](float value) { return value * 0.02f; }
+  },
+  new UserSettingUtil::UserSetting {
+    .key = "colorGradeSaturation",
+    .binding = &shaderInjection.colorGradeSaturation,
+    .defaultValue = 50.f,
+    .label = "Saturation",
+    .section = "Color Grading",
+    .max = 100.f,
+    .parse = [](float value) { return value * 0.02f; }
+  }
+};
+
+// clang-format on
+
+static void onPresetOff() {
+  UserSettingUtil::updateUserSetting("toneMapType", 0.f);
+  UserSettingUtil::updateUserSetting("toneMapPeakNits", 203.f);
+  UserSettingUtil::updateUserSetting("toneMapGameNits", 203.f);
+  UserSettingUtil::updateUserSetting("toneMapUINits", 203.f);
+  UserSettingUtil::updateUserSetting("colorGradeHighlights", 50.f);
+  UserSettingUtil::updateUserSetting("colorGradeShadows", 50.f);
+  UserSettingUtil::updateUserSetting("colorGradeContrast", 50.f);
+  UserSettingUtil::updateUserSetting("colorGradeSaturation", 50.f);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID) {
+  switch (fdwReason) {
+    case DLL_PROCESS_ATTACH:
+      if (!reshade::register_addon(hModule)) return FALSE;
+      break;
+    case DLL_PROCESS_DETACH:
+      reshade::unregister_addon(hModule);
+      break;
+  }
+
+  // UserSettingUtil::use(fdwReason, &userSettings, &onPresetOff);
+  ShaderReplaceMod::use(fdwReason, &customShaders);
+
+  return TRUE;
+}
