8dbdd8ef9d8d7c935b599742f0f3a98dfd22dd31
Carlos Lopez <clshortfuse@gmail.com>
2024-03-28 09:46:16 -0400

    feat(colorgrade): support wcg shadows and highlights

--- src/common/colorgrade.hlsl
+++ src/common/colorgrade.hlsl
@@ -1,5 +1,5 @@
 float3 applyContrastSafe(float3 color, float contrast, float midGray = 0.18f, float3x3 colorspace = BT709_2_XYZ_MAT) {
-  float3 workingColor = pow(abs(color) / midGray, contrast) * midGray * sign(color);
+  float3 workingColor = pow(abs(color) / midGray, contrast) * sign(color) * midGray;
   float workingLuminance = dot(abs(workingColor), float3(colorspace[1].r, colorspace[1].g, colorspace[1].b));
   float colorLuminance = dot(abs(color), float3(colorspace[1].r, colorspace[1].g, colorspace[1].b));
   return color * (colorLuminance ? (workingLuminance / colorLuminance) : 1.f);
@@ -40,19 +40,16 @@ float3 hl_con(float3 rgb, float ex, float th) {
   return rgb * (pow((n - b) / a, 1.0f / p) / n);
 }
 
-
-
-
 float3 apply_user_shadows(float3 rgb, float shadows = 1.f) {
   // Perf: explicit cube
   // rgb = shd_con(rgb, -1.8f, pow(2.f - shadows, 3) * 0.04); // 0.04 @ 1
-  rgb = shd_con(rgb, -1.8f, pow(2.f - 2 * min(shadows, 1.f), 4.f) * 0.025);  // 0.04 @ 1
-  rgb = shd_con(rgb, -0.50f * shadows * (1.f - shadows), 0.25f);           // 0 @ 1
+  rgb = sign(rgb) * shd_con(abs(rgb), -1.8f, pow(2.f - 2 * min(shadows, 1.f), 4.f) * 0.025);  // 0.04 @ 1
+  rgb = sign(rgb) * shd_con(abs(rgb), -0.50f * shadows * (1.f - shadows), 0.25f);           // 0 @ 1
 
   return rgb;
 }
 
 float3 apply_user_highlights(float3 rgb, float highlights = 1.f) {
-  rgb = hl_con(rgb, (highlights - 1.f) * 4.f, 2.f);
+  rgb = sign(rgb) * hl_con(abs(rgb), (highlights - 1.f) * 4.f, 2.f);
   return rgb;
 }
