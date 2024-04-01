9912dbd690101f265cbd67f94b4f94030ce0e802
Carlos Lopez <clshortfuse@gmail.com>
2024-04-01 15:35:54 -0400

    feat(hifirush): unclamp BT709, expand color grading options

--- src/hifirush/tonemapper00_0xF6E7E494.ps_5_0.hlsl
+++ src/hifirush/tonemapper00_0xF6E7E494.ps_5_0.hlsl
@@ -171,59 +171,44 @@ float4 main(
     outputColor = pow(outputColor, 2.2f);
   } else {
     outputColor = untonemapped.rgb;
-    if (injectedData.toneMapType != 1.f) {
-      if (injectedData.colorGradeShadows != 1.f) {
-        outputColor = apply_user_shadows(outputColor, injectedData.colorGradeShadows);
-      }
-      if (injectedData.colorGradeHighlights != 1.f) {
-        outputColor = apply_user_highlights(outputColor, injectedData.colorGradeHighlights);
-      }
-      if (injectedData.colorGradeContrast != 1.f) {
-        float3 workingColor = pow(outputColor / 0.18f, injectedData.colorGradeContrast) * 0.18f;
-        // Working in BT709 still
-        float workingColorY = yFromBT709(workingColor);
-        float outputColorY = yFromBT709(outputColor);
-        outputColor *= outputColorY ? workingColorY / outputColorY : 1.f;
-      }
+  }
+  outputColor = applyUserColorGrading(
+    outputColor,
+    1.f,
+    injectedData.colorGradeSaturation,
+    injectedData.colorGradeShadows,
+    injectedData.colorGradeHighlights,
+    injectedData.colorGradeContrast
+  );
 
-      if (injectedData.colorGradeSaturation != 1.f) {
-        float3 okLCh = okLChFromBT709(outputColor);
-        okLCh[1] *= injectedData.colorGradeSaturation;
-        outputColor = max(0, bt709FromOKLCh(okLCh));
-      }
-
-      const float vanillaMidGray = 0.18f;
-      if (injectedData.toneMapType == 2.f) {
-        float paperWhite = injectedData.toneMapGameNits * (vanillaMidGray / 0.10);  // ACES mid gray is 10%
-        float hdrScale = (injectedData.toneMapPeakNits / paperWhite);
-        outputColor = aces_rgc_rrt_odt(
-          outputColor,
-          0.0001f / hdrScale,  // minY
-          48.f * hdrScale,
-          AP1_2_BT2020_MAT
-        );
-        outputColor /= 48.f;
-        outputColor *= (vanillaMidGray / 0.10);
-        outputColor = mul(BT2020_2_BT709_MAT, outputColor);
-      } else if (injectedData.toneMapType == 3.f) {
-        outputColor = mul(BT709_2_BT2020_MAT, outputColor);
-        outputColor = max(0, outputColor);
-        const float openDRTMidGray = 11.696f / 100.f;  // open_drt_transform(0.18);
-        float paperWhite = injectedData.toneMapGameNits * (vanillaMidGray / openDRTMidGray);
-        float hdrScale = (injectedData.toneMapPeakNits / paperWhite);
-
-        outputColor = open_drt_transform(
-          outputColor,
-          100.f * hdrScale,
-          0,
-          1.f,
-          0
-        );
-        outputColor *= hdrScale;
-        outputColor *= (vanillaMidGray / openDRTMidGray);
-        outputColor = mul(BT2020_2_BT709_MAT, outputColor);
-      }
-    }
+  const float vanillaMidGray = 0.18f;
+  if (injectedData.toneMapType == 2.f) {
+    const float ACES_MID_GRAY = 0.10f;
+    float paperWhite = injectedData.toneMapGameNits * (vanillaMidGray / ACES_MID_GRAY);
+    float hdrScale = (injectedData.toneMapPeakNits / paperWhite);
+    outputColor = aces_rgc_rrt_odt(
+      outputColor,
+      0.0001f / hdrScale,
+      48.f * hdrScale
+    );
+    outputColor /= 48.f;
+    outputColor *= (vanillaMidGray / ACES_MID_GRAY);
+  } else if (injectedData.toneMapType == 3.f) {
+    const float OPENDRT_MID_GRAY = 11.696f / 100.f;  // open_drt_transform(0.18);
+    float paperWhite = injectedData.toneMapGameNits * (vanillaMidGray / OPENDRT_MID_GRAY);
+    float hdrScale = (injectedData.toneMapPeakNits / paperWhite);
+    outputColor = mul(BT709_2_BT2020_MAT, outputColor);
+    outputColor = max(0, outputColor);
+    outputColor = open_drt_transform(
+      outputColor,
+      100.f * hdrScale,
+      0,
+      1.f,
+      0
+    );
+    outputColor = mul(BT2020_2_BT709_MAT, outputColor);
+    outputColor *= hdrScale;
+    outputColor *= (vanillaMidGray / OPENDRT_MID_GRAY);
   }
 
   outputColor *= injectedData.toneMapGameNits;  // Scale by user nits
