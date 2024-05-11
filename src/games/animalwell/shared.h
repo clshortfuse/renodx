07e00fa179a26e3299b6c9e3bb953842c2561c32
Carlos Lopez <clshortfuse@gmail.com>
2024-05-11 00:44:36 -0400

    feat(animalwell): first commit

---
+++ src/games/animalwell/shared.h
@@ -0,0 +1,28 @@
+#ifndef SRC_GAMES_ANIMALWELL_SHARED_H_
+#define SRC_GAMES_ANIMALWELL_SHARED_H_
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float toneMapGammaCorrection;
+  float colorGradeExposure;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float colorGradeLUTStrength;
+  float colorGradeLUTScaling;
+  float fxScanlines;
+};
+
+#ifndef __cplusplus
+cbuffer cb4 : register(b4) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_GAMES_ANIMALWELL_SHARED_H_
