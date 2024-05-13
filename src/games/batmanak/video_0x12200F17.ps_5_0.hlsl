c74d97acd94302e10ee96f929a9acfa2722c085b
Musa Haji <mqhaji@vt.edu>
2024-05-13 03:31:17 -0400

    fo4, batmanak, la noire: fixed bt2446a_inverse_tonemapping_bt709 call, scaling with peak and paperwhite better

--- src/games/batmanak/video_0x12200F17.ps_5_0.hlsl
+++ src/games/batmanak/video_0x12200F17.ps_5_0.hlsl
@@ -38,7 +38,7 @@ void main(float4 v0 : COLOR0, float2 v1 : TEXCOORD0, out float4 o0 : SV_TARGET0)
   o0.rgb = saturate(o0.rgb);
   o0.rgb = injectedData.toneMapGammaCorrection ? pow(o0.rgb, 2.2f) : linearFromSRGB(o0.rgb);
   float videoPeak = injectedData.toneMapPeakNits / (injectedData.toneMapGameNits / 203.f);
-  o0.rgb = bt2446a_inverse_tonemapping_bt709(o0.rgb, 100.f, videoPeak);
+  o0.rgb = bt2446a_inverse_tonemapping_bt709(o0.rgb, 100.f * injectedData.toneMapGameNits / injectedData.toneMapPeakNits, videoPeak);
   o0.rgb *= injectedData.toneMapPeakNits / videoPeak;
   o0.rgb /= 80.f;
   return;
