f14d2d3c3ddcd13f7fef176035632fb09e901143
Musa Haji <mqhaji@vt.edu>
2024-05-13 14:05:42 -0400

    lanoire: fixed BT.2446A for b&w video

--- src/games/lanoire/02_bwvideo_0x10A3CFDD.ps_4_0.hlsl
+++ src/games/lanoire/02_bwvideo_0x10A3CFDD.ps_4_0.hlsl
@@ -25,7 +25,7 @@ void main(float4 v0 : SV_Position0, float2 v1 : TEXCOORD0, out float4 o0 : SV_TA
   o0 = saturate(o0);
   o0 = injectedData.toneMapGammaCorrection ? pow(o0, 2.2f) : linearFromSRGBA(o0);
   float videoPeak = injectedData.toneMapPeakNits / (injectedData.toneMapGameNits / 203.f);
-  o0.rgb = bt2446a_inverse_tonemapping_bt709(o0.rgb, 100.f, videoPeak);
+  o0.rgb = bt2446a_inverse_tonemapping_bt709(o0.rgb, 100.f * injectedData.toneMapGameNits / injectedData.toneMapPeakNits, videoPeak);
   o0.rgb *= injectedData.toneMapPeakNits / videoPeak;
   o0.rgb /= 80.f;
   return;
