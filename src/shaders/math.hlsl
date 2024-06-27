f10c6ca30074bedb9599bb032d59b49f97383a48
Filippo Tarpini <filippotarpini@hotmail.it>
2024-06-28 00:29:51 +0300

    feat(thetownoflight): first commit
    * Add 2D LUT extrapolation (needed by The Town of Light mod)
    
    Similar to the Starfield Luma implementation, but obviously simpler and more parametric.
    I think this is a pretty good start, not sure if it's final though, but I can guarentee that it works and that the math should be correct.
    
    * Add some generic color helpers and polish code
    
    * Add support for The Town of Light.
    The game outputs in gamma space with a paper white of 80 nits so it still needs an external post ReShade (e.g. Pumbo AdvancedAutoHDR) to be linearized properly. This can simply be fixed by adding a final RenoDX pass at the end on the swapchain passes.
    Tonemapping is fixed to 1000 nits atm, it doesn't really need to go further for this game.

---
+++ src/shaders/math.hlsl
@@ -0,0 +1,32 @@
+#ifndef SRC_COMMON_MATH_HLSL_
+#define SRC_COMMON_MATH_HLSL_
+
+#define FLT_MIN asfloat(0x00800000) //1.175494351e-38f
+#define FLT_MAX asfloat(0x7F7FFFFF) //3.402823466e+38f
+#define FLT10_MAX 64512.f
+#define FLT11_MAX 65024.f
+#define FLT16_MAX 65504.f
+
+float average(float3 color)
+{
+	return (color.x + color.y + color.z) / 3.f;
+}
+
+// Returns 1 or FLT_MAX if "dividend" is 0
+float safeDivision(float quotient, float dividend, bool fallbackMax = false)
+{
+	if (dividend == 0.f) {
+        return fallbackMax ? (FLT_MAX * sign(quotient)) : 1.f;
+    }
+    return quotient / dividend;
+}
+
+// Returns 1 or FLT_MAX if "dividend" is 0
+float3 safeDivision(float3 quotient, float3 dividend, bool fallbackMax = false)
+{
+    return float3(safeDivision(quotient.x, dividend.x, fallbackMax),
+	              safeDivision(quotient.y, dividend.y, fallbackMax),
+	              safeDivision(quotient.z, dividend.z, fallbackMax));
+}
+
+#endif  // SRC_COMMON_MATH_HLSL_
