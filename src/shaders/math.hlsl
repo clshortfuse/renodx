2d88dff5dcd253458ebf629b2e3e947c6bc1488f
marat569 <marat569@gmail.com>
2024-08-06 21:23:25 -0400

    remove 2.2f from exponent + change float1 to float

--- src/shaders/math.hlsl
+++ src/shaders/math.hlsl
@@ -10,15 +10,15 @@ static const float FLT10_MAX = 64512.f;
 static const float FLT11_MAX = 65024.f;
 static const float FLT16_MAX = 65504.f;
 
- float3 SafePow(float3 color, float exponent = 2.2f){
+ float3 SafePow(float3 color, float exponent){
   return sign(color) * pow(abs(color), exponent);
  }
 
- float1 SafePow(float1 color, float exponent = 2.2f){
+ float SafePow(float color, float exponent){
   return sign(color) * pow(abs(color), exponent);
  }
         
-float3 Pow(float3 color, float exponent = 2.2f){
+float3 Pow(float3 color, float exponent){
   return pow(color, exponent);
  }
         
