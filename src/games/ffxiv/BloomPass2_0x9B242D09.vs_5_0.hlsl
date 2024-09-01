b9de2bafed358331f0a8997c6067f58a9799fb34
Ersh <16157893+ersh1@users.noreply.github.com>
2024-09-01 22:51:48 +0200

    fix(ffxiv): fixed bloom radius, tonemap not running in rare cases

--- src/games/ffxiv/BloomPass2_0x9B242D09.vs_5_0.hlsl
+++ src/games/ffxiv/BloomPass2_0x9B242D09.vs_5_0.hlsl
@@ -20,25 +20,20 @@ void main(
   out float4 o4 : TEXCOORD3)
 {
   float radiusMult = injectedData.bloomRadiusMult;
-  float4 adjustments = cParam[1].xyzw;
-  adjustments.x = 1 + (1 - adjustments.x) * radiusMult;
-  adjustments.y = 1 + (1 - adjustments.y) * radiusMult;
-  adjustments.z = (1 - adjustments.x) * 0.5;
-  adjustments.w = (1 - adjustments.y) * 0.5;
 
   float4 r0;
   r0.xy = v0.xy * cParam[0].xy + cParam[0].zw;
   o0.xy = r0.xy * float2(2,-2) + float2(-1,1);
   o0.z = cParam[2].x;
   o0.w = 1;
-  r0.xy = cSamplingOffset[3].xy + v0.zw;
-  o1.xy = r0.xy * adjustments.xy + adjustments.zw;
+  r0.xy = cSamplingOffset[3].xy * radiusMult + v0.zw;
+  o1.xy = r0.xy * cParam[1].xy + cParam[1].zw;
 
   r0.xyzw = cSamplingOffset[0].xyzw * radiusMult + v0.zwzw;
-  o2.xyzw = r0.xyzw * adjustments.xyxy + adjustments.zwzw;
+  o2.xyzw = r0.xyzw * cParam[1].xyxy + cParam[1].zwzw;
   r0.xyzw = cSamplingOffset[1].xyzw * radiusMult + v0.zwzw;
-  o3.xyzw = r0.xyzw * adjustments.xyxy + adjustments.zwzw;
+  o3.xyzw = r0.xyzw * cParam[1].xyxy + cParam[1].zwzw;
   r0.xyzw = cSamplingOffset[2].xyzw * radiusMult + v0.zwzw;
-  o4.xyzw = r0.xyzw * adjustments.xyxy + adjustments.zwzw;
+  o4.xyzw = r0.xyzw * cParam[1].xyxy + cParam[1].zwzw;
   return;
 }
