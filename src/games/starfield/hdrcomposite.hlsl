720cb36ed31c94df54548ce12804c84451b12cb1
Carlos Lopez <clshortfuse@gmail.com>
2024-09-01 16:46:30 -0400

    refactor(starfield): use ApplyToneMaps

--- src/games/starfield/hdrcomposite.hlsl
+++ src/games/starfield/hdrcomposite.hlsl
@@ -114,9 +114,6 @@ float4 HDRComposite(float4 gl_FragCoord : SV_Position, float2 TEXCOORD : TEXCOOR
   // float renoDRTSaturation = 1.0f;
   // float renoDRTHighlights = 1.0f;
 
-  float3 hdrColor;
-  float3 sdrColor;
-
   float customACESParam;
   if (pushConstants.toneMapType == 2u) {
     customACESParam = ((0.56f / _28_m0[3262u].x) + 2.43f) + (_28_m0[3262u].y / (_28_m0[3262u].x * _28_m0[3262u].x));
@@ -239,156 +236,139 @@ float4 HDRComposite(float4 gl_FragCoord : SV_Position, float2 TEXCOORD : TEXCOOR
   config.reno_drt_saturation = 1.2f;
   config.reno_drt_flare = 0.01f;
 
-  if (config.type == 3.f) {
-    config.reno_drt_saturation *= config.saturation;
+  renodx::tonemap::config::DualToneMap dual_tone_map = renodx::tonemap::config::ApplyToneMaps(inputColor, config);
 
-    sdrColor = renodx::tonemap::config::ApplyRenoDRT(inputColor, config, true);
+  float3 hdrColor = dual_tone_map.color_hdr;
+  float3 sdrColor = dual_tone_map.color_sdr;
 
-    config.reno_drt_highlights *= config.highlights;
-    config.reno_drt_shadows *= config.shadows;
-    config.reno_drt_contrast *= config.contrast;
+  if (config.type == 0.f) {
+    // Vanilla
 
-    hdrColor = renodx::tonemap::config::ApplyRenoDRT(inputColor, config);
-  } else {
-    inputColor = renodx::color::grade::UserColorGrading(
-        inputColor,
-        config.exposure,
-        config.highlights,
-        config.shadows,
-        config.contrast,
-        config.saturation);
-
-    if (config.type == 1.f) {
-      hdrColor = inputColor;
+    if (pushConstants.toneMapType == 0u) {
+      // Clip SDR
       sdrColor = saturate(inputColor);
-    } else if (config.type == 2.f) {
-      hdrColor = renodx::tonemap::config::ApplyACES(inputColor, config);
-      sdrColor = renodx::tonemap::config::ApplyACES(inputColor, config, true);
-    } else {
-      if (pushConstants.toneMapType == 0u) {
-        // Clip SDR
-        sdrColor = saturate(inputColor);
-      } else if (pushConstants.toneMapType == 1u) {
-        // ACES SDR
-        sdrColor = saturate((((inputColor * 2.51f) + 0.03f) * inputColor) / ((((inputColor * 2.43f) + 0.59f) * inputColor) + 0.14f));
-      } else if (pushConstants.toneMapType == 2u) {
-        // Custom ACES SDR
-        sdrColor = saturate((((customACESParam * inputColor) + 0.03f) * inputColor) / (_28_m0[3262u].y + (((inputColor * 2.43f) + 0.59f) * inputColor)));
-      } else if (pushConstants.toneMapType == 3u) {
-        // Custom Hable SDR
-        float _159 = inputColor.r;
-        float _160 = inputColor.g;
-        float _161 = inputColor.b;
+    } else if (pushConstants.toneMapType == 1u) {
+      // ACES SDR
+      sdrColor = saturate((((inputColor * 2.51f) + 0.03f) * inputColor) / ((((inputColor * 2.43f) + 0.59f) * inputColor) + 0.14f));
+    } else if (pushConstants.toneMapType == 2u) {
+      // Custom ACES SDR
+      sdrColor = saturate((((customACESParam * inputColor) + 0.03f) * inputColor) / (_28_m0[3262u].y + (((inputColor * 2.43f) + 0.59f) * inputColor)));
+    } else if (pushConstants.toneMapType == 3u) {
+      // Custom Hable SDR
+      float _159 = inputColor.r;
+      float _160 = inputColor.g;
+      float _161 = inputColor.b;
 
-        float _57[3];
-        _57[0u] = _159;
-        _57[1u] = _160;
-        _57[2u] = _161;
-        float _520 = pow(saturate(_28_m0[3262u].w), 2.2f);
-        float _522a = saturate(_28_m0[3262u].z);
-        float _525a = saturate(_28_m0[3263u].y);
-        float _525b = clamp(_525a, 1.1920928955078125e-07f, 0.999989986419677734375f);
+      float _57[3];
+      _57[0u] = _159;
+      _57[1u] = _160;
+      _57[2u] = _161;
+      float _520 = pow(saturate(_28_m0[3262u].w), 2.2f);
+      float _522a = saturate(_28_m0[3262u].z);
+      float _525a = saturate(_28_m0[3263u].y);
+      float _525b = clamp(_525a, 1.1920928955078125e-07f, 0.999989986419677734375f);
 
-        float _517 = max(0.0f, _28_m0[3263u].x);
-        float _519 = saturate(_28_m0[3263u].z);
+      float _517 = max(0.0f, _28_m0[3263u].x);
+      float _519 = saturate(_28_m0[3263u].z);
 
-        float _522 = _520 * 0.5f * (1.0f - _522a);
-        float _523 = 1.0f - _522;
-        float _525 = (1.0f - _525b) * _523;
+      float _522 = _520 * 0.5f * (1.0f - _522a);
+      float _523 = 1.0f - _522;
+      float _525 = (1.0f - _525b) * _523;
 
-        float _570a = _525 + _522;
-        float _532a = exp2(_517);
+      float _570a = _525 + _522;
+      float _532a = exp2(_517);
 
-        float _532 = ((_520 + (-0.99999988079071044921875f)) + _523) + _532a;
-        float _534 = (_517 * 2.0f) * _519;
-        float _538 = 1.0f / _532;
-        float _539 = _520 / _532;
-        float _540 = (_525 + _520) / _532;
-        float _541 = _525 / _532;
-        float _545 = (abs(_541) < 1.1920928955078125e-07f) ? 1.0f : (_525 / _541);
-        float _548 = _545 + 1.1920928955078125e-07f;
-        float _549 = (_522 - (_545 * _539)) / _548;
-        float _550 = log2(_548);
-        float _553 = max(1.1920928955078125e-07f, exp2(log2(_522)));
-        float _559 = exp2(log2(((_517 * 0.5f) * _519) + 1.0f));
-        float _562 = (_548 * _539) / (_553 + 1.1920928955078125e-07f);
-        float _563 = log2(_553);
-        float _567 = (_562 * (-0.693147182464599609375f)) * log2(_539);
-        float _569 = (1.0f - _540) + _534;
-        float _570 = _559 - max(1.1920928955078125e-07f, exp2(log2(_570a)));
-        float _578 = ((_548 * _569) / (_570 + 1.1920928955078125e-07f)) * 0.693147182464599609375f;
-        float _580 = (log2(_570) * 0.693147182464599609375f) - (_578 * log2(_569));
-        float _590;
-        if (_534 > 0.0f) {
-          precise float _588 = (-0.0f) - exp2(((_578 * log2(_534)) + _580) * 1.44269502162933349609375f);
-          _590 = _588;
-        } else {
-          _590 = -0.0f;
-        }
-        float _592 = 1.0f / (_590 + _559);
-        float _58[3];
-        _58[0u] = 0.0f;
-        _58[1u] = 0.0f;
-        _58[2u] = 0.0f;
-        uint _599;
-        float _596 = _159;
-        uint _598 = 0u;
-        float _600;
-        bool _601;
-        for (;;) {
-          _600 = _596 * _538;
-          _601 = _600 < _539;
-          float _611;
-          if (_601) {
-            float frontier_phi_20_17_ladder;
-            if (_600 > 0.0f) {
-              frontier_phi_20_17_ladder = exp2(
-                  ((((log2(_600) * _562) + _563) * 0.693147182464599609375f) + _567) * 1.44269502162933349609375f);
-            } else {
-              frontier_phi_20_17_ladder = 0.0f;
-            }
-            _611 = frontier_phi_20_17_ladder;
-          } else {
-            float frontier_phi_20_18_ladder;
-            if (_600 < _540) {
-              float _617 = _600 + _549;
-              float frontier_phi_20_18_ladder_21_ladder;
-              if (_617 > 0.0f) {
-                frontier_phi_20_18_ladder_21_ladder = exp2(log2(_617) + _550);
-              } else {
-                frontier_phi_20_18_ladder_21_ladder = 0.0f;
-              }
-              frontier_phi_20_18_ladder = frontier_phi_20_18_ladder_21_ladder;
-            } else {
-              float _620 = ((-1.0f) - _534) + _600;
-              float _631;
-              if (_620 < (-0.0f)) {
-                _631 = exp2(((_578 * log2((-0.0f) - _620)) + _580) * 1.44269502162933349609375f);
-              } else {
-                _631 = 0.0f;
-              }
-              frontier_phi_20_18_ladder = _559 - _631;
-            }
-            _611 = frontier_phi_20_18_ladder;
-          }
-          precise float _614 = _611 * _592;
-          _58[_598] = _614;
-          _599 = _598 + 1u;
-          if (_599 == 3u) {
-            break;
-          }
-          _596 = _57[_599];
-          _598 = _599;
-          continue;
-        }
-        sdrColor.r = _58[0u];
-        sdrColor.g = _58[1u];
-        sdrColor.b = _58[2u];
+      float _532 = ((_520 + (-0.99999988079071044921875f)) + _523) + _532a;
+      float _534 = (_517 * 2.0f) * _519;
+      float _538 = 1.0f / _532;
+      float _539 = _520 / _532;
+      float _540 = (_525 + _520) / _532;
+      float _541 = _525 / _532;
+      float _545 = (abs(_541) < 1.1920928955078125e-07f) ? 1.0f : (_525 / _541);
+      float _548 = _545 + 1.1920928955078125e-07f;
+      float _549 = (_522 - (_545 * _539)) / _548;
+      float _550 = log2(_548);
+      float _553 = max(1.1920928955078125e-07f, exp2(log2(_522)));
+      float _559 = exp2(log2(((_517 * 0.5f) * _519) + 1.0f));
+      float _562 = (_548 * _539) / (_553 + 1.1920928955078125e-07f);
+      float _563 = log2(_553);
+      float _567 = (_562 * (-0.693147182464599609375f)) * log2(_539);
+      float _569 = (1.0f - _540) + _534;
+      float _570 = _559 - max(1.1920928955078125e-07f, exp2(log2(_570a)));
+      float _578 = ((_548 * _569) / (_570 + 1.1920928955078125e-07f)) * 0.693147182464599609375f;
+      float _580 = (log2(_570) * 0.693147182464599609375f) - (_578 * log2(_569));
+      float _590;
+      if (_534 > 0.0f) {
+        precise float _588 = (-0.0f) - exp2(((_578 * log2(_534)) + _580) * 1.44269502162933349609375f);
+        _590 = _588;
       } else {
-        sdrColor = saturate(inputColor);
+        _590 = -0.0f;
       }
-      hdrColor = sdrColor;
+      float _592 = 1.0f / (_590 + _559);
+      float _58[3];
+      _58[0u] = 0.0f;
+      _58[1u] = 0.0f;
+      _58[2u] = 0.0f;
+      uint _599;
+      float _596 = _159;
+      uint _598 = 0u;
+      float _600;
+      bool _601;
+      for (;;) {
+        _600 = _596 * _538;
+        _601 = _600 < _539;
+        float _611;
+        if (_601) {
+          float frontier_phi_20_17_ladder;
+          if (_600 > 0.0f) {
+            frontier_phi_20_17_ladder = exp2(
+                ((((log2(_600) * _562) + _563) * 0.693147182464599609375f) + _567) * 1.44269502162933349609375f);
+          } else {
+            frontier_phi_20_17_ladder = 0.0f;
+          }
+          _611 = frontier_phi_20_17_ladder;
+        } else {
+          float frontier_phi_20_18_ladder;
+          if (_600 < _540) {
+            float _617 = _600 + _549;
+            float frontier_phi_20_18_ladder_21_ladder;
+            if (_617 > 0.0f) {
+              frontier_phi_20_18_ladder_21_ladder = exp2(log2(_617) + _550);
+            } else {
+              frontier_phi_20_18_ladder_21_ladder = 0.0f;
+            }
+            frontier_phi_20_18_ladder = frontier_phi_20_18_ladder_21_ladder;
+          } else {
+            float _620 = ((-1.0f) - _534) + _600;
+            float _631;
+            if (_620 < (-0.0f)) {
+              _631 = exp2(((_578 * log2((-0.0f) - _620)) + _580) * 1.44269502162933349609375f);
+            } else {
+              _631 = 0.0f;
+            }
+            frontier_phi_20_18_ladder = _559 - _631;
+          }
+          _611 = frontier_phi_20_18_ladder;
+        }
+        precise float _614 = _611 * _592;
+        _58[_598] = _614;
+        _599 = _598 + 1u;
+        if (_599 == 3u) {
+          break;
+        }
+        _596 = _57[_599];
+        _598 = _599;
+        continue;
+      }
+      sdrColor.r = _58[0u];
+      sdrColor.g = _58[1u];
+      sdrColor.b = _58[2u];
+    } else {
+      sdrColor = saturate(inputColor);
     }
+    hdrColor = sdrColor;
+  } else if (config.type == 1.f) {
+    sdrColor = saturate(hdrColor);
   }
 
   float constant316 = _28_m0[316u].z;
