ec3f163ed58b5bbdd467fbae74f5db3febb21efa
Musa Haji <mqhaji@vt.edu>
2024-09-08 16:22:47 -0400

    shadow of war: initial commit; add DICE, add bloom control, add color grading sliders, fix gamma on game and UI

---
+++ src/games/shadowofwar/shared.h
@@ -0,0 +1,31 @@
+#ifndef SRC_SHADOWOFWAR_SHARED_H_
+#define SRC_SHADOWOFWAR_SHARED_H_
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float toneMapHueCorrection;
+  float fxBloom;
+  float colorGradeExposure;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float colorGradeBlowout;
+};
+
+#ifndef __cplusplus
+cbuffer cb11 : register(b11) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_SHADOWOFWAR_SHARED_H_
