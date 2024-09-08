1b61b0da7d0415689bfdad1ab7421e3e0c53584e
Musa Haji <mqhaji@vt.edu>
2024-09-08 16:22:47 -0400

    shadow of war: initial commit; add DICE, add bloom control, add color grading sliders, fix gamma on game and UI

---
+++ src/games/shadowofwar/addon.cpp
@@ -0,0 +1,200 @@
+/*
+ * Copyright (C) 2024 Musa Haji
+ * Copyright (C) 2024 Carlos Lopez
+ * SPDX-License-Identifier: MIT
+ */
+
+#define ImTextureID ImU64
+
+#define DEBUG_LEVEL_0
+#define DEBUG_SLIDERS_OFF
+
+#include <deps/imgui/imgui.h>
+
+#include <embed/0xA35C971A.h>   // Bloom
+#include <embed/0xA4592384.h>   // Game gamma / color grading
+#include <embed/0x68EABB8D.h>   // Final - DICE + UI/Game Blend
+
+#include <include/reshade.hpp>
+#include "../../mods/shader.hpp"
+#include "../../utils/settings.hpp"
+#include "./shared.h"
+
+namespace {
+
+renodx::mods::shader::CustomShaders custom_shaders = {
+    CustomShaderEntry(0xA35C971A),  // Bloom
+    CustomShaderEntry(0xA4592384),  // Game gamma / color grading
+    CustomShaderEntry(0x68EABB8D),  // Final - DICE + UI/Game Blend
+};
+
+ShaderInjectData shader_injection;
+
+renodx::utils::settings::Settings settings = {
+    new renodx::utils::settings::Setting{
+        .key = "toneMapType",
+        .binding = &shader_injection.toneMapType,
+        .value_type = renodx::utils::settings::SettingValueType::INTEGER,
+        .default_value = 2.f,
+        .can_reset = false,
+        .label = "Tone Mapper",
+        .section = "Tone Mapping",
+        .tooltip = "Sets the tone mapper type",
+        .labels = {"None", "DICE"},
+    },
+    new renodx::utils::settings::Setting{
+        .key = "toneMapPeakNits",
+        .binding = &shader_injection.toneMapPeakNits,
+        .default_value = 1000.f,
+        .can_reset = false,
+        .label = "Peak Brightness",
+        .section = "Tone Mapping",
+        .tooltip = "Sets the value of peak white in nits",
+        .min = 48.f,
+        .max = 4000.f,
+    },
+    new renodx::utils::settings::Setting{
+        .key = "toneMapGameNits",
+        .binding = &shader_injection.toneMapGameNits,
+        .default_value = 203.f,
+        .can_reset = false,
+        .label = "Game Brightness",
+        .section = "Tone Mapping",
+        .tooltip = "Sets the value of 100%% white in nits",
+        .min = 48.f,
+        .max = 500.f,
+    },
+    new renodx::utils::settings::Setting{
+        .key = "toneMapUINits",
+        .binding = &shader_injection.toneMapUINits,
+        .default_value = 203.f,
+        .can_reset = false,
+        .label = "UI Brightness",
+        .section = "Tone Mapping",
+        .tooltip = "Sets the brightness of UI and HUD elements in nits",
+        .min = 48.f,
+        .max = 500.f,
+    },
+    new renodx::utils::settings::Setting{
+        .key = "toneMapHueCorrection",
+        .binding = &shader_injection.toneMapHueCorrection,
+        .default_value = 0.f,
+        .can_reset = false,
+        .label = "SDR Hue Emulation",
+        .section = "Tone Mapping",
+        .tooltip = "Emulates hue shifting from sdr tonemapping",
+        .max = 100.f,
+        .is_enabled = []() { return shader_injection.toneMapType != 0; },
+        .parse = [](float value) { return value * 0.01f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "colorGradeExposure",
+        .binding = &shader_injection.colorGradeExposure,
+        .default_value = 1.f,
+        .label = "Exposure",
+        .section = "Color Grading",
+        .max = 10.f,
+        .format = "%.2f",
+    },
+    new renodx::utils::settings::Setting{
+        .key = "colorGradeHighlights",
+        .binding = &shader_injection.colorGradeHighlights,
+        .default_value = 50.f,
+        .label = "Highlights",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "colorGradeShadows",
+        .binding = &shader_injection.colorGradeShadows,
+        .default_value = 50.f,
+        .label = "Shadows",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "colorGradeContrast",
+        .binding = &shader_injection.colorGradeContrast,
+        .default_value = 50.f,
+        .label = "Contrast",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "colorGradeSaturation",
+        .binding = &shader_injection.colorGradeSaturation,
+        .default_value = 50.f,
+        .label = "Saturation",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "colorGradeBlowout",
+        .binding = &shader_injection.colorGradeBlowout,
+        .default_value = 0.f,
+        .label = "Blowout",
+        .section = "Color Grading",
+        .tooltip = "Controls highlight desaturation due to overexposure.",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.01f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "fxBloom",
+        .binding = &shader_injection.fxBloom,
+        .default_value = 50.f,
+        .label = "Bloom",
+        .section = "Effects",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+};
+
+void OnPresetOff() {
+  renodx::utils::settings::UpdateSetting("toneMapType", 0);
+  renodx::utils::settings::UpdateSetting("toneMapPeakNits", 1000.f);
+  renodx::utils::settings::UpdateSetting("toneMapGameNits", 203.f);
+  renodx::utils::settings::UpdateSetting("toneMapUINits", 203.f);
+  renodx::utils::settings::UpdateSetting("toneMapHueCorrection", 0.f);
+  renodx::utils::settings::UpdateSetting("colorGradeExposure", 1.f);
+  renodx::utils::settings::UpdateSetting("colorGradeHighlights", 50.f);
+  renodx::utils::settings::UpdateSetting("colorGradeShadows", 50.f);
+  renodx::utils::settings::UpdateSetting("colorGradeContrast", 50.f);
+  renodx::utils::settings::UpdateSetting("colorGradeSaturation", 50.f);
+  renodx::utils::settings::UpdateSetting("colorGradeBlowout", 0.f);
+  renodx::utils::settings::UpdateSetting("fxBloom", 50.f);
+
+}
+
+}  // namespace
+
+// NOLINTBEGIN(readability-identifier-naming)
+
+extern "C" __declspec(dllexport) const char* NAME = "RenoDX";
+extern "C" __declspec(dllexport) const char* DESCRIPTION = "RenoDX for Middle Earth: Shadow of War";
+
+// NOLINTEND(readability-identifier-naming)
+
+BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
+  switch (fdw_reason) {
+    case DLL_PROCESS_ATTACH:
+      renodx::mods::shader::force_pipeline_cloning = true;
+      renodx::mods::shader::expected_constant_buffer_index = 11;
+
+      if (!reshade::register_addon(h_module)) return FALSE;
+
+      break;
+    case DLL_PROCESS_DETACH:
+      reshade::unregister_addon(h_module);
+      break;
+  }
+
+  renodx::utils::settings::Use(fdw_reason, &settings, &OnPresetOff);
+
+  renodx::mods::shader::Use(fdw_reason, custom_shaders, &shader_injection);
+
+  return TRUE;
+}
---
+++ src/games/shadowofwar/final_0x68EABB8D.ps_5_0.hlsl
@@ -0,0 +1,61 @@
+#include "./shared.h"
+#include "./DICE.hlsl"
+// ---- Created with 3Dmigoto v1.3.16 on Thu Sep 05 23:49:36 2024
+
+cbuffer CBuffer_Data : register(b0)
+{
+  float2 TexCoordScale0 : packoffset(c0);
+  float2 MaxTexCoord0 : packoffset(c0.z);
+  float2 Gamma : packoffset(c1);
+}
+
+SamplerState TextureSampler_s : register(s0);
+Texture2D<float4> gameTexture : register(t0);
+Texture2D<float4> uiTexture : register(t1);
+
+
+// 3Dmigoto declarations
+#define cmp -
+
+
+void main(
+  float4 v0 : SV_POSITION0,
+  float2 v1 : TEXCOORD0,
+  out float4 o0 : SV_TARGET0)
+{
+  float4 r0,r1;
+  uint4 bitmask, uiDest;
+  float4 fDest;
+
+  r0.xy = TexCoordScale0.xy * v1.xy;
+  r0.xy = min(MaxTexCoord0.xy, r0.xy);
+
+  r0.xyz = gameTexture.SampleLevel(TextureSampler_s, r0.xy, 0).xyz;
+  r0.xyz *= 80.f / 300.f;
+
+  r0.xyz = renodx::color::grade::UserColorGrading(
+        r0.xyz,
+        1.f,
+        1.f,
+        1.f,
+        1.f,
+        injectedData.colorGradeSaturation,
+        injectedData.colorGradeBlowout);
+
+  r0.xyz *= injectedData.toneMapGameNits / 80.f;
+
+  DICESettings config = DefaultDICESettings();
+  config.Type = 3u;
+  // config.ShoulderStart = 0.5f;
+  r0.rgb = DICETonemap(r0.rgb, injectedData.toneMapPeakNits/80.f, config);
+
+  r1.xyzw = uiTexture.SampleLevel(TextureSampler_s, v1.xy, 0).xyzw;
+  r0.w = 1 + -r1.w;
+  // r1.xyz = pow(r1.xyz, Gamma.xxx);    // Gamma on scaled up linearized texture, covering up mismatch?
+  // r1.xyz = Gamma.yyy * r1.xyz;        // Paper White
+  r1.xyz = renodx::color::correct::GammaSafe(r1.xyz);
+  r1.xyz *= injectedData.toneMapUINits/80.f;
+  o0.xyz = r0.xyz * r0.www + r1.xyz;
+  o0.w = 1;
+  return;
+}
