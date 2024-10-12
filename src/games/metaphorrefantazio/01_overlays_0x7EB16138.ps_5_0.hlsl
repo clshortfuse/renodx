bb5a1e735ab85c1ea18778675db217e9a32eea18
Mohanned Hassan <gailardiagalane@gmail.com>
2024-10-12 21:30:28 +0400

    Clarify shader name, remove overlay's restore luminance

--- src/games/metaphorrefantazio/01_overlays_0x7EB16138.ps_5_0.hlsl
+++ src/games/metaphorrefantazio/01_overlays_0x7EB16138.ps_5_0.hlsl
@@ -1,6 +1,5 @@
 // ---- Created with 3Dmigoto v1.3.16 on Thu Sep 26 18:26:06 2024
 #include "./shared.h"
-#include "./tonemapper.hlsl"
 
 SamplerState diffuseSampler_s : register(s0);
 Texture2D<float4> diffuseTexture : register(t0);
@@ -21,11 +20,8 @@ void main(float4 v0
   /* r1.xyz = log2(abs(r0.xyz));
   r1.xyz = float3(2.20000005,2.20000005,2.20000005) * r1.xyz;
   r0.xyz = exp2(r1.xyz); */
+  r0.rgb = renodx::color::gamma::DecodeSafe(r0.rgb);
 
-  r0.rgb = renodx::color::gamma::Decode(r0.rgb);
-
-  // We want this to scale with UI
-  r0.rgb = restoreLuminance(r0.rgb);
   o0.xyzw = v1.xyzw * r0.xyzw;
   return;
 }
