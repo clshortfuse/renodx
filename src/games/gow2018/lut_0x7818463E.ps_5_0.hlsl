738f6d34c2a1a04a4678f15df39f345183b0b61f
Musa Haji <131800246+mqhaji@users.noreply.github.com>
2024-10-14 06:18:19 -0400

    gow2018: add taa lut and pq shaders, move hue correction to pq shader, fix negatives breaking lut scaling, fix incorrect paper white scaling factor, clang-format (#55)

--- src/games/gow2018/lut_0x7818463E.ps_5_0.hlsl
+++ src/games/gow2018/lut_0x7818463E.ps_5_0.hlsl
@@ -31,40 +31,35 @@ SamplerState s1_s : register(s1);
 
 SamplerState s0_s : register(s0);
 
-cbuffer cb0 : register(b0)
-{
+cbuffer cb0 : register(b0) {
   float4 cb0[31];
 }
-
-
-
 
 // 3Dmigoto declarations
 #define cmp -
 
 void main(
-  float4 v0 : SV_POSITION0,
-  float2 v1 : TEXCOORD0,
-  float2 w1 : TEXCOORD1,
-  out float4 o0 : SV_Target0)
-{
-// Needs manual fix for instruction:
-// unknown dcl_: dcl_uav_typed_texture2d (float,float,float,float) u9
-  float4 r0,r1,r2,r3,r4,r5;
+    float4 v0: SV_POSITION0,
+    float2 v1: TEXCOORD0,
+    float2 w1: TEXCOORD1,
+    out float4 o0: SV_Target0) {
+  // Needs manual fix for instruction:
+  // unknown dcl_: dcl_uav_typed_texture2d (float,float,float,float) u9
+  float4 r0, r1, r2, r3, r4, r5;
   uint4 bitmask, uiDest;
   float4 fDest;
 
   r0.xy = (uint2)v0.xy;
-  r0.zw = float2(0,0);
+  r0.zw = float2(0, 0);
   r0.zw = t18.Load(r0.xyz).xy;
   r0.zw = cb0[2].zw * r0.zw;
   r1.xyz = t6.SampleLevel(s1_s, v1.xy, 0).xyz;
-  r0.zw = r0.zw * float2(8,8) + v1.xy;
+  r0.zw = r0.zw * float2(8, 8) + v1.xy;
   r1.w = t6.SampleLevel(s1_s, r0.zw, 0).w;
   r2.x = cmp(r1.w < 1);
   if (r2.x != 0) {
     r2.xyz = t14.SampleLevel(s1_s, r0.zw, 0).xyw;
-    r3.xy = float2(1,-1) * r2.xy;
+    r3.xy = float2(1, -1) * r2.xy;
     r2.x = dot(r2.xy, r2.xy);
     r2.x = cmp(r2.x != 0.000000);
     if (r2.x != 0) {
@@ -74,20 +69,20 @@ void main(
       r2.x = min(r2.x, r4.w);
       r2.x = max(1, -r2.x);
       r2.x = r2.z / r2.x;
-      r4.xy = (int2)r0.xy & int2(63,63);
+      r4.xy = (int2)r0.xy & int2(63, 63);
       r4.z = 7 & asint(cb0[14].w);
       r4.w = 0;
       r2.y = t17.Load(r4.xyzw).x;
       r2.z = 0.25 * r2.y;
       r3.zw = r3.xy * r2.zz + v1.xy;
       r2.w = t2.SampleCmpLevelZero(s3_s, r3.zw, r2.x).x;
-      r4.xyz = r2.yyy * float3(0.25,0.25,0.25) + float3(0.25,0.5,0.75);
+      r4.xyz = r2.yyy * float3(0.25, 0.25, 0.25) + float3(0.25, 0.5, 0.75);
       r5.xyzw = r3.xyxy * r4.xxyy + v1.xyxy;
       r2.y = t2.SampleCmpLevelZero(s3_s, r5.xy, r2.x).x;
       r3.z = t2.SampleCmpLevelZero(s3_s, r5.zw, r2.x).x;
       r5.xy = r3.xy * r4.zz + v1.xy;
       r2.x = t2.SampleCmpLevelZero(s3_s, r5.xy, r2.x).x;
-      r2.xyw = cmp(float3(0,0,0) >= r2.xyw);
+      r2.xyw = cmp(float3(0, 0, 0) >= r2.xyw);
       r2.z = r2.w ? r2.z : 0;
       r2.y = r2.y ? r4.x : r2.z;
       r2.z = cmp(0 >= r3.z);
@@ -99,9 +94,9 @@ void main(
     r2.xy = r3.xy * r2.xx + v1.xy;
     r2.xyz = t3.SampleLevel(s1_s, r2.xy, 0).xyz;
     r3.xyzw = t12.SampleLevel(s1_s, v1.xy, 0).xyzw;
-    r3.xyzw = max(float4(0,0,0,0), r3.xyzw);
+    r3.xyzw = max(float4(0, 0, 0, 0), r3.xyzw);
     r4.xyzw = t13.SampleLevel(s1_s, r0.zw, 0).xyzw;
-    r4.xyzw = max(float4(0,0,0,0), r4.xyzw);
+    r4.xyzw = max(float4(0, 0, 0, 0), r4.xyzw);
     r0.z = r4.w * r3.w;
     r3.xyz = r3.xyz * r4.www + r4.xyz;
     r0.w = r3.y + -r2.y;
@@ -144,11 +139,11 @@ void main(
   r2.xyz = cb0[20].xyz * r1.yyy;
   r2.xyz = r1.xxx * cb0[19].xyz + r2.xyz;
   r1.xyz = r1.zzz * cb0[21].xyz + r2.xyz;
-  r1.xyz = max(float3(0,0,0), r1.xyz);
-  r2.xyzw = -cb0[12].wxyz + float4(1,1,1,1);
+  r1.xyz = max(float3(0, 0, 0), r1.xyz);
+  r2.xyzw = -cb0[12].wxyz + float4(1, 1, 1, 1);
   r0.z = cb0[13].y * v1.x;
   r0.z = v1.y * r0.z;
-  r3.xy = float2(1,1) + -v1.xy;
+  r3.xy = float2(1, 1) + -v1.xy;
   r0.z = r3.x * r0.z;
   r0.z = r0.z * r3.y;
   r0.z = log2(abs(r0.z));
@@ -158,33 +153,33 @@ void main(
   r2.xyz = r0.zzz * r2.yzw + cb0[12].xyz;
   r1.xyz = r2.xyz * r1.xyz;
   r0.z = cmp(0 != cb0[0].w);
-  r1.xyz = r0.zzz ? float3(0,0,0) : r1.xyz;
-    
+  r1.xyz = r0.zzz ? float3(0, 0, 0) : r1.xyz;
+
   float3 lutInputColor = r1.xyz;
   if (injectedData.toneMapType == 0) {
     // convert arri logc800
-    r2.xyz = cmp(float3(0.0105910003,0.0105910003,0.0105910003) < r1.xyz);
-    r3.xyzw = r1.xxyy * float4(5.55555582,5.3676548,5.55555582,5.3676548) + float4(0.0522719994,0.0928089991,0.0522719994,0.0928089991);
+    r2.xyz = cmp(float3(0.0105910003, 0.0105910003, 0.0105910003) < r1.xyz);
+    r3.xyzw = r1.xxyy * float4(5.55555582, 5.3676548, 5.55555582, 5.3676548) + float4(0.0522719994, 0.0928089991, 0.0522719994, 0.0928089991);
     r1.xy = log2(r3.xz);
-    r1.xy = r1.xy * float2(0.0744116008,0.0744116008) + float2(0.385536999,0.385536999);
+    r1.xy = r1.xy * float2(0.0744116008, 0.0744116008) + float2(0.385536999, 0.385536999);
     r3.xy = r2.xy ? r1.xy : r3.yw;
-    r1.xy = r1.zz * float2(5.55555582,5.3676548) + float2(0.0522719994,0.0928089991);
+    r1.xy = r1.zz * float2(5.55555582, 5.3676548) + float2(0.0522719994, 0.0928089991);
     r0.z = log2(r1.x);
     r0.z = r0.z * 0.0744116008 + 0.385536999;
     r3.z = r2.z ? r0.z : r1.y;
     // Sample 64x64x64 LUT
-    r1.xyz = r3.xyz * float3(0.984375,0.984375,0.984375) + float3(0.0078125,0.0078125,0.0078125);
+    r1.xyz = r3.xyz * float3(0.984375, 0.984375, 0.984375) + float3(0.0078125, 0.0078125, 0.0078125);
     r1.xyz = t0.SampleLevel(s1_s, r1.xyz, 0).xyz;
     // back to linear
-    r2.xyz = cmp(float3(0.149658203,0.149658203,0.149658203) < r1.xyz);
-    r3.xyzw = float4(-0.385536999,-0.0928089991,-0.385536999,-0.0928089991) + r1.xxyy;
-    r3.xyzw = float4(13.4387865,0.186301097,13.4387865,0.186301097) * r3.xyzw;
+    r2.xyz = cmp(float3(0.149658203, 0.149658203, 0.149658203) < r1.xyz);
+    r3.xyzw = float4(-0.385536999, -0.0928089991, -0.385536999, -0.0928089991) + r1.xxyy;
+    r3.xyzw = float4(13.4387865, 0.186301097, 13.4387865, 0.186301097) * r3.xyzw;
     r1.xy = exp2(r3.xz);
-    r1.xy = float2(-0.0522719994,-0.0522719994) + r1.xy;
-    r1.xy = float2(0.179999992,0.179999992) * r1.xy;
+    r1.xy = float2(-0.0522719994, -0.0522719994) + r1.xy;
+    r1.xy = float2(0.179999992, 0.179999992) * r1.xy;
     r3.xy = r2.xy ? r1.xy : r3.yw;
-    r1.xy = float2(-0.385536999,-0.0928089991) + r1.zz;
-    r1.xy = float2(13.4387865,0.186301097) * r1.xy;
+    r1.xy = float2(-0.385536999, -0.0928089991) + r1.zz;
+    r1.xy = float2(13.4387865, 0.186301097) * r1.xy;
     r0.z = exp2(r1.x);
     r0.z = -0.0522719994 + r0.z;
     r0.z = 0.179999992 * r0.z;
@@ -195,12 +190,12 @@ void main(
   } else {
     lutInputColor = renodx::color::grade::UserColorGrading(
         lutInputColor,
-        injectedData.colorGradeExposure,              // exposure
-        injectedData.colorGradeHighlights,            // highlights
-        injectedData.colorGradeShadows,               // shadows
-        injectedData.colorGradeContrast,              // contrast
-        1.f,                                          // saturation, applied later
-        0.f);                                         // dechroma, applied later
+        injectedData.colorGradeExposure,    // exposure
+        injectedData.colorGradeHighlights,  // highlights
+        injectedData.colorGradeShadows,     // shadows
+        injectedData.colorGradeContrast,    // contrast
+        1.f,                                // saturation, applied later
+        0.f);                               // dechroma, applied later
 
     renodx::lut::Config lut_config = renodx::lut::config::Create(
         s1_s,
@@ -214,7 +209,7 @@ void main(
     // Cleans up raised black floor
     if (injectedData.colorGradeLUTScaling && injectedData.colorGradeLUTStrength) {
       float3 minBlack = renodx::color::arri::logc::c800::Decode(t0.SampleLevel(s1_s, renodx::color::arri::logc::c800::Encode((0.f).xxx), 0.0f).rgb);
-      const float lutMinY = renodx::color::y::from::BT709(minBlack);
+      const float lutMinY = renodx::color::y::from::BT709(max(0, minBlack));
       if (lutMinY > 0) {
         float3 correctedBlack = renodx::lut::CorrectBlack(lutInputColor, lutOutputColor, lutMinY, 0.f);
         lutOutputColor = lerp(lutOutputColor, correctedBlack, injectedData.colorGradeLUTScaling);
@@ -222,14 +217,16 @@ void main(
     }
 
     r1.xyz = lerp(lutInputColor, lutOutputColor, injectedData.colorGradeLUTStrength);
+
+    r1.xyz = max(0, r1.xyz);  // DLSS/FSR clamps to BT.709
   }
 
   r0.z = max(r1.y, r1.z);
   r0.z = max(r1.x, r0.z);
-  r2.xyz = cmp(r1.xyz < float3(0.180000007,0.180000007,0.180000007));
+  r2.xyz = cmp(r1.xyz < float3(0.180000007, 0.180000007, 0.180000007));
   r3.xyz = min(cb0[24].xxx, r1.xyz);
-  r4.xyz = sign(r3.xyz) * pow(abs(r3.xyz), cb0[23].www);  // r4.xyz = exp2(cb0[23].www * log2(r3.xyz));
-  r3.xyz = sign(r3.xyz) * pow(abs(r3.xyz), cb0[24].zzz);  // r3.xyz = exp2(cb0[24].zzz * log2(r3.xyz));
+  r4.xyz = pow(r3.xyz, cb0[23].www);  // r4.xyz = exp2(cb0[23].www * log2(r3.xyz));
+  r3.xyz = pow(r3.xyz, cb0[24].zzz);  // r3.xyz = exp2(cb0[24].zzz * log2(r3.xyz));
   r3.xyz = r3.xyz * cb0[25].xxx + cb0[25].yyy;
   r3.xyz = r4.xyz / r3.xyz;
   r2.xyz = r2.xyz ? r3.xyz : r1.xyz;
@@ -239,22 +236,7 @@ void main(
   r1.xyz = r1.xyz / r0.zzz;
   r2.xyz = r2.xyz + -r1.xyz;
   r1.xyz = cb0[24].www * r2.xyz + r1.xyz;
-  o0.xyz = r1.xyz;  // o0.xyz = max(float3(0,0,0), r1.xyz);
-  if (injectedData.toneMapType == 0) {
-    o0.xyz = max(0, o0.xyz);
-  } else {
-    o0.rgb = renodx::color::grade::UserColorGrading(  // apply saturation adjustment after LUT
-        o0.rgb,
-        1.f,                                            // exposure, applied earlier
-        1.f,                                            // highlights, applied earlier
-        1.f,                                            // shadows, applied earlier
-        1.f,                                            // contrast, applied earlier
-        injectedData.colorGradeSaturation,              // saturation
-        injectedData.colorGradeBlowout,                 // dechroma
-        injectedData.toneMapHueCorrection,              // hue correction
-        renodx::tonemap::uncharted2::BT709(lutInputColor));
-
-  }
+  o0.xyz = max(0, r1.xyz);  // DLSS/FSR clamps to BT.709
   r0.z = saturate(r1.w * 4 + -1);
   r0.z = -cb0[29].w * r0.z;
   r1.x = cmp(-0.100000001 < r0.z);
@@ -263,7 +245,7 @@ void main(
   r0.z = cb0[29].w + r0.z;
   r0.z = cb0[30].z * r0.z;
   // Store the value r0.zzzz at the location r0.xy in the UAV u9
-  u9[r0.xy] = float4(r0.zzzz);    // store_uav_typed u9.xyzw, r0.xyyy, r0.zzzz
+  u9[r0.xy] = float4(r0.zzzz);  // store_uav_typed u9.xyzw, r0.xyyy, r0.zzzz
   o0.w = 1;
   return;
 }
