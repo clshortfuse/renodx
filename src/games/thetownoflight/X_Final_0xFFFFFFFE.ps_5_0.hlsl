6c00b19720db8dc90596f69dacf74808938f2956
Musa Haji <mqhaji@vt.edu>
2024-10-22 18:42:04 -0400

    thetownoflight:improve comments, change slider minimums for game nits, ui nits, and peak nits

--- src/games/thetownoflight/X_Final_0xFFFFFFFE.ps_5_0.hlsl
+++ src/games/thetownoflight/X_Final_0xFFFFFFFE.ps_5_0.hlsl
@@ -10,10 +10,12 @@ void main(
   float4 color = sourceTexture.Sample(sourceSampler_s, texcoord.xy);
 
   if (injectedData.outputMode == 1) {
-    // Linearize with 2.2 Gamma and scale paper white
+    // Linearize with 2.2 Gamma and scale paper white for HDR
     color.rgb = renodx::color::gamma::DecodeSafe(color.rgb, 2.2);
     color.rgb *= injectedData.toneMapUINits / renodx::color::srgb::REFERENCE_WHITE;
   } else {
+    // scRGB HDR can also be used in SDR in Windows, but it's interpreted with sRGB transfer function,
+    // so we apply the sRGB transfer function and let the display apply the sRGB<->2.2 gamma mismatch
     color.rgb = max(0, color.rgb);
     color.rgb = renodx::color::srgb::Decode(color.rgb);
   }
