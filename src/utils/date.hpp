1fab421994376c6d586ea44489f7122eb0437427
Voosh <3669773+NotVoosh@users.noreply.github.com>
2024-10-26 21:15:23 +0200

    (added) comment

--- src/utils/date.hpp
+++ src/utils/date.hpp
@@ -16,7 +16,7 @@ constexpr unsigned int compileMonth = (__DATE__[0] == 'J')   ? ((__DATE__[1] == 
                                       : (__DATE__[0] == 'D') ? 12                                                           // Dec
                                                              : 0;
 constexpr unsigned int compileDay = (__DATE__[4] == ' ') ? (__DATE__[5] - '0') : (__DATE__[4] - '0') * 10 + (__DATE__[5] - '0');
-
+// IsoDate returns "YYYY-MM-DD" format 
 constexpr char IsoDate[] =
     {compileYear / 1000 + '0', (compileYear % 1000) / 100 + '0', (compileYear % 100) / 10 + '0', compileYear % 10 + '0',
      '-', compileMonth / 10 + '0', compileMonth % 10 + '0',
