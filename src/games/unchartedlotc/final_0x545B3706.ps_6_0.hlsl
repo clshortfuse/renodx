5dd7b09a4c8d69196b247668f6e700d7024caf64
Musa Haji <mqhaji@vt.edu>
2024-10-28 19:44:59 -0400

    unchartedlotc: update use of deprecated functions

--- src/games/unchartedlotc/final_0x545B3706.ps_6_0.hlsl
+++ src/games/unchartedlotc/final_0x545B3706.ps_6_0.hlsl
@@ -17,20 +17,20 @@ float4 main(PSInput IN)
   float3 inputColor = g_applyHdrCodingSrcTmpBuffer.Sample(g_applyHdrCodingSamplerState, IN.TEXCOORD).xyz;
 
   // Linearize with 2.2 Gamma
-  float3 linearColor = renodx::math::SafePow(inputColor, 2.2);
+  float3 linearColor = renodx::color::gamma::DecodeSafe(inputColor, 2.2);
 
-  // Custom paper white values based on the brightness setting (m_params.x)
+  // Use custom paper white values based on the brightness setting (m_params.x)
   float paperWhite = m_params.x;
   // Check brightness levels and adjust paper white accordingly
   if (m_params.x == 220) {
-    paperWhite = 203.0f;  // Brightness level 3
+    paperWhite = 203.0f;  // Brightness level 3 (set to BT.2408 reference white)
   } else if (m_params.x == 260) {
     paperWhite = 250.0f;  // Brightness level 4
   }
 
   // Convert from BT.709 to BT.2020 PQ with paper white based on brightness slider
   float3 bt2020Color = renodx::color::bt2020::from::BT709(linearColor);
-  float3 pqEncodedColor = renodx::color::pq::from::BT2020(bt2020Color, paperWhite);
+  float3 pqEncodedColor = renodx::color::pq::Encode(bt2020Color, paperWhite);
 
   // Return the final PQ-encoded color with alpha set to 1.0
   return float4(pqEncodedColor, 1.0);
