51dc08a612835cd016775ff03c6a884cf750b1b3
Musa Haji <mqhaji@vt.edu>
2024-10-28 19:37:04 -0400

    unchartedlotc: remove all settings and relevant dependencies, clang-format

--- src/games/unchartedlotc/addon.cpp
+++ src/games/unchartedlotc/addon.cpp
@@ -4,58 +4,16 @@
  * SPDX-License-Identifier: MIT
  */
 
-#define ImTextureID ImU64
-
-#define DEBUG_LEVEL_0
-#define DEBUG_SLIDERS_OFF
-
-#include <deps/imgui/imgui.h>
-
-#include <embed/0x545B3706.h>   // Linearization + DICE + BT.2020 + PQ Encoding
+#include <embed/0x545B3706.h>  // Linearization + BT.2020 + PQ Encoding
 
 #include <include/reshade.hpp>
 #include "../../mods/shader.hpp"
-#include "../../utils/settings.hpp"
-#include "./shared.h"
 
 namespace {
 
 renodx::mods::shader::CustomShaders custom_shaders = {
-    CustomShaderEntry(0x545B3706),  // Linearization + DICE + BT.2020 + PQ Encoding
+    CustomShaderEntry(0x545B3706),  // Linearization + BT.2020 + PQ Encoding
 };
-
-ShaderInjectData shader_injection;
-
-renodx::utils::settings::Settings settings = {
-    new renodx::utils::settings::Setting{
-        .key = "toneMapType",
-        .binding = &shader_injection.toneMapType,
-        .value_type = renodx::utils::settings::SettingValueType::INTEGER,
-        .default_value = 3.f,
-        .can_reset = false,
-        .label = "Tone Mapper",
-        .section = "Tone Mapping",
-        .tooltip = "Sets the tone mapper type",
-        .labels = {"None", "DICE"},
-    },
-    new renodx::utils::settings::Setting{
-        .key = "toneMapPeakNits",
-        .binding = &shader_injection.toneMapPeakNits,
-        .default_value = 1000.f,
-        .can_reset = false,
-        .label = "Peak Brightness",
-        .section = "Tone Mapping",
-        .tooltip = "Sets the value of peak white in nits",
-        .min = 48.f,
-        .max = 4000.f,
-        .is_enabled = []() { return shader_injection.toneMapType != 0; },
-    },
-};
-
-void OnPresetOff() {
-  renodx::utils::settings::UpdateSetting("toneMapType", 0.f);
-  renodx::utils::settings::UpdateSetting("toneMapPeakNits", 1000.f);
-}
 
 }  // namespace
 
@@ -69,21 +27,14 @@ extern "C" __declspec(dllexport) const char* DESCRIPTION = "RenoDX for Uncharted
 BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
   switch (fdw_reason) {
     case DLL_PROCESS_ATTACH:
-      renodx::mods::shader::expected_constant_buffer_space = 18;
-      renodx::mods::shader::trace_unmodified_shaders = true;
-
-
       if (!reshade::register_addon(h_module)) return FALSE;
-
       break;
     case DLL_PROCESS_DETACH:
       reshade::unregister_addon(h_module);
       break;
   }
 
-  renodx::utils::settings::Use(fdw_reason, &settings, &OnPresetOff);
-
-  renodx::mods::shader::Use(fdw_reason, custom_shaders, &shader_injection);
+  renodx::mods::shader::Use(fdw_reason, custom_shaders);
 
   return TRUE;
 }
--- src/games/unchartedlotc/shared.h
+++ src/games/unchartedlotc/shared.h
@@ -5,17 +5,4 @@
 #include "../../shaders/renodx.hlsl"
 #endif
 
-// Must be 32bit aligned
-// Should be 4x32
-struct ShaderInjectData {
-  float toneMapType;
-  float toneMapPeakNits;
-};
-
-#ifndef __cplusplus
-cbuffer injectedBuffer : register(b0,space18) {
-  ShaderInjectData injectedData : packoffset(c0);
-}
-#endif
-
 #endif  // SRC_UNCHARTEDLOTC_SHARED_H_
