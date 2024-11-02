5072e10576a7405f2e41ce3f409abefb1a0ac955
Musa Haji <mqhaji@vt.edu>
2024-11-01 22:29:23 -0400

    rdr1: initial commit, add PSRDR2PostfxOptics

---
+++ src/games/rdr1/addon.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2024 Musa Haji
+ * Copyright (C) 2024 Carlos Lopez
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <embed/0xEEEE53C5.h>  // Color Grade
+
+#include <include/reshade.hpp>
+#include "../../mods/shader.hpp"
+
+namespace {
+
+renodx::mods::shader::CustomShaders custom_shaders = {
+    CustomShaderEntry(0xEEEE53C5),  // Color Grade
+};
+
+}  // namespace
+
+// NOLINTBEGIN(readability-identifier-naming)
+
+extern "C" __declspec(dllexport) const char* NAME = "RenoDX";
+extern "C" __declspec(dllexport) const char* DESCRIPTION = "RenoDX for Red Dead Redemption";
+
+// NOLINTEND(readability-identifier-naming)
+
+BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
+  switch (fdw_reason) {
+    case DLL_PROCESS_ATTACH:
+      if (!reshade::register_addon(h_module)) return FALSE;
+      break;
+    case DLL_PROCESS_DETACH:
+      reshade::unregister_addon(h_module);
+      break;
+  }
+
+  renodx::mods::shader::Use(fdw_reason, custom_shaders);
+
+  return TRUE;
+}
