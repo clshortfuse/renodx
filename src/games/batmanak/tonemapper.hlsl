1fa548bf2a2304297183b4d7cc9f1118c5ac483a
Musa Haji <mqhaji@vt.edu>
2024-12-18 13:40:46 -0500

    feat(batmanak): add filmgraintype, tonemapblend, huecorrection, perchannel, flare, fix effect + motion blur off
    - vanilla and perceptual film grain types
    - blend with vanilla color
    - hue correction
    - renodrt per channel tonemapping
    - flare slider
    - fixed detective mode + motion blur off breaking when vanilla tonemapper is selected
    - fixed flipped graph_config.draw bool in detective mode + motion blur

--- src/games/batmanak/tonemapper.hlsl
+++ src/games/batmanak/tonemapper.hlsl
@@ -78,6 +78,40 @@ float3 applyUserToneMap(float3 untonemapped, Texture2D lut_texture, SamplerState
   }
   float renoDRTHighlights = 1.2f;
 
+  renodx::tonemap::Config config = renodx::tonemap::config::Create();
+
+  // hue correction requires per channel tonemap or highlights will have artifacts
+  // per channel tonemap is also more saturated in midtones and shadows 
+  if (injectedData.toneMapPerChannel == 1.f || injectedData.toneMapType == 2) {
+    config.reno_drt_per_channel = true;
+    config.hue_correction_strength = injectedData.toneMapHueCorrection;
+  } else {
+    config.hue_correction_strength = 0.f;
+  }
+  config.type = injectedData.toneMapType;
+  config.peak_nits = injectedData.toneMapPeakNits;
+  config.game_nits = injectedData.toneMapGameNits;
+  config.gamma_correction = injectedData.toneMapGammaCorrection - 1;
+  config.exposure = injectedData.colorGradeExposure;
+  config.highlights = injectedData.colorGradeHighlights;
+  config.shadows = injectedData.colorGradeShadows;
+  config.contrast = injectedData.colorGradeContrast;
+  config.saturation = injectedData.colorGradeSaturation;
+  config.reno_drt_highlights = renoDRTHighlights;
+  config.reno_drt_shadows = renoDRTShadows;
+  config.reno_drt_contrast = renoDRTContrast;
+  config.reno_drt_saturation = renoDRTSaturation;
+  config.reno_drt_dechroma = renoDRTDechroma;
+  config.mid_gray_value = vanillaMidGray;
+  config.mid_gray_nits = vanillaMidGray * 100.f;
+  config.reno_drt_flare = renoDRTFlare;
+  config.hue_correction_type = renodx::tonemap::config::hue_correction_type::CUSTOM;
+  config.hue_correction_color = vanillaColor;
+  config.reno_drt_hue_correction_method = renodx::tonemap::renodrt::config::hue_correction_method::DARKTABLE_UCS;
+  config.reno_drt_tone_map_method = renodx::tonemap::renodrt::config::tone_map_method::DANIELE;
+  config.reno_drt_working_color_space = 2u;
+
+
   tm_config.hue_correction_strength = injectedData.toneMapHueCorrection * injectedData.toneMapPerChannel;
   tm_config.reno_drt_per_channel = injectedData.toneMapPerChannel;
   tm_config.type = injectedData.toneMapType;
@@ -145,6 +179,16 @@ float3 applyUserToneMap(float3 untonemapped, Texture2D lut_texture, SamplerState
         injectedData.colorGradeShadows,
         injectedData.colorGradeContrast,
         injectedData.colorGradeSaturation);
+
+    outputColor = lerp(vanillaColor, outputColor, saturate(vanillaColor));
+  }
+
+  if (injectedData.toneMapBlend == 1.f) {
+    float3 vanillaLUTInputColor = min(1.f, pow(vanillaColor, 1.f / 2.2f));
+    float3 vanillaLUT = renodx::lut::Sample(lutTexture, lutSampler, vanillaLUTInputColor).rgb;
+    vanillaLUT = pow(vanillaLUT, 2.2f);
+
+    vanillaColor = lerp(vanillaColor, vanillaLUT, injectedData.colorGradeLUTStrength);
 
     outputColor = lerp(vanillaColor, outputColor, saturate(vanillaColor));
   }
