7f6d1eb82dbd181e2ca27ae8eb06ffaa47d56efa
Musa Haji <mqhaji@vt.edu>
2025-01-03 12:51:16 -0500

    feat(elitedangerous): intiial commit, vanilla+ (DICE) tonemapper and ui slider

---
+++ src/games/elitedangerous/shared.h
@@ -0,0 +1,33 @@
+#ifndef SRC_ELITEDANGEROUS_SHARED_H_
+#define SRC_ELITEDANGEROUS_SHARED_H_
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float toneMapGammaCorrection;
+  float toneMapHueCorrectionMethod;
+  float toneMapHueCorrection;
+  float colorGradeExposure;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float colorGradeBlowout;
+  float colorGradeLUTStrength;
+};
+
+#ifndef __cplusplus
+cbuffer cb13 : register(b13) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_ELITEDANGEROUS_SHARED_H_
