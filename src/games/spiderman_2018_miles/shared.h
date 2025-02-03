d38d385da547e9f9b35e5ac39e020c7b87a29d63
Musa Haji <mqhaji@vt.edu>
2025-02-03 01:43:54 -0500

    feat(spiderman_2018_miles): set gamma to 2.35, enable 2018 cAb off shader, rename mod

--- src/games/spiderman_2018_miles/shared.h
+++ src/games/spiderman_2018_miles/shared.h
@@ -1,8 +1,8 @@
-#ifndef SRC_SPIDERMANMILESMORALES_SHARED_H_
-#define SRC_SPIDERMANMILESMORALES_SHARED_H_
+#ifndef SRC_SPIDERMAN_2018_MILES_SHARED_H_
+#define SRC_SPIDERMAN_2018_MILES_SHARED_H_
 
 #define RENODX_GAMMA_ADJUST_TYPE  1u    // 0 - multiplier, 1 - fixed
-#define RENODX_GAMMA_ADJUST_VALUE 2.2f
+#define RENODX_GAMMA_ADJUST_VALUE 2.35f // UI paper white slider behavior fixed at 2.35
 
 #ifndef __cplusplus
 
@@ -10,4 +10,4 @@
 
 #endif
 
-#endif  // SRC_SPIDERMANMILESMORALES_SHARED_H_
+#endif  // SRC_SPIDERMAN_2018_MILES_SHARED_H_
