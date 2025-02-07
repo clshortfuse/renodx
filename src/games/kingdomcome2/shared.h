fe3a990286cf8ef75d9a97b22749fca9ec30d625
Mohanned Hassan <gailardiagalane@gmail.com>
2025-02-07 19:05:12 +0400

    fix(kingdomcome2): Grab correct untonemapped, and fix unsafe sqrt

--- src/games/kingdomcome2/shared.h
+++ src/games/kingdomcome2/shared.h
@@ -1,21 +1,18 @@
 #ifndef SRC_KINGDOM_COME2_SHARED_H_
 #define SRC_KINGDOM_COME2_SHARED_H_
 
-#define RENODX_TONE_MAP_TYPE                 injectedData.toneMapType
-#define RENODX_PEAK_NITS                     injectedData.toneMapPeakNits
-#define RENODX_GAME_NITS                     injectedData.toneMapGameNits
-#define RENODX_UI_NITS                       injectedData.toneMapUINits
-#define RENODX_TONE_MAP_EXPOSURE             injectedData.colorGradeExposure
-#define RENODX_TONE_MAP_HIGHLIGHTS           injectedData.colorGradeHighlights
-#define RENODX_TONE_MAP_SHADOWS              injectedData.colorGradeShadows
-#define RENODX_TONE_MAP_CONTRAST             injectedData.colorGradeContrast
-#define RENODX_TONE_MAP_SATURATION           injectedData.colorGradeSaturation
-#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION injectedData.colorGradeHighlightSaturation
-#define RENODX_TONE_MAP_BLOWOUT              injectedData.colorGradeBlowout
-#define RENODX_TONE_MAP_FLARE                injectedData.colorGradeFlare
-#define RENODX_EFFECT_BLOOM                  injectedData.fxBloom
-#define RENODX_GAMMA_CORRECTION              injectedData.toneMapGammaCorrection
-#define RENODX_INTERMEDIATE_ENCODING         1.f
+#define RENODX_TONE_MAP_TYPE                 shader_injection.toneMapType
+#define RENODX_PEAK_NITS                     shader_injection.toneMapPeakNits
+#define RENODX_GAME_NITS                     shader_injection.toneMapGameNits
+#define RENODX_UI_NITS                       shader_injection.toneMapUINits
+#define RENODX_TONE_MAP_EXPOSURE             shader_injection.colorGradeExposure
+#define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.colorGradeHighlights
+#define RENODX_TONE_MAP_SHADOWS              shader_injection.colorGradeShadows
+#define RENODX_TONE_MAP_CONTRAST             shader_injection.colorGradeContrast
+#define RENODX_TONE_MAP_SATURATION           shader_injection.colorGradeSaturation
+#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION shader_injection.colorGradeHighlightSaturation
+#define RENODX_TONE_MAP_BLOWOUT              shader_injection.colorGradeBlowout
+#define RENODX_TONE_MAP_FLARE                shader_injection.colorGradeFlare
 #define RENODX_SWAP_CHAIN_CLAMP_COLOR_SPACE  2.f
 
 // Must be 32bit aligned
@@ -44,11 +41,11 @@ struct ShaderInjectData {
 
 #ifndef __cplusplus
 #if ((__SHADER_TARGET_MAJOR == 5 && __SHADER_TARGET_MINOR >= 1) || __SHADER_TARGET_MAJOR >= 6)
-cbuffer injectedBuffer : register(b13, space50) {
+cbuffer shader_injection : register(b13, space50) {
 #elif (__SHADER_TARGET_MAJOR < 5) || ((__SHADER_TARGET_MAJOR == 5) && (__SHADER_TARGET_MINOR < 1))
-cbuffer injectedBuffer : register(b13) {
+cbuffer shader_injection : register(b13) {
 #endif
-  ShaderInjectData injectedData : packoffset(c0);
+  ShaderInjectData shader_injection : packoffset(c0);
 }
 
 #if (__SHADER_TARGET_MAJOR >= 6)
