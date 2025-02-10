fbd0ef79b4c76af79cf5bc5f3825e3b3aec7948d
Mohanned Hassan <gailardiagalane@gmail.com>
2025-02-11 03:22:23 +0400

    fix(kingdomcome2): Fix AMD GPUs issues

--- src/games/kingdomcome2/addon.cpp
+++ src/games/kingdomcome2/addon.cpp
@@ -318,11 +318,12 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
           .use_resource_view_cloning = true,
       });
 
-      renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
+      // Bugs out AMD GPUs
+      /* renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
           .old_format = reshade::api::format::r11g11b10_float,
-          .new_format = reshade::api::format::r16g16b16a16_float,
+          .new_format = reshade::api::format::r16g16b16a16_typeless,
           .use_resource_view_cloning = true,
-      });
+      }); */
 
       break;
     case DLL_PROCESS_DETACH:
