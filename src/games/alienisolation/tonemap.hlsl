7b6079bfe32000ca8c81a42ba8f4c795b187fe6e
Musa Haji <mqhaji@vt.edu>
2025-02-10 20:36:45 -0500

    feat(alienisolation): use noise tex for rng, add colored grain

--- src/games/alienisolation/tonemap.hlsl
+++ src/games/alienisolation/tonemap.hlsl
@@ -5,6 +5,7 @@
 
 float3 applyFilmGrain(float3 input_color, Texture2D<float4> SamplerNoise_TEX,
                       SamplerState SamplerNoise_SMP_s, float4 v1) {
+  float3 grained_color;
   if (injectedData.fxFilmGrainType == 0.f) {  // Noise
     float4 r0, r2;
     float3 r1, r3;
@@ -22,14 +23,25 @@ float3 applyFilmGrain(float3 input_color, Texture2D<float4> SamplerNoise_TEX,
     r1.xyz = r1.xyz * r2.zzz + r2.xyw;
     r0.xyz = injectedData.fxFilmGrain * r1.xyz + r0.xyz;
 
-    return r0.rgb;
-  } else {  // Film Grain (B&W)
+    grained_color = r0.rgb;
+  } else {  // Film Grain, applied in linear space
     float3 linear_color = renodx::color::gamma::DecodeSafe(input_color, 2.2f);
-    float3 grained_color = renodx::effects::ApplyFilmGrain(
-        linear_color,
-        v1.xy,  // Screen-space coordinates
-        v1.z,   // `v1.z` as random seed
-        injectedData.fxFilmGrain * .02f);
-    return renodx::color::gamma::EncodeSafe(grained_color, 2.2f);
+    if (injectedData.fxFilmGrainType == 1.f) {  // B&W
+      float random_seed = SamplerNoise_TEX.Sample(SamplerNoise_SMP_s, v1.xy).y;
+      grained_color = renodx::effects::ApplyFilmGrain(
+          linear_color,
+          v1.xy,        // Screen-space coordinates
+          random_seed,  // Sample noise tex for random seed
+          injectedData.fxFilmGrain * .02f);
+    } else {  // Colored
+      float3 random_seed = SamplerNoise_TEX.Sample(SamplerNoise_SMP_s, v1.xy).rgb;
+      grained_color = renodx::effects::ApplyFilmGrainColored(
+          linear_color,
+          v1.xy,        // Screen-space coordinates
+          random_seed,  // Sample noise tex for random seed
+          injectedData.fxFilmGrain * .02f);
+    }
+    grained_color = renodx::color::gamma::EncodeSafe(grained_color, 2.2f);
   }
+  return grained_color;
 }
