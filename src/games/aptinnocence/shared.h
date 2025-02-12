ba836c341db0f5ed0ef497036f573db23fcdc68d
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-02-12 23:37:46 +0100

    feat(A Plague Tale Innocence): add game

---
+++ src/games/aptinnocence/shared.h
@@ -0,0 +1,49 @@
+#ifndef SRC_APTINNOCENCE_SHARED_H_
+#define SRC_APTINNOCENCE_SHARED_H_
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float toneMapGammaCorrection;
+  float toneMapPerChannel;
+  float toneMapHueProcessor;
+  float toneMapHueCorrection;
+  float toneMapShoulderStart;
+  float colorGradeExposure;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float colorGradeBlowout;
+  float colorGradeDechroma;
+  float colorGradeFlare;
+  float colorGradeLUTStrength;
+  float colorGradeLUTScaling;
+  float fxChroma;
+  float fxSharpen;
+  float fxBloom;
+  float fxLens;
+  float fxDoF;
+  float fxAutoExposure;
+  float fxVignette;
+  float fxFilmGrain;
+  float fxFilmGrainType;
+
+  float elapsedTime;
+};
+
+#ifndef __cplusplus
+cbuffer cb13 : register(b13) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_APTINNOCENCE_SHARED_H_
