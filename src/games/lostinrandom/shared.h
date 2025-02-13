1b64271f6bc6df05a7d6db38413b579596d556c1
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-02-13 23:46:13 +0100

    lostinrandom

---
+++ src/games/lostinrandom/shared.h
@@ -0,0 +1,49 @@
+#ifndef SRC_LOSTINRANDOM_SHARED_H_
+#define SRC_LOSTINRANDOM_SHARED_H_
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float toneMapGammaCorrection;
+  float toneMapHueProcessor;
+  float toneMapHueCorrection;
+  float toneMapColorSpace;
+  float toneMapPerChannel;
+  float colorGradeExposure;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float colorGradeBlowout;
+  float colorGradeDechroma;
+  float colorGradeFlare;
+  float colorGradeLUTStrength;
+  float colorGradeLUTScaling;
+  float colorGradeLUTSampling;
+  float upgradePerChannel;
+  float fxBloom;
+  float fxVignette;
+  float fxFilmGrain;
+  float fxFilmGrainType;
+
+  float elapsedTime;
+  float random_1;
+  float random_2;
+  float random_3;
+};
+
+#ifndef __cplusplus
+cbuffer cb13 : register(b13) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_LOSTINRANDOM_SHARED_H_
