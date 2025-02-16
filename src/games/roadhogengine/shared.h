878f2815b1ac1b5cd98ea89fb21c506fb220e6d9
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-02-16 13:53:13 +0100

    feat(roadhogengine): add Hard Reset Redux mod

---
+++ src/games/roadhogengine/shared.h
@@ -0,0 +1,45 @@
+#ifndef SRC_ROADHOGENGINE_SHARED_H_
+#define SRC_ROADHOGENGINE_SHARED_H_
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float toneMapGammaCorrection;
+  float toneMapPerChannel;
+  float toneMapHueProcessor;
+  float toneMapHueCorrection;
+  float toneMapShoulderStart;
+  float colorGradeExposure;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float colorGradeBlowout;
+  float colorGradeDechroma;
+  float colorGradeFlare;
+  float fxAutoExposure;
+  float fxBloom;
+  float fxLens;
+  float fxSharpen;
+  float fxVignette;
+  float fxFilmGrain;
+  float fxFilmGrainType;
+
+  float elapsedTime;
+};
+
+#ifndef __cplusplus
+cbuffer cb13 : register(b13) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_ROADHOGENGINE_SHARED_H_
