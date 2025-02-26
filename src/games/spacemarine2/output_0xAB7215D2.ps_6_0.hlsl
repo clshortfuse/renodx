de875494842e6560be155bb9e80c252fe050fcdb
Mohanned Hassan <gailardiagalane@gmail.com>
2025-02-26 13:44:52 +0400

    feat(spacemarine2): Add more shaders, finish up mod

--- src/games/spacemarine2/output_0xAB7215D2.ps_6_0.hlsl
+++ src/games/spacemarine2/output_0xAB7215D2.ps_6_0.hlsl
@@ -1,3 +1,5 @@
+#include "./common.hlsl"
+
 Texture2D<float4> HDR_TEX1 : register(t0, space2);
 
 Texture2D<float4> HDR_TEX2 : register(t1, space2);
@@ -23,7 +25,6 @@ float4 main(
   float3 untonemapped;
 
   float4 _10 = HDR_TEX1.Sample(PS_SAMPLERS[4], float2((TEXCOORD.x), (TEXCOORD.y)));
-  untonemapped = _10.rgb;
 
   float4 _18 = HDR_TEX2.Sample(PS_SAMPLERS[4], float2((TEXCOORD.x), (TEXCOORD.y)));
   float _30 = (CB_PASS_HDR_004z) * ((min((_18.x), 64512.0f)) + (min((_10.x), 64512.0f)));
@@ -47,16 +48,21 @@ float4 main(
       _62 = (_55 * _32);
     }
   }
+  untonemapped = float3(_60, _61, _62);
+
   float _70 = max(0.0f, ((exp2((-0.0f - ((CB_PASS_HDR_008w) * (CB_PASS_HDR_008z))))) * 0.18000000715255737f));
   float4 _96 = HDR_TEX_TONEMAP_LUT.Sample(PS_SAMPLERS[4], float3((((saturate(((CB_PASS_HDR_008w) + (((log2((_70 + _60))) + 2.473931074142456f) / (CB_PASS_HDR_008z))))) * 0.96875f) + 0.015625f), (((saturate(((CB_PASS_HDR_008w) + (((log2((_70 + _61))) + 2.473931074142456f) / (CB_PASS_HDR_008z))))) * 0.96875f) + 0.015625f), (((saturate(((CB_PASS_HDR_008w) + (((log2((_70 + _62))) + 2.473931074142456f) / (CB_PASS_HDR_008z))))) * 0.96875f) + 0.015625f)));
   float _106 = ((frac(((sin((((TEXCOORD.y) * 543.3099975585938f) + (TEXCOORD.x)))) * 493013.0f))) * 2.0f) + -1.0f;
   float _109 = min((max((_106 * +1.#INF), -1.0f)), 1.0f);
   float _116 = (_109 - ((sqrt((saturate((1.0f - (abs(_106))))))) * _109)) * 0.003921568859368563f;
+
+
   SV_Target.x = (_116 + (_96.x));
   SV_Target.y = (_116 + (_96.y));
   SV_Target.z = (_116 + (_96.z));
+  SV_Target.rgb = LutToneMap(untonemapped, SV_Target.rgb);
+
   SV_Target.w = 0.0f;
-  SV_Target.rgb = float3(2, 2, 0);
 
   return SV_Target;
 }
