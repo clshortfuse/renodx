cbd87413721910ec28152e838b70a305d80dbb25
Carlos Lopez <clshortfuse@gmail.com>
2025-03-13 12:55:03 -0400

    fix(shaders::reinhard): fix exposure scaling with reinhard extended/white clip

--- src/shaders/reinhard.hlsl
+++ src/shaders/reinhard.hlsl
@@ -36,20 +36,20 @@ float3 ReinhardScalable(float3 x, float x_max = 1.f, float x_min = 0.f, float gr
 }
 
 float ComputeReinhardExtendableScale(float w = 100.f, float p = 1.f, float m = 0.f, float x = 0.18f, float y = 0.18f) {
-  // y = (sx / (sx + 1) * (1 + (psx)/(sw*sw))
+  // y = (sx / (sx/p + 1) * (1 + (psx)/(sw*sw))
   // solve for s (scale)
   // Min not currently supported
-  return p * (w * w * y - p * x) / (w * w * x * (p - y));
+  return p * (w * w * y - (p * x * x)) / (w * w * x * (p - y));
 }
 
 float ReinhardScalableExtended(float x, float white_max = 100.f, float x_max = 1.f, float x_min = 0.f, float gray_in = 0.18f, float gray_out = 0.18f) {
-  float exposure = ComputeReinhardScale(x_max, x_min, gray_in, gray_out);
+  float exposure = ComputeReinhardExtendableScale(white_max, x_max, x_min, gray_in, gray_out);
   float extended = ReinhardExtended(x * exposure, white_max * exposure, x_max);
   return min(extended, x_max);
 }
 
 float3 ReinhardScalableExtended(float3 x, float white_max = 100.f, float x_max = 1.f, float x_min = 0.f, float gray_in = 0.18f, float gray_out = 0.18f) {
-  float exposure = ComputeReinhardScale(x_max, x_min, gray_in, gray_out);
+  float exposure = ComputeReinhardExtendableScale(white_max, x_max, x_min, gray_in, gray_out);
   float3 extended = ReinhardExtended(x * exposure, white_max * exposure, x_max);
   return min(extended, x_max);
 }
