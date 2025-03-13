1e3c7a30a04e0645c6f87c8c5bacb9beb7a5bd8d
Musa Haji <mqhaji@vt.edu>
2025-03-13 05:14:18 -0400

    feat(maxpayne3): unclamp AA shaders, disable colorgrade sliders with vanilla

--- src/games/maxpayne3/common.hlsl
+++ src/games/maxpayne3/common.hlsl
@@ -25,27 +25,29 @@ float3 GammaEncode(float3 color) {
 float3 ApplyToneMapAndScale(float3 color, bool clamp = false) {
   float3 untonemapped = GammaDecode(max(0, color));
 
-  color = renodx::color::grade::UserColorGrading(
-      untonemapped,
-      RENODX_TONE_MAP_EXPOSURE,
-      RENODX_TONE_MAP_HIGHLIGHTS,
-      RENODX_TONE_MAP_SHADOWS,
-      RENODX_TONE_MAP_CONTRAST,
-      RENODX_TONE_MAP_SATURATION,
-      RENODX_TONE_MAP_BLOWOUT);
+  if (RENODX_TONE_MAP_TYPE == 0.f) {  // Vanilla
+    color = saturate(untonemapped);
+  } else {
+    color = renodx::color::grade::UserColorGrading(
+        untonemapped,
+        RENODX_TONE_MAP_EXPOSURE,
+        RENODX_TONE_MAP_HIGHLIGHTS,
+        RENODX_TONE_MAP_SHADOWS,
+        RENODX_TONE_MAP_CONTRAST,
+        RENODX_TONE_MAP_SATURATION,
+        RENODX_TONE_MAP_BLOWOUT);
 
-  if (RENODX_TONE_MAP_TYPE == 3.f) {  // Frostbite
-    color = renodx::tonemap::frostbite::BT709(
-        color, RENODX_PEAK_WHITE_NITS / RENODX_DIFFUSE_WHITE_NITS, 1.f,
-        RENODX_TONE_MAP_HIGHLIGHT_SATURATION, RENODX_TONE_MAP_HUE_CORRECTION, RENODX_TONE_MAP_HUE_PROCESSOR);
-  } else if (RENODX_TONE_MAP_TYPE == 2.f) {  // Exponential Rolloff
-    color = renodx::tonemap::ExponentialRollOff(color, min(1.f, RENODX_PEAK_WHITE_NITS / RENODX_DIFFUSE_WHITE_NITS), RENODX_PEAK_WHITE_NITS / RENODX_DIFFUSE_WHITE_NITS);
-    color = renodx::color::correct::Hue(color, untonemapped, RENODX_TONE_MAP_HUE_CORRECTION, RENODX_TONE_MAP_HUE_PROCESSOR);
-  } else if (RENODX_TONE_MAP_TYPE == 0.f) {  // Vanilla
-    color = saturate(color);
+    if (RENODX_TONE_MAP_TYPE == 3.f) {  // Frostbite
+      color = renodx::tonemap::frostbite::BT709(
+          color, RENODX_PEAK_WHITE_NITS / RENODX_DIFFUSE_WHITE_NITS, 1.f,
+          RENODX_TONE_MAP_HIGHLIGHT_SATURATION, RENODX_TONE_MAP_HUE_CORRECTION, RENODX_TONE_MAP_HUE_PROCESSOR);
+    } else if (RENODX_TONE_MAP_TYPE == 2.f) {  // Exponential Rolloff
+      color = renodx::tonemap::ExponentialRollOff(color, min(1.f, RENODX_PEAK_WHITE_NITS / RENODX_DIFFUSE_WHITE_NITS), RENODX_PEAK_WHITE_NITS / RENODX_DIFFUSE_WHITE_NITS);
+      color = renodx::color::correct::Hue(color, untonemapped, RENODX_TONE_MAP_HUE_CORRECTION, RENODX_TONE_MAP_HUE_PROCESSOR);
+    }
   }
 
-  if (clamp) color = saturate(color);
+  if (clamp) color = min(1, color);
 
   color *= RENODX_INTERMEDIATE_SCALING;
 
