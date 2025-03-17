ebe883a89c414f6f55ae1803fd215858fdd00504
Musa Haji <mqhaji@vt.edu>
2025-03-16 23:57:37 -0400

    fix(maxpayne3): upgrade only necessary rgba8_typeless

--- src/games/maxpayne3/addon.cpp
+++ src/games/maxpayne3/addon.cpp
@@ -488,9 +488,11 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
           .old_format = reshade::api::format::r8g8b8a8_unorm,
           .new_format = reshade::api::format::r16g16b16a16_float,
       });
+
       renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
           .old_format = reshade::api::format::r8g8b8a8_typeless,
           .new_format = reshade::api::format::r16g16b16a16_float,
+          .index = 6,  // Results in artifacts otherwise
       });
 
       renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
