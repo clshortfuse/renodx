32ef601488a89be58b94e8abdf8536f514260431
Musa Haji <mqhaji@vt.edu>
2025-03-23 19:26:24 -0400

    feat(asscreedorigins): adjust exposure, tonemap, ui brightness

---
+++ src/games/asscreedorigins/shared.h
@@ -0,0 +1,11 @@
+#ifndef SRC_ASSCREEDORIGINS_SHARED_H_
+#define SRC_ASSCREEDORIGINS_SHARED_H_
+
+#define RENODX_TONE_MAP_TYPE         2u
+#define RENODX_GAME_GAMMA_CORRECTION 1u
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+#endif  // SRC_ASSCREEDORIGINS_SHARED_H_
