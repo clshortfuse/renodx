181be9ae8a89905d59d8545c1f15e2d4aa2cfe22
Carlos Lopez <clshortfuse@gmail.com>
2025-03-31 13:25:57 -0400

    feat(unrealengine): add lutbuilder from lies of p

--- src/games/unrealengine/addon.cpp
+++ src/games/unrealengine/addon.cpp
@@ -124,6 +124,7 @@ renodx::mods::shader::CustomShaders custom_shaders = {
     TracedDualShaderEntry(0xF5AC79AB),
     TracedDualShaderEntry(0x40A581ED),
     TracedDualShaderEntry(0x18639D8F),
+    TracedDualShaderEntry(0x95A3F50E),
 
     // SM6 LUT Builder
 
