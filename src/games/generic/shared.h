fc23002937f29295922635287f6028b35763db73
Carlos Lopez <clshortfuse@gmail.com>
2025-04-06 20:35:56 -0400

    feat(generic): adjust hue shift, add scene grading, use reinhard for renodrt

--- src/games/generic/shared.h
+++ src/games/generic/shared.h
@@ -112,6 +112,7 @@ cbuffer shader_injection : register(b13) {
 #define RENODX_SWAP_CHAIN_CLAMP_COLOR_SPACE    shader_injection.swap_chain_clamp_color_space
 #define RENODX_SWAP_CHAIN_ENCODING             shader_injection.swap_chain_encoding
 #define RENODX_SWAP_CHAIN_ENCODING_COLOR_SPACE shader_injection.swap_chain_encoding_color_space
+#define RENODX_RENO_DRT_TONE_MAP_METHOD        renodx::tonemap::renodrt::config::tone_map_method::REINHARD
 
 #include "../../shaders/renodx.hlsl"
 
