126bf657b099df9d9361e9873012da6e648edd3f
Carlos Lopez <clshortfuse@gmail.com>
2025-04-07 15:38:36 -0400

    feat(unrealengine): add crash bandictor lutbuilder with lut

--- src/games/unrealengine/addon.cpp
+++ src/games/unrealengine/addon.cpp
@@ -83,6 +83,7 @@ renodx::mods::shader::CustomShaders custom_shaders = {
     TracedDualShaderEntry(0xB864B3B8),
 
     // SM5 LUT Builder
+    TracedDualShaderEntry(0x1BA80C5E),
     TracedDualShaderEntry(0x1DF6036B),
     TracedDualShaderEntry(0x20EAC9B6),
     TracedDualShaderEntry(0x2A94C68A),
