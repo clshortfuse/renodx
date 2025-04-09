cea47520674f630c72bf87c63b7b6e6174193d60
Carlos Lopez <clshortfuse@gmail.com>
2025-04-09 18:04:20 -0400

    feat(unrealengine): change hue shift method, add reset all, add sdr grading bypass, add prevent fullscreen

--- src/games/unrealengine/addon.cpp
+++ src/games/unrealengine/addon.cpp
@@ -415,9 +415,32 @@ const std::unordered_map<std::string, reshade::api::format> UPGRADE_TARGETS = {
 renodx::utils::settings::Settings info_settings = {
     new renodx::utils::settings::Setting{
         .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "Reset All",
+        .section = "Options",
+        .group = "button-line-1",
+        .on_change = []() { renodx::utils::settings::ResetSettings(); },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "SDR Grading Bypass",
+        .section = "Options",
+        .group = "button-line-1",
+        .tooltip = "Improves highlight appearance in games with little to no SDR grading",
+        .on_change = []() {
+          renodx::utils::settings::ResetSettings();
+          renodx::utils::settings::UpdateSettings({
+              {"ColorGradeContrast", 80.f},
+              {"ColorGradeSaturation", 80.f},
+              {"ColorGradeBlowout", 80.f},
+              {"ColorGradeStrength", 0.f},
+          });
+        },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
         .label = "Discord",
         .section = "Links",
-        .group = "button-line-1",
+        .group = "button-line-2",
         .tint = 0x5865F2,
         .on_change = []() {
           renodx::utils::platform::Launch(
@@ -430,7 +453,7 @@ renodx::utils::settings::Settings info_settings = {
         .value_type = renodx::utils::settings::SettingValueType::BUTTON,
         .label = "More Mods",
         .section = "Links",
-        .group = "button-line-1",
+        .group = "button-line-2",
         .tint = 0x2B3137,
         .on_change = []() {
           renodx::utils::platform::Launch("https://github.com/clshortfuse/renodx/wiki/Mods");
@@ -440,7 +463,7 @@ renodx::utils::settings::Settings info_settings = {
         .value_type = renodx::utils::settings::SettingValueType::BUTTON,
         .label = "Github",
         .section = "Links",
-        .group = "button-line-1",
+        .group = "button-line-2",
         .tint = 0x2B3137,
         .on_change = []() {
           renodx::utils::platform::Launch("https://github.com/clshortfuse/renodx");
@@ -450,7 +473,7 @@ renodx::utils::settings::Settings info_settings = {
         .value_type = renodx::utils::settings::SettingValueType::BUTTON,
         .label = "ShortFuse's Ko-Fi",
         .section = "Links",
-        .group = "button-line-1",
+        .group = "button-line-2",
         .tint = 0xFF5A16,
         .on_change = []() {
           renodx::utils::platform::Launch("https://ko-fi.com/shortfuse");
@@ -505,11 +528,11 @@ bool fired_on_init_swapchain = false;
 
 void OnInitSwapchain(reshade::api::swapchain* swapchain, bool resize) {
   if (fired_on_init_swapchain) return;
-  fired_on_init_swapchain = true;
   auto peak = renodx::utils::swapchain::GetPeakNits(swapchain);
   if (peak.has_value()) {
     settings[2]->default_value = peak.value();
     settings[2]->can_reset = true;
+    fired_on_init_swapchain = true;
   }
 }
 
@@ -768,6 +791,27 @@ void AddAdvancedSettings() {
   }
 
   {
+    auto* setting = new renodx::utils::settings::Setting{
+        .key = "PreventFullscreen",
+        .value_type = renodx::utils::settings::SettingValueType::INTEGER,
+        .default_value = 0.f,
+        .label = "Prevent Fullscreen",
+        .section = "Resource Upgrades",
+        .tooltip = "Prevent exclusive fullscreen for proper HDR",
+        .labels = {
+            "Disabled",
+            "Enabled",
+        },
+        .on_change_value = [](float previous, float current) { renodx::mods::swapchain::prevent_full_screen = (current == 1.f); },
+        .is_global = true,
+        .is_visible = []() { return settings[0]->GetValue() >= 2; },
+    };
+
+    renodx::mods::swapchain::prevent_full_screen = (setting->GetValue() == 1.f);
+    add_setting(setting);
+  }
+
+  {
     auto* lut_dump_setting = new renodx::utils::settings::Setting{
         .key = "DumpLUTShaders",
         .binding = &g_dump_shaders,
@@ -814,16 +858,13 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
         auto process_path = renodx::utils::platform::GetCurrentProcessPath();
         auto product_name = renodx::utils::platform::GetProductName(process_path);
         auto param_count = params.size();
-        if (params.size() >= 20) {
-          return false;
-        }
+        if (params.size() >= 20) return false;
+
         // UE DX12 has a 4 param root sig that crashes if modified. Track for now
         return std::ranges::any_of(params, [](auto param) {
           return (param.type == reshade::api::pipeline_layout_param_type::descriptor_table);
         });
       };
-
-      // while (IsDebuggerPresent() == 0) Sleep(100);
 
       if (!initialized) {
         AddAdvancedSettings();
@@ -860,6 +901,7 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
         renodx::utils::shader::use_shader_cache = true;
         renodx::utils::resource::Use(fdw_reason);
         reshade::register_event<reshade::addon_event::draw>(OnDrawForLUTDump);
+        reshade::log::message(reshade::log::level::info, "DumpLUTShaders enabled.");
       }
 
       break;
