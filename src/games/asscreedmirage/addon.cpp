0a4fbe4526c447f4f7f683415c4d676fb359db76
Musa Haji <mqhaji@vt.edu>
2025-04-16 00:03:56 -0400

    feat(asscreedmirage): add sliders and hue correction

--- src/games/asscreedmirage/addon.cpp
+++ src/games/asscreedmirage/addon.cpp
@@ -16,18 +16,134 @@
 #include "../../mods/shader.hpp"
 #include "../../utils/date.hpp"
 #include "../../utils/settings.hpp"
+#include "shared.h"
 
 namespace {
 
+ShaderInjectData shader_injection;
+
 renodx::mods::shader::CustomShaders custom_shaders = {
-    CustomShaderEntry(0xFE07E3F3),  // ACES ToneMap
+    CustomShaderEntry(0x5EEEC729),  // Sample LUT + Bloom
+    CustomShaderEntry(0xAA7F0DB7),  // Color Grade + ToneMap LutBuilder
+    CustomShaderEntry(0xFE07E3F3),  // ACES ToneMap LutBuilder
 
     CustomShaderEntry(0x0E048C6D),  // UI - sRGB to HDR
-    // CustomShaderEntry(0xAF8A7EAB),  // UI - Video sRGB to HDR
+                                    // CustomShaderEntry(0xAF8A7EAB),  // UI - Video sRGB to HDR
 
 };
 
+const std::unordered_map<std::string, float> HDR_LOOK_VALUES = {
+    {"ColorGradeSaturation", 55.f},
+    {"ColorGradeBlowout", 33.f},
+    {"FxBloom", 50.f},
+};
+
+
 renodx::utils::settings::Settings settings = {
+    new renodx::utils::settings::Setting{
+        .key = "ColorGradeExposure",
+        .binding = &shader_injection.tone_map_exposure,
+        .default_value = 1.f,
+        .label = "Exposure",
+        .section = "Color Grading",
+        .max = 2.f,
+        .format = "%.2f",
+    },
+    new renodx::utils::settings::Setting{
+        .key = "ColorGradeHighlights",
+        .binding = &shader_injection.tone_map_highlights,
+        .default_value = 50.f,
+        .label = "Highlights",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "ColorGradeShadows",
+        .binding = &shader_injection.tone_map_shadows,
+        .default_value = 50.f,
+        .label = "Shadows",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "ColorGradeContrast",
+        .binding = &shader_injection.tone_map_contrast,
+        .default_value = 50.f,
+        .label = "Contrast",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "ColorGradeSaturation",
+        .binding = &shader_injection.tone_map_saturation,
+        .default_value = 50.f,
+        .label = "Saturation",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.02f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "ColorGradeBlowout",
+        .binding = &shader_injection.tone_map_blowout,
+        .default_value = 0.f,
+        .label = "Blowout",
+        .section = "Color Grading",
+        .tooltip = "Controls highlight desaturation due to overexposure.",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.01f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "ColorFilterStrength",
+        .binding = &shader_injection.custom_color_filter_strength,
+        .default_value = 100.f,
+        .label = "Color Filter Strength",
+        .section = "Color Grading",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.01f; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "FxBloom",
+        .binding = &shader_injection.custom_bloom,
+        .default_value = 100.f,
+        .label = "Bloom",
+        .section = "Effects",
+        .max = 100.f,
+        .parse = [](float value) { return value * 0.01f; },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "Reset All",
+        .section = "Options",
+        .group = "button-line-1",
+        .on_change = []() {
+          for (auto* setting : settings) {
+            if (setting->key.empty()) continue;
+            if (!setting->can_reset) continue;
+            renodx::utils::settings::UpdateSetting(setting->key, setting->default_value);
+          }
+        },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "HDR Look",
+        .section = "Options",
+        .group = "button-line-1",
+        .on_change = []() {
+          for (auto* setting : settings) {
+            if (setting->key.empty()) continue;
+            if (!setting->can_reset) continue;
+
+            if (HDR_LOOK_VALUES.contains(setting->key)) {
+              renodx::utils::settings::UpdateSetting(setting->key, HDR_LOOK_VALUES.at(setting->key));
+            } else {
+              renodx::utils::settings::UpdateSetting(setting->key, setting->default_value);
+            }
+          }
+        },
+    },
     new renodx::utils::settings::Setting{
         .value_type = renodx::utils::settings::SettingValueType::BUTTON,
         .label = "Discord",
@@ -82,6 +198,17 @@ renodx::utils::settings::Settings settings = {
     },
 };
 
+void OnPresetOff() {
+  renodx::utils::settings::UpdateSetting("ColorFilterStrength", 100.f);
+  renodx::utils::settings::UpdateSetting("ColorGradeExposure", 1.f);
+  renodx::utils::settings::UpdateSetting("ColorGradeHighlights", 50.f);
+  renodx::utils::settings::UpdateSetting("ColorGradeShadows", 50.f);
+  renodx::utils::settings::UpdateSetting("ColorGradeContrast", 50.f);
+  renodx::utils::settings::UpdateSetting("ColorGradeSaturation", 50.f);
+  renodx::utils::settings::UpdateSetting("ColorGradeBlowout", 0.f);
+  renodx::utils::settings::UpdateSetting("FxBloom", 100.f);
+}
+
 }  // namespace
 
 // NOLINTBEGIN(readability-identifier-naming)
@@ -95,7 +222,8 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
   switch (fdw_reason) {
     case DLL_PROCESS_ATTACH:
       if (!reshade::register_addon(h_module)) return FALSE;
-      renodx::utils::settings::use_presets = false;
+      renodx::mods::shader::expected_constant_buffer_space = 50;
+      renodx::mods::shader::expected_constant_buffer_index = 13;
       renodx::mods::shader::force_pipeline_cloning = true;
       break;
     case DLL_PROCESS_DETACH:
@@ -103,8 +231,8 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
       break;
   }
 
-  renodx::utils::settings::Use(fdw_reason, &settings);
-  renodx::mods::shader::Use(fdw_reason, custom_shaders);
+  renodx::utils::settings::Use(fdw_reason, &settings, &OnPresetOff);
+  renodx::mods::shader::Use(fdw_reason, custom_shaders, &shader_injection);
 
   return TRUE;
 }
--- src/games/asscreedmirage/shared.h
+++ src/games/asscreedmirage/shared.h
@@ -4,8 +4,35 @@
 #define RENODX_GRAPHICS_WHITE_NITS   203.f
 #define RENODX_TONE_MAP_TYPE         2u
 #define RENODX_GAME_GAMMA_CORRECTION 1u
+#define HUE_CORRECTION               1u
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float custom_color_filter_strength;
+  float tone_map_exposure;
+  float tone_map_highlights;
+  float tone_map_shadows;
+  float tone_map_contrast;
+  float tone_map_saturation;
+  float tone_map_blowout;
+  float custom_bloom;
+};
 
 #ifndef __cplusplus
+cbuffer shader_injection : register(b13, space50) {
+  ShaderInjectData shader_injection : packoffset(c0);
+}
+
+#define CUSTOM_COLOR_FILTER_STRENGTH  shader_injection.custom_color_filter_strength
+#define RENODX_TONE_MAP_EXPOSURE      shader_injection.tone_map_exposure
+#define RENODX_TONE_MAP_HIGHLIGHTS    shader_injection.tone_map_highlights
+#define RENODX_TONE_MAP_SHADOWS       shader_injection.tone_map_shadows
+#define RENODX_TONE_MAP_CONTRAST      shader_injection.tone_map_contrast
+#define RENODX_TONE_MAP_SATURATION    shader_injection.tone_map_saturation
+#define RENODX_TONE_MAP_BLOWOUT       shader_injection.tone_map_blowout
+#define CUSTOM_BLOOM                  shader_injection.custom_bloom
+
 #include "../../shaders/renodx.hlsl"
 #endif
 
