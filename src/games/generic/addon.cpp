3f99237baac0f6bb26ff5f537045917d8c6fed9b
Carlos Lopez <clshortfuse@gmail.com>
2025-04-17 15:45:22 -0400

    dev(generic): remove unused manual scheduling option

--- src/games/generic/addon.cpp
+++ src/games/generic/addon.cpp
@@ -424,7 +424,6 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
         renodx::mods::shader::force_pipeline_cloning = true;
         renodx::mods::shader::expected_constant_buffer_space = 50;
         renodx::mods::shader::expected_constant_buffer_index = 13;
-        renodx::mods::shader::manual_shader_scheduling = true;
         renodx::mods::shader::allow_multiple_push_constants = true;
 
         renodx::mods::swapchain::expected_constant_buffer_index = 13;
