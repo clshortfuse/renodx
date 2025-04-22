dedba9f13d97d6c907660bb1184a8f3d53279f27
Mohanned Hassan <gailardiagalane@gmail.com>
2025-04-22 17:21:15 +0400

    feat(kunitsugami): Add game

---
+++ src/games/kunitsugami/common.hlsl
@@ -0,0 +1,19 @@
+#include "./shared.h"
+
+float3 Tonemap(float3 untonemapped, float3 graded) {
+  float3 color = graded;
+  if (RENODX_TONE_MAP_TYPE > 0.f) {
+    color = renodx::draw::ToneMapPass(untonemapped, color);
+  }
+
+  color = renodx::draw::RenderIntermediatePass(color);
+  return color;
+}
+
+float3 FinalOutput(float3 final_image) {
+  final_image = renodx::draw::SwapChainPass(final_image);
+  final_image = renodx::color::correct::GammaSafe(final_image);
+  final_image = renodx::color::bt2020::from::BT709(final_image);
+  final_image = renodx::color::pq::EncodeSafe(final_image, 80.f);
+  return final_image;
+}
