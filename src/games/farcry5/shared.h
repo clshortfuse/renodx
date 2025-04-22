4b88ab3377e4062362775804abc6dc7e8712001f
Musa Haji <mqhaji@vt.edu>
2025-04-21 21:49:52 -0400

    feat(farcry5): add UI and video shaders

--- src/games/farcry5/shared.h
+++ src/games/farcry5/shared.h
@@ -1,19 +1,19 @@
 #ifndef SRC_FARCRY5_SHARED_H
 #define SRC_FARCRY5_SHARED_H
 
-#define RENODX_TONE_MAP_TYPE       2u
-#define RENODX_PEAK_WHITE_NITS     400.f
-#define RENODX_DIFFUSE_WHITE_NITS  100.f
-#define RENODX_GRAPHICS_WHITE_NITS 100.f
-#define RENODX_GAMMA_CORRECTION    1u
-#define RENODX_TONE_MAP_EXPOSURE   1.f
-#define RENODX_TONE_MAP_HIGHLIGHTS 1.f
-#define RENODX_TONE_MAP_SHADOWS    1.f
-#define RENODX_TONE_MAP_CONTRAST   1.f
-#define RENODX_TONE_MAP_SATURATION 1.f
-#define RENODX_TONE_MAP_BLOWOUT    0.f
-#define RENODX_COLOR_GRADE_SCALING 1.f
-#define CUSTOM_BLOOM               1.f
+// #define RENODX_TONE_MAP_TYPE       2u
+// #define RENODX_PEAK_WHITE_NITS     400.f
+// #define RENODX_DIFFUSE_WHITE_NITS  100.f
+// #define RENODX_GRAPHICS_WHITE_NITS 100.f
+// #define RENODX_GAMMA_CORRECTION    1u
+// #define RENODX_TONE_MAP_EXPOSURE   1.f
+// #define RENODX_TONE_MAP_HIGHLIGHTS 1.f
+// #define RENODX_TONE_MAP_SHADOWS    1.f
+// #define RENODX_TONE_MAP_CONTRAST   1.f
+// #define RENODX_TONE_MAP_SATURATION 1.f
+// #define RENODX_TONE_MAP_BLOWOUT    0.f
+// #define RENODX_COLOR_GRADE_SCALING 1.f
+// #define CUSTOM_BLOOM               1.f
 
 // Must be 32bit aligned
 // Should be 4x32
@@ -38,19 +38,19 @@ cbuffer cb13 : register(b13) {
   ShaderInjectData shader_injection : packoffset(c0);
 }
 
-// #define RENODX_TONE_MAP_TYPE       shader_injection.tone_map_type
-// #define RENODX_PEAK_WHITE_NITS     shader_injection.peak_white_nits
-// #define RENODX_DIFFUSE_WHITE_NITS  shader_injection.diffuse_white_nits
-// #define RENODX_GRAPHICS_WHITE_NITS shader_injection.graphics_white_nits
-// #define RENODX_GAMMA_CORRECTION    shader_injection.gamma_correction
-// #define RENODX_TONE_MAP_EXPOSURE   shader_injection.tone_map_exposure
-// #define RENODX_TONE_MAP_HIGHLIGHTS shader_injection.tone_map_highlights
-// #define RENODX_TONE_MAP_SHADOWS    shader_injection.tone_map_shadows
-// #define RENODX_TONE_MAP_CONTRAST   shader_injection.tone_map_contrast
-// #define RENODX_TONE_MAP_SATURATION shader_injection.tone_map_saturation
-// #define RENODX_TONE_MAP_BLOWOUT    shader_injection.tone_map_blowout
-// #define RENODX_COLOR_GRADE_SCALING shader_injection.color_grade_scaling
-// #define CUSTOM_BLOOM               shader_injection.custom_bloom
+#define RENODX_TONE_MAP_TYPE       shader_injection.tone_map_type
+#define RENODX_PEAK_WHITE_NITS     shader_injection.peak_white_nits
+#define RENODX_DIFFUSE_WHITE_NITS  shader_injection.diffuse_white_nits
+#define RENODX_GRAPHICS_WHITE_NITS shader_injection.graphics_white_nits
+#define RENODX_GAMMA_CORRECTION    shader_injection.gamma_correction
+#define RENODX_TONE_MAP_EXPOSURE   shader_injection.tone_map_exposure
+#define RENODX_TONE_MAP_HIGHLIGHTS shader_injection.tone_map_highlights
+#define RENODX_TONE_MAP_SHADOWS    shader_injection.tone_map_shadows
+#define RENODX_TONE_MAP_CONTRAST   shader_injection.tone_map_contrast
+#define RENODX_TONE_MAP_SATURATION shader_injection.tone_map_saturation
+#define RENODX_TONE_MAP_BLOWOUT    shader_injection.tone_map_blowout
+#define RENODX_COLOR_GRADE_SCALING shader_injection.color_grade_scaling
+#define CUSTOM_BLOOM               shader_injection.custom_bloom
 
 #include "../../shaders/renodx.hlsl"
 
--- src/games/farcry5/uishaders/ui_map_0xF6BE0A1D.ps_5_0.hlsl
+++ src/games/farcry5/uishaders/ui_map_0xF6BE0A1D.ps_5_0.hlsl
@@ -1,8 +1,7 @@
 #include "../shared.h"
 // ---- Created with 3Dmigoto v1.4.1 on Mon Apr 21 20:57:15 2025
 
-cbuffer CGlobalShaderParameterProvider : register(b0)
-{
+cbuffer CGlobalShaderParameterProvider : register(b0) {
   float4 BurnColor : packoffset(c0);
   float4 BurnParams : packoffset(c1);
   float4 BurnParams2 : packoffset(c2);
@@ -44,8 +43,7 @@ cbuffer CGlobalShaderParameterProvider : register(b0)
   bool Isolate3 : packoffset(c19);
 }
 
-cbuffer CSceneFireUiPrimitiveParameterProvider : register(b1)
-{
+cbuffer CSceneFireUiPrimitiveParameterProvider : register(b1) {
   float DistanceFieldFloatArray[12] : packoffset(c0);
   float BordersSizeFloatArray[8] : packoffset(c12);
   float4x4 Transform : packoffset(c20);
@@ -75,19 +73,16 @@ SamplerState Clamp_s : register(s1);
 SamplerState Mirror_s : register(s2);
 Texture2D<float4> DiffuseSampler0 : register(t0);
 
-
 // 3Dmigoto declarations
 #define cmp -
 
-
 void main(
-  float4 v0 : SV_Position0,
-  float4 v1 : TEXCOORD0,
-  float4 v2 : TEXCOORD1,
-  uint v3 : SV_IsFrontFace0,
-  out float4 o0 : SV_Target0)
-{
-  float4 r0,r1,r2,r3;
+    float4 v0: SV_Position0,
+    float4 v1: TEXCOORD0,
+    float4 v2: TEXCOORD1,
+    uint v3: SV_IsFrontFace0,
+    out float4 o0: SV_Target0) {
+  float4 r0, r1, r2, r3;
   uint4 bitmask, uiDest;
   float4 fDest;
 
@@ -109,15 +104,15 @@ void main(
     }
   }
   r1.xyzw = v1.xyzw * r0.xyzw;
-  r0.xyz = cmp(float3(0.0404499993,0.0404499993,0.0404499993) >= r1.xyz);
-  r2.xyz = float3(0.0773993805,0.0773993805,0.0773993805) * r1.xyz;
-  r3.xyz = float3(0.0549999997,0.0549999997,0.0549999997) + abs(r1.xyz);
-  r3.xyz = float3(0.947867334,0.947867334,0.947867334) * r3.xyz;
+  r0.xyz = cmp(float3(0.0404499993, 0.0404499993, 0.0404499993) >= r1.xyz);
+  r2.xyz = float3(0.0773993805, 0.0773993805, 0.0773993805) * r1.xyz;
+  r3.xyz = float3(0.0549999997, 0.0549999997, 0.0549999997) + abs(r1.xyz);
+  r3.xyz = float3(0.947867334, 0.947867334, 0.947867334) * r3.xyz;
   r3.xyz = log2(r3.xyz);
-  r3.xyz = float3(2.4000001,2.4000001,2.4000001) * r3.xyz;
+  r3.xyz = float3(2.4000001, 2.4000001, 2.4000001) * r3.xyz;
   r3.xyz = exp2(r3.xyz);
   r0.xyz = r0.xyz ? r2.xyz : r3.xyz;
-  r2.x = dot(r0.xyz, float3(0.298999995,0.587000012,0.114));
+  r2.x = dot(r0.xyz, float3(0.298999995, 0.587000012, 0.114));
   r2.xyz = r2.xxx + -r0.xyz;
   r1.xyz = Desaturation * r2.xyz + r0.xyz;
   r2.xyzw = ColorMultiplier.xyzw * r1.xyzw;
@@ -128,6 +123,9 @@ void main(
   r1.x = ColorTransparent.w * r1.x + 1;
   r1.yzw = -ColorTransparent.xyz + r0.xyz;
   r1.xyz = r1.xxx * r1.yzw + ColorTransparent.xyz;
+
+  if (RENODX_TONE_MAP_TYPE != 0 && RENODX_GAMMA_CORRECTION) r1.rgb = renodx::color::correct::GammaSafe(r1.rgb);
+
   if (SCRGB != 0) {
     if (RENODX_TONE_MAP_TYPE == 0.f) {
       r1.w = 0.0125000002 * HDRReferenceWhiteNits;
@@ -137,34 +135,34 @@ void main(
     r0.xyz = r1.xyz * r1.www;
 
   } else {
-    r2.x = dot(r1.xyz, float3(0.753832996,0.198596999,0.047569599));
-    r2.y = dot(r1.xyz, float3(0.0457439013,0.941776991,0.0124787996));
-    r2.z = dot(r1.xyz, float3(-0.00121032004,0.0176017992,0.983609021));
+    r2.x = dot(r1.xyz, float3(0.753832996, 0.198596999, 0.047569599));
+    r2.y = dot(r1.xyz, float3(0.0457439013, 0.941776991, 0.0124787996));
+    r2.z = dot(r1.xyz, float3(-0.00121032004, 0.0176017992, 0.983609021));
     r1.xyz = max(float3(0, 0, 0), r2.xyz);
     if (RENODX_TONE_MAP_TYPE == 0.f) {
       r1.xyz = log2(r1.xyz);
-      r1.xyz = r1.xyz * float3(0.0588235296,0.0588235296,0.0588235296) + float3(0.527878284,0.527878284,0.527878284);
-      r1.xyz = r1.xyz * float3(-3,-3,-3) + float3(1.14705884,1.14705884,1.14705884);
+      r1.xyz = r1.xyz * float3(0.0588235296, 0.0588235296, 0.0588235296) + float3(0.527878284, 0.527878284, 0.527878284);
+      r1.xyz = r1.xyz * float3(-3, -3, -3) + float3(1.14705884, 1.14705884, 1.14705884);
       r1.xyz = exp2(r1.xyz);
-      r1.xyz = float3(1,1,1) + r1.xyz;
+      r1.xyz = float3(1, 1, 1) + r1.xyz;
       r1.xyz = rcp(r1.xyz);
-      r1.xyz = r1.xyz * float3(2.11813974,2.11813974,2.11813974) + float3(-0.658908367,-0.658908367,-0.658908367);
-      r1.xyz = r1.xyz * float3(17,17,17) + float3(-8.97393131,-8.97393131,-8.97393131);
+      r1.xyz = r1.xyz * float3(2.11813974, 2.11813974, 2.11813974) + float3(-0.658908367, -0.658908367, -0.658908367);
+      r1.xyz = r1.xyz * float3(17, 17, 17) + float3(-8.97393131, -8.97393131, -8.97393131);
       r1.xyz = exp2(r1.xyz);
       r1.xyz = HDRReferenceWhiteNits * r1.xyz;
     } else {
       r1.rgb *= RENODX_GRAPHICS_WHITE_NITS;
     }
-    r1.xyz = float3(9.99999975e-05,9.99999975e-05,9.99999975e-05) * r1.xyz;
+    r1.xyz = float3(9.99999975e-05, 9.99999975e-05, 9.99999975e-05) * r1.xyz;
     r1.xyz = log2(abs(r1.xyz));
-    r1.xyz = float3(0.159301758,0.159301758,0.159301758) * r1.xyz;
+    r1.xyz = float3(0.159301758, 0.159301758, 0.159301758) * r1.xyz;
     r1.xyz = exp2(r1.xyz);
-    r2.xyzw = r1.xxyy * float4(18.8515625,18.6875,18.8515625,18.6875) + float4(0.8359375,1,0.8359375,1);
+    r2.xyzw = r1.xxyy * float4(18.8515625, 18.6875, 18.8515625, 18.6875) + float4(0.8359375, 1, 0.8359375, 1);
     r1.xy = r2.xz / r2.yw;
     r1.xy = log2(r1.xy);
-    r1.xy = float2(78.84375,78.84375) * r1.xy;
+    r1.xy = float2(78.84375, 78.84375) * r1.xy;
     r0.xy = exp2(r1.xy);
-    r1.xy = r1.zz * float2(18.8515625,18.6875) + float2(0.8359375,1);
+    r1.xy = r1.zz * float2(18.8515625, 18.6875) + float2(0.8359375, 1);
     r1.x = r1.x / r1.y;
     r1.x = log2(r1.x);
     r1.x = 78.84375 * r1.x;
