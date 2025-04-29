6ca3adb929b41de93b2cbe75717883cf7960682d
Carlos Lopez <clshortfuse@gmail.com>
2025-04-29 16:00:36 -0400

    dev(shaders): rename UpgradeToneMap variables

--- src/shaders/tonemap.hlsl
+++ src/shaders/tonemap.hlsl
@@ -203,36 +203,36 @@ struct Config {
 };
 
 float3 UpgradeToneMap(
-    float3 color_hdr,
-    float3 color_sdr,
-    float3 post_process_color,
+    float3 color_untonemapped,
+    float3 color_tonemapped,
+    float3 color_tonemapped_graded,
     float post_process_strength = 1.f,
     float auto_correction = 0.f) {
   float ratio = 1.f;
 
-  float y_hdr = renodx::color::y::from::BT709(abs(color_hdr));
-  float y_sdr = renodx::color::y::from::BT709(abs(color_sdr));
-  float y_post_process = renodx::color::y::from::BT709(abs(post_process_color));
+  float y_untonemapped = renodx::color::y::from::BT709(abs(color_untonemapped));
+  float y_tonemapped = renodx::color::y::from::BT709(abs(color_tonemapped));
+  float y_tonemapped_graded = renodx::color::y::from::BT709(abs(color_tonemapped_graded));
 
-  if (y_hdr < y_sdr) {
+  if (y_untonemapped < y_tonemapped) {
     // If substracting (user contrast or paperwhite) scale down instead
     // Should only apply on mismatched HDR
-    ratio = y_hdr / y_sdr;
+    ratio = y_untonemapped / y_tonemapped;
   } else {
-    float y_delta = y_hdr - y_sdr;
+    float y_delta = y_untonemapped - y_tonemapped;
     y_delta = max(0, y_delta);  // Cleans up NaN
-    const float y_new = y_post_process + y_delta;
+    const float y_new = y_tonemapped_graded + y_delta;
 
-    const bool y_valid = (y_post_process > 0);  // Cleans up NaN and ignore black
-    ratio = y_valid ? (y_new / y_post_process) : 0;
+    const bool y_valid = (y_tonemapped_graded > 0);  // Cleans up NaN and ignore black
+    ratio = y_valid ? (y_new / y_tonemapped_graded) : 0;
   }
-  float auto_correct_ratio = lerp(1.f, ratio, saturate(y_hdr));
+  float auto_correct_ratio = lerp(1.f, ratio, saturate(y_untonemapped));
   ratio = lerp(ratio, auto_correct_ratio, auto_correction);
 
-  float3 color_scaled = post_process_color * ratio;
+  float3 color_scaled = color_tonemapped_graded * ratio;
   // Match hue
-  color_scaled = renodx::color::correct::Hue(color_scaled, post_process_color);
-  return lerp(color_hdr, color_scaled, post_process_strength);
+  color_scaled = renodx::color::correct::Hue(color_scaled, color_tonemapped_graded);
+  return lerp(color_untonemapped, color_scaled, post_process_strength);
 }
 
 namespace config {
