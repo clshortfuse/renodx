354e908617e17c8f2abbfcefa2c73aec733488b8
Musa Haji <mqhaji@vt.edu>
2025-05-16 18:28:55 -0400

    deadisland2: initial commit
    - add lutbuilder with Vanilla, None, ACES, SDR
    - add bloom
    - add output
    - add composite

---
+++ src/games/deadisland2/addon.cpp
@@ -0,0 +1,297 @@
+/*
+ * Copyright (C) 2024 Carlos Lopez
+ * SPDX-License-Identifier: MIT
+ */
+
+#define ImTextureID ImU64
+
+#define DEBUG_LEVEL_0
+
+#include <deps/imgui/imgui.h>
+#include <include/reshade.hpp>
+
+#include <embed/shaders.h>
+
+#include "../../mods/shader.hpp"
+#include "../../utils/date.hpp"
+#include "../../utils/settings.hpp"
+#include "./shared.h"
+
+
+namespace {
+
+renodx::mods::shader::CustomShaders custom_shaders = {__ALL_CUSTOM_SHADERS};
+
+
+renodx::utils::settings::Setting* CreateDefault0PercentSetting(const renodx::utils::settings::Setting& setting) {
+  auto* new_setting = new renodx::utils::settings::Setting(setting);
+  new_setting->default_value = 0.f;
+  new_setting->parse = [](float value) { return value * 0.01f; };
+  return new_setting;
+}
+
+renodx::utils::settings::Setting* CreateDefault50PercentSetting(const renodx::utils::settings::Setting& setting) {
+  auto* new_setting = new renodx::utils::settings::Setting(setting);
+  new_setting->default_value = 50.f;
+  new_setting->parse = [](float value) { return value * 0.02f; };
+  return new_setting;
+}
+
+renodx::utils::settings::Setting* CreateDefault100PercentSetting(const renodx::utils::settings::Setting& setting) {
+  auto* new_setting = new renodx::utils::settings::Setting(setting);
+  new_setting->default_value = 100.f;
+  new_setting->parse = [](float value) { return value * 0.01f; };
+  return new_setting;
+}
+
+renodx::utils::settings::Setting* CreateMultiLabelSetting(const renodx::utils::settings::Setting& setting) {
+  auto* new_setting = new renodx::utils::settings::Setting(setting);
+  new_setting->value_type = renodx::utils::settings::SettingValueType::INTEGER;
+  return new_setting;
+}
+
+ShaderInjectData shader_injection;
+
+float current_settings_mode = 0;
+
+renodx::utils::settings::Settings settings = {
+    CreateMultiLabelSetting({
+        .key = "SettingsMode",
+        .binding = &current_settings_mode,
+        .default_value = 0.f,
+        .can_reset = false,
+        .label = "Settings Mode",
+        .labels = {"Simple", "Intermediate", "Advanced"},
+        .is_global = true,
+    }),
+    CreateMultiLabelSetting({
+        .key = "ToneMapType",
+        .binding = &shader_injection.tone_map_type,
+        .default_value = 2.f,
+        .label = "Tone Mapper",
+        .section = "Tone Mapping",
+        .tooltip = "Sets the tone mapper type",
+        .labels = {"Vanilla", "None", "ACES", "SDR"},
+        .is_visible = []() { return current_settings_mode >= 1; },
+    }),
+    new renodx::utils::settings::Setting{
+        .key = "ToneMapPeakNits",
+        .binding = &shader_injection.peak_white_nits,
+        .default_value = 1000.f,
+        .label = "Peak Brightness",
+        .section = "Tone Mapping",
+        .tooltip = "Sets the value of peak white in nits",
+        .min = 48.f,
+        .max = 4000.f,
+        .is_enabled = []() { return shader_injection.tone_map_type >= 1; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "ToneMapGameNits",
+        .binding = &shader_injection.diffuse_white_nits,
+        .default_value = 203.f,
+        .label = "Game Brightness",
+        .section = "Tone Mapping",
+        .tooltip = "Sets the value of 100% white in nits",
+        .min = 48.f,
+        .max = 500.f,
+        .is_enabled = []() { return shader_injection.tone_map_type >= 1; },
+    },
+    new renodx::utils::settings::Setting{
+        .key = "ToneMapUINits",
+        .binding = &shader_injection.graphics_white_nits,
+        .default_value = 203.f,
+        .label = "UI Brightness",
+        .section = "Tone Mapping",
+        .tooltip = "Sets the brightness of UI and HUD elements in nits",
+        .min = 48.f,
+        .max = 500.f,
+    },
+    CreateDefault0PercentSetting({
+        .key = "toneMapHueCorrection",
+        .binding = &shader_injection.tone_map_hue_correction,
+        .label = "Hue Correction",
+        .section = "Tone Mapping",
+        .tooltip = "Hue retention strength.",
+        .is_enabled = []() { return shader_injection.tone_map_type > 1.f; },
+        .is_visible = []() { return current_settings_mode >= 2; },
+    }),
+    CreateMultiLabelSetting({
+        .key = "GammaCorrection",
+        .binding = &shader_injection.gamma_correction,
+        .default_value = 1.f,
+        .label = "Gamma Correction",
+        .section = "Tone Mapping",
+        .tooltip = "Emulates a display EOTF.",
+        .labels = {"Off (sRGB)", "2.2"},
+        .is_visible = []() { return current_settings_mode >= 1; },
+    }),
+    new renodx::utils::settings::Setting{
+        .key = "ColorGradeExposure",
+        .binding = &shader_injection.tone_map_exposure,
+        .default_value = 1.f,
+        .label = "Exposure",
+        .section = "Color Grading",
+        .max = 2.f,
+        .format = "%.2f",
+        .is_enabled = []() { return shader_injection.tone_map_type > 0; },
+        .is_visible = []() { return current_settings_mode >= 1; },
+    },
+    CreateDefault50PercentSetting({
+        .key = "ColorGradeHighlights",
+        .binding = &shader_injection.tone_map_highlights,
+        .label = "Highlights",
+        .section = "Color Grading",
+        .is_enabled = []() { return shader_injection.tone_map_type > 0; },
+        .is_visible = []() { return current_settings_mode >= 1; },
+    }),
+    CreateDefault50PercentSetting({
+        .key = "ColorGradeShadows",
+        .binding = &shader_injection.tone_map_shadows,
+        .label = "Shadows",
+        .section = "Color Grading",
+        .is_enabled = []() { return shader_injection.tone_map_type > 0; },
+        .is_visible = []() { return current_settings_mode >= 1; },
+    }),
+    CreateDefault50PercentSetting({
+        .key = "ColorGradeContrast",
+        .binding = &shader_injection.tone_map_contrast,
+        .label = "Contrast",
+        .section = "Color Grading",
+        .is_enabled = []() { return shader_injection.tone_map_type > 0; },
+    }),
+    CreateDefault50PercentSetting({
+        .key = "ColorGradeSaturation",
+        .binding = &shader_injection.tone_map_saturation,
+        .label = "Saturation",
+        .section = "Color Grading",
+        .is_enabled = []() { return shader_injection.tone_map_type > 0; },
+    }),
+    CreateDefault0PercentSetting({
+        .key = "ColorGradeBlowout",
+        .binding = &shader_injection.tone_map_blowout,
+        .label = "Blowout",
+        .section = "Color Grading",
+        .tooltip = "Controls highlight desaturation due to overexposure.",
+        .is_enabled = []() { return shader_injection.tone_map_type > 0; },
+    }),
+    CreateDefault100PercentSetting({
+        .key = "FxBloom",
+        .binding = &shader_injection.custom_bloom,
+        .label = "Bloom",
+        .section = "Effects",
+    }),
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "Reset All",
+        .section = "Options",
+        .group = "button-line-1",
+        .on_change = []() {
+          for (auto* setting : settings) {
+            if (setting->key.empty()) continue;
+            if (!setting->can_reset) continue;
+            renodx::utils::settings::UpdateSetting(setting->key, setting->default_value);
+          }
+        },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "Discord",
+        .section = "Links",
+        .group = "button-line-2",
+        .tint = 0x5865F2,
+        .on_change = []() {
+          renodx::utils::platform::LaunchURL("https://discord.gg/", "5WZXDpmbpP");
+        },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "More Mods",
+        .section = "Links",
+        .group = "button-line-2",
+        .tint = 0x2B3137,
+        .on_change = []() {
+          renodx::utils::platform::LaunchURL("https://github.com/clshortfuse/renodx/wiki/Mods");
+        },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "Github",
+        .section = "Links",
+        .group = "button-line-2",
+        .tint = 0x2B3137,
+        .on_change = []() {
+          renodx::utils::platform::LaunchURL("https://github.com/clshortfuse/renodx");
+        },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+        .label = "Musa's Ko-Fi",
+        .section = "Links",
+        .group = "button-line-3",
+        .tint = 0xFF5A16,
+        .on_change = []() { renodx::utils::platform::LaunchURL("https://ko-fi.com/musaqh"); },
+    },
+    new renodx::utils::settings::Setting{
+        .value_type = renodx::utils::settings::SettingValueType::TEXT,
+        .label = std::string("Build: ") + renodx::utils::date::ISO_DATE_TIME,
+        .section = "About",
+    },
+};
+
+void OnPresetOff() {
+  renodx::utils::settings::UpdateSettings({
+      {"ToneMapType", 0.f},
+      {"ToneMapPeakNits", 203.f},
+      {"ToneMapGameNits", 203.f},
+      {"ToneMapUINits", 203.f},
+      {"GammaCorrection", 0.f},
+      {"ToneMapHueCorrection", 0.f},
+      {"ColorGradeExposure", 1.f},
+      {"ColorGradeHighlights", 50.f},
+      {"ColorGradeShadows", 50.f},
+      {"ColorGradeContrast", 50.f},
+      {"ColorGradeSaturation", 50.f},
+      {"ColorGradeBlowout", 0.f},
+      {"FxBloom", 100.f},
+  });
+}
+
+bool fired_on_init_swapchain = false;
+
+void OnInitSwapchain(reshade::api::swapchain* swapchain, bool resize) {
+  if (fired_on_init_swapchain) return;
+  fired_on_init_swapchain = true;
+  auto peak = renodx::utils::swapchain::GetPeakNits(swapchain);
+  if (peak.has_value()) {
+    settings[2]->default_value = peak.value();
+    settings[2]->can_reset = true;
+  }
+}
+
+}  // namespace
+
+extern "C" __declspec(dllexport) constexpr const char* NAME = "RenoDX";
+extern "C" __declspec(dllexport) constexpr const char* DESCRIPTION = "RenoDX for Dead Island 2";
+
+BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
+  switch (fdw_reason) {
+    case DLL_PROCESS_ATTACH:
+      if (!reshade::register_addon(h_module)) return FALSE;
+
+      renodx::mods::shader::force_pipeline_cloning = true;
+      renodx::mods::shader::expected_constant_buffer_index = 13;
+
+      reshade::register_event<reshade::addon_event::init_swapchain>(OnInitSwapchain);  // auto detect peak and paper white
+
+      break;
+    case DLL_PROCESS_DETACH:
+      reshade::unregister_event<reshade::addon_event::init_swapchain>(OnInitSwapchain);  // auto detect peak and paper white
+      reshade::unregister_addon(h_module);
+      break;
+  }
+
+  renodx::utils::settings::Use(fdw_reason, &settings, &OnPresetOff);
+  renodx::mods::shader::Use(fdw_reason, custom_shaders, &shader_injection);
+
+  return TRUE;
+}
---
+++ src/games/deadisland2/shared.h
@@ -0,0 +1,60 @@
+#ifndef SRC_DEADISLAND2_SHARED_H_
+#define SRC_DEADISLAND2_SHARED_H_
+
+// #define RENODX_TONE_MAP_TYPE           2.f
+// #define RENODX_PEAK_WHITE_NITS         400.f
+// #define RENODX_DIFFUSE_WHITE_NITS      100.f
+// #define RENODX_GRAPHICS_WHITE_NITS     100.f
+// #define RENODX_GAMMA_CORRECTION        1.f
+// #define RENODX_TONE_MAP_HUE_CORRECTION 0.f
+// #define RENODX_TONE_MAP_EXPOSURE       1.f
+// #define RENODX_TONE_MAP_HIGHLIGHTS     1.f
+// #define RENODX_TONE_MAP_SHADOWS        1.f
+// #define RENODX_TONE_MAP_CONTRAST       1.f
+// #define RENODX_TONE_MAP_SATURATION     1.f
+// #define RENODX_TONE_MAP_BLOWOUT        0.f
+// #define CUSTOM_BLOOM                   1.f
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float peak_white_nits;
+  float diffuse_white_nits;
+  float graphics_white_nits;
+  float tone_map_type;
+  float tone_map_exposure;
+  float tone_map_highlights;
+  float tone_map_shadows;
+  float tone_map_contrast;
+  float tone_map_saturation;
+  float tone_map_blowout;
+  float tone_map_hue_correction;
+  float gamma_correction;
+
+  float custom_bloom;
+};
+
+#ifndef __cplusplus
+cbuffer shader_injection : register(b13) {
+  ShaderInjectData shader_injection : packoffset(c0);
+}
+
+#define RENODX_TONE_MAP_TYPE           shader_injection.tone_map_type
+#define RENODX_PEAK_WHITE_NITS         shader_injection.peak_white_nits
+#define RENODX_DIFFUSE_WHITE_NITS      shader_injection.diffuse_white_nits
+#define RENODX_GRAPHICS_WHITE_NITS     shader_injection.graphics_white_nits
+#define RENODX_GAMMA_CORRECTION        shader_injection.gamma_correction
+#define RENODX_TONE_MAP_HUE_CORRECTION shader_injection.tone_map_hue_correction
+#define RENODX_TONE_MAP_EXPOSURE       shader_injection.tone_map_exposure
+#define RENODX_TONE_MAP_HIGHLIGHTS     shader_injection.tone_map_highlights
+#define RENODX_TONE_MAP_SHADOWS        shader_injection.tone_map_shadows
+#define RENODX_TONE_MAP_CONTRAST       shader_injection.tone_map_contrast
+#define RENODX_TONE_MAP_SATURATION     shader_injection.tone_map_saturation
+#define RENODX_TONE_MAP_BLOWOUT        shader_injection.tone_map_blowout
+#define CUSTOM_BLOOM                   shader_injection.custom_bloom
+
+#include "../../shaders/renodx.hlsl"
+
+#endif
+
+#endif  // SRC_DEADISLAND2_SHARED_H_
