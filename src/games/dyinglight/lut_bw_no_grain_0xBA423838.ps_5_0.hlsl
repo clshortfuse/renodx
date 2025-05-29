160aecc4e8e4af706a4d1db10996527736a6f42b
Musa Haji <mqhaji@vt.edu>
2025-05-29 03:17:42 -0400

    feat(dyinglight): replace `renoDRTSmoothClamp()` with `ToneMapMaxCLL()`

--- src/games/dyinglight/lut_bw_no_grain_0xBA423838.ps_5_0.hlsl
+++ src/games/dyinglight/lut_bw_no_grain_0xBA423838.ps_5_0.hlsl
@@ -63,23 +63,22 @@ void main(
   float3 outputColor = r0.xyz;
   float3 hdrColor = outputColor;
   float3 sdrColor = outputColor;
-  const float vanillaMidGray = 0.178f;
   if (RENODX_TONE_MAP_TYPE == 2) {
     hdrColor = outputColor;
-    sdrColor = renoDRTSmoothClamp(outputColor);
+    sdrColor = ToneMapMaxCLL(outputColor);
   }
   r0.xyz = sdrColor;
 
   // LUT
-  r0.xyz = sign(r0.xyz) * pow(abs(r0.xyz), 1.f / 2.2f );
+  r0.xyz = renodx::color::gamma::Encode(saturate(r0.rgb), 2.2f);
   r0.xyz = r0.xyz * float3(0.99609375,0.99609375,0.99609375) + float3(0.001953125,0.001953125,0.001953125);
   r1.x = t2.Sample(s2_s, r0.xx).x;
   r1.y = t2.Sample(s2_s, r0.yy).y;
   r1.z = t2.Sample(s2_s, r0.zz).z;
-  r0.xyz = sign(r1.xyz) * pow(abs(r1.xyz), 2.2f );
+  r0.xyz = renodx::color::gamma::Decode(r1.rgb, 2.2f);
 
   if (RENODX_TONE_MAP_TYPE > 1) {
-    outputColor = renodx::tonemap::UpgradeToneMap(hdrColor, saturate(sdrColor), r0.xyz, RENODX_COLOR_GRADE_STRENGTH);
+    outputColor = renodx::tonemap::UpgradeToneMap(hdrColor, sdrColor, r0.xyz, RENODX_COLOR_GRADE_STRENGTH);
   } else if (RENODX_TONE_MAP_TYPE == 0) {
     outputColor = lerp(outputColor, r0.xyz, RENODX_COLOR_GRADE_STRENGTH);
   }
