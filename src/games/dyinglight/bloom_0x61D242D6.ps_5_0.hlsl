b815d7c324660ff8f62099a8fff8b03c0a6dbe48
Musa Haji <mqhaji@vt.edu>
2025-05-28 23:48:05 -0400

    feat(dyinglight): adjust bloom behavior

--- src/games/dyinglight/bloom_0x61D242D6.ps_5_0.hlsl
+++ src/games/dyinglight/bloom_0x61D242D6.ps_5_0.hlsl
@@ -89,6 +89,9 @@ void main(
   r1.xyzw = t0.Sample(s0_s, v4.wy).xyzw;
   r0.xyzw = r1.xyzw * float4(0.000310633885,0.000310633885,0.000310633885,0.000310633885) + r0.xyzw;
   r1.xyzw = t0.Sample(s0_s, v4.wz).xyzw;
-  o0.xyzw = r1.xyzw * float4(0.000194795124,0.000194795124,0.000194795124,0.000194795124) + r0.xyzw * injectedData.fxBloom;
+  o0.xyzw = r1.xyzw * float4(0.000194795124,0.000194795124,0.000194795124,0.000194795124) + r0.xyzw;
+
+  o0.rgb *= injectedData.fxBloom;
+
   return;
 }
