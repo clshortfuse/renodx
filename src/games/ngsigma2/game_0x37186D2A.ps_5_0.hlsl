263a31aca27aba93ddfa0e2068db59b885361ad0
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-07-13 11:32:06 +0200

    feat(ngsigma2): initial commit

---
+++ src/games/ngsigma2/game_0x37186D2A.ps_5_0.hlsl
@@ -0,0 +1,40 @@
+#include "./common.hlsl"
+
+// ---- Created with 3Dmigoto v1.3.16 on Sun Jul 13 10:33:37 2025
+
+cbuffer vector_register_block : register(b0)
+{
+  float3 gBloomScale : packoffset(c0);
+  float4 gColorCorrectACParameter[3] : packoffset(c1);
+  float4 gColorCorrectSecondaryACParameter[3] : packoffset(c4);
+  float4 gColorCorrectSecondaryMaskAdjust : packoffset(c7);
+}
+
+SamplerState gScreenSampler_SamplerState_s : register(s0);
+SamplerState gBloomEffectIuminous_SamplerState_s : register(s1);
+Texture2D<float4> gScreenSampler_Texture : register(t0);
+Texture2D<float4> gBloomEffectIuminous_Texture : register(t1);
+
+
+// 3Dmigoto declarations
+#define cmp -
+
+
+void main(
+    float4 v0: SV_Position0,
+    float4 v1: TEXCOORD0,
+    out float4 o0: SV_Target0)
+{
+  float4 r0,r1;
+  uint4 bitmask, uiDest;
+  float4 fDest;
+
+  r0.xyz = gBloomEffectIuminous_Texture.Sample(gBloomEffectIuminous_SamplerState_s, v1.xy).xyz;
+  r1.xyzw = gScreenSampler_Texture.Sample(gScreenSampler_SamplerState_s, v1.xy).xyzw;
+  o0.xyz = r0.xyz * gBloomScale.xyz + r1.xyz;
+  o0.w = r1.w;
+
+  o0.rgb = applyToneMap(o0.rgb);
+
+  return;
+}
