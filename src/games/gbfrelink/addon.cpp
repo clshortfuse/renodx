73df690ce08378c7e4a40f88d3b5b97ccadf322e
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-07-14 21:41:31 +0200

    fix(gbfrelink): fix hue correction

--- src/games/gbfrelink/addon.cpp
+++ src/games/gbfrelink/addon.cpp
@@ -175,7 +175,7 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
       renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
           .old_format = reshade::api::format::r8g8b8a8_typeless,
           .new_format = reshade::api::format::r16g16b16a16_float,
-          //.aspect_ratio = renodx::mods::swapchain::SwapChainUpgradeTarget::BACK_BUFFER,
+          .aspect_ratio = renodx::mods::swapchain::SwapChainUpgradeTarget::BACK_BUFFER,
       });
 
       reshade::register_event<reshade::addon_event::init_swapchain>(OnInitSwapchain);
