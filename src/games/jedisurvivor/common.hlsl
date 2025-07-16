5274c9998853622acef28d3576387755b8239b95
Musa Haji <mqhaji@vt.edu>
2025-07-16 18:05:01 -0400

    feat(jedisurvivor): overhaul mod - add new tonemappers in SDR lutbuilder path - fix ui blending - add lut scaling - add addon.cpp

--- src/games/jedisurvivor/common.hlsl
+++ src/games/jedisurvivor/common.hlsl
@@ -1,29 +1,97 @@
 #include "./shared.h"
 
-float3 applyUserToneMap(float3 untonemapped, float vanillaMidGray) {
-  renodx::tonemap::Config tm_config = renodx::tonemap::config::Create();
+float3 ApplyExposureContrastFlareHighlightsShadowsByLuminance(float3 untonemapped, float y, renodx::color::grade::Config config, float mid_gray = 0.18f) {
+  if (config.exposure == 1.f && config.shadows == 1.f && config.highlights == 1.f && config.contrast == 1.f && config.flare == 0.f) {
+    return untonemapped;
+  }
+  float3 color = untonemapped;
 
-  // ShortFuse's previous values
-  // float renoDRTHighlights = 1.20f;
-  // float renoDRTShadows = 1.0f;
-  // float renoDRTContrast = 1.80f;
-  // float renoDRTSaturation = 1.40f;
-  // float renoDRTDechroma = 0.60f;
-  // float renoDRTFlare = 0.f;
+  color *= config.exposure;
+
+  const float y_normalized = y / mid_gray;
+  const float highlight_mask = 1.f / mid_gray;
+  const float shadow_mask = mid_gray;
+
+  // contrast & flare
+  float flare = renodx::math::DivideSafe(y_normalized + config.flare, y_normalized, 1.f);
+  float exponent = config.contrast * flare;
+  const float y_contrasted = pow(y_normalized, exponent);
+
+  // highlights
+  float y_highlighted = pow(y_contrasted, config.highlights);
+  y_highlighted = lerp(y_contrasted, y_highlighted, saturate(y_contrasted / highlight_mask));
+
+  // shadows
+  float y_shadowed = pow(y_highlighted, -1.f * (config.shadows - 2.f));
+  y_shadowed = lerp(y_shadowed, y_highlighted, saturate(y_highlighted / shadow_mask));
+
+  const float y_final = y_shadowed * mid_gray;
+
+  color *= (y > 0 ? (y_final / y) : 0);
+
+  return color;
+}
+
+float3 ApplySaturationBlowoutHueCorrectionHighlightSaturation(float3 tonemapped, float3 hue_reference_color, float y, renodx::color::grade::Config config) {
+  float3 color = tonemapped;
+  if (config.saturation != 1.f || config.dechroma != 0.f || config.hue_correction_strength != 0.f || config.blowout != 0.f) {
+    float3 perceptual_new = renodx::color::oklab::from::BT709(color);
+
+    if (config.hue_correction_strength != 0.f) {
+      float3 perceptual_old = renodx::color::oklab::from::BT709(hue_reference_color);
+
+      // Save chrominance to apply black
+      float chrominance_pre_adjust = distance(perceptual_new.yz, 0);
+
+      perceptual_new.yz = lerp(perceptual_new.yz, perceptual_old.yz, config.hue_correction_strength);
+
+      float chrominance_post_adjust = distance(perceptual_new.yz, 0);
+
+      // Apply back previous chrominance
+      perceptual_new.yz *= renodx::math::DivideSafe(chrominance_pre_adjust, chrominance_post_adjust, 1.f);
+    }
+
+    if (config.dechroma != 0.f) {
+      perceptual_new.yz *= lerp(1.f, 0.f, saturate(pow(y / (10000.f / 100.f), (1.f - config.dechroma))));
+    }
+
+    if (config.blowout != 0.f) {
+      float percent_max = saturate(y * 100.f / 10000.f);
+      // positive = 1 to 0, negative = 1 to 2
+      float blowout_strength = 100.f;
+      float blowout_change = pow(1.f - percent_max, blowout_strength * abs(config.blowout));
+      if (config.blowout < 0) {
+        blowout_change = (2.f - blowout_change);
+      }
+
+      perceptual_new.yz *= blowout_change;
+    }
+
+    perceptual_new.yz *= config.saturation;
+
+    color = renodx::color::bt709::from::OkLab(perceptual_new);
+
+    color = renodx::color::bt709::clamp::AP1(color);
+  }
+  return color;
+}
+
+float3 ApplyUserToneMap(float3 untonemapped_ap1) {
+  renodx::tonemap::Config tm_config = renodx::tonemap::config::Create();
+  float mid_gray = 0.1f;
 
   // RENOCES
-  float renoDRTHighlights = 0.925f;
-  float renoDRTShadows = 1.f;
-  float renoDRTContrast = 1.485f;
-  float renoDRTSaturation = 1.225f;
-  float renoDRTDechroma = 0.f;
-  float renoDRTFlare = 0.0205f;
-
-  tm_config.reno_drt_tone_map_method = renodx::tonemap::renodrt::config::tone_map_method::REINHARD;
-  tm_config.hue_correction_strength = RENODX_TONE_MAP_HUE_CORRECTION;
+  float reno_drt_highlights = 1.1025f;
+  float reno_drt_shadows = 0.86f;
+  float reno_drt_contrast = 1.45f;
+  float reno_drt_saturation = 3.2f;
+  float renoDRTDechroma = 0.90f;
+  tm_config.reno_drt_tone_map_method = renodx::tonemap::renodrt::config::tone_map_method::DANIELE;
+  tm_config.hue_correction_strength = RENODX_TONE_MAP_HUE_SHIFT;
   tm_config.hue_correction_type = renodx::tonemap::renodrt::config::hue_correction_type::CUSTOM;
-  tm_config.hue_correction_color = renodx::tonemap::ACESFittedAP1(untonemapped);
-  tm_config.reno_drt_per_channel = RENODX_TONE_MAP_PER_CHANNEL;
+  tm_config.reno_drt_hue_correction_method = renodx::tonemap::renodrt::config::hue_correction_method::ICTCP;
+  tm_config.reno_drt_per_channel = false;
+
   tm_config.type = RENODX_TONE_MAP_TYPE;
   tm_config.peak_nits = RENODX_PEAK_WHITE_NITS;
   tm_config.game_nits = RENODX_DIFFUSE_WHITE_NITS;
@@ -33,16 +101,87 @@ float3 applyUserToneMap(float3 untonemapped, float vanillaMidGray) {
   tm_config.shadows = RENODX_TONE_MAP_SHADOWS;
   tm_config.contrast = RENODX_TONE_MAP_CONTRAST;
   tm_config.saturation = RENODX_TONE_MAP_SATURATION;
-  tm_config.reno_drt_highlights = renoDRTHighlights;
-  tm_config.reno_drt_shadows = renoDRTShadows;
-  tm_config.reno_drt_contrast = renoDRTContrast;
-  tm_config.reno_drt_saturation = renoDRTSaturation;
-  tm_config.reno_drt_dechroma = renoDRTDechroma;
-  tm_config.reno_drt_blowout = RENODX_TONE_MAP_HIGHLIGHT_SATURATION;
-  tm_config.mid_gray_value = vanillaMidGray;
-  tm_config.mid_gray_nits = vanillaMidGray * 100.f;
-  tm_config.reno_drt_flare = renoDRTFlare;
-  tm_config.reno_drt_working_color_space = RENODX_TONE_MAP_WORKING_COLOR_SPACE;
+  tm_config.reno_drt_highlights = reno_drt_highlights;
+  tm_config.reno_drt_shadows = reno_drt_shadows;
+  tm_config.reno_drt_contrast = reno_drt_contrast;
+  tm_config.reno_drt_saturation = reno_drt_saturation;
+  tm_config.reno_drt_dechroma = RENODX_TONE_MAP_BLOWOUT;
+  tm_config.reno_drt_blowout = -1.f * (RENODX_TONE_MAP_HIGHLIGHT_SATURATION - 1.f);
+  ;
+  tm_config.mid_gray_value = mid_gray;
+  tm_config.mid_gray_nits = mid_gray * 100.f;
+  tm_config.reno_drt_flare = 0.10f * pow(RENODX_TONE_MAP_FLARE, 10.f);
+  tm_config.reno_drt_working_color_space = 2;
 
-  return renodx::tonemap::config::Apply(untonemapped, tm_config);
+  float3 incorrect_hue_ap1 = (untonemapped_ap1 * tm_config.mid_gray_value / 0.18f);
+  float3 hue_shifted_color = renodx::color::bt709::from::AP1(renodx::tonemap::ExponentialRollOff(incorrect_hue_ap1, tm_config.mid_gray_value, 1.5f));
+
+  tm_config.hue_correction_color = hue_shifted_color;
+
+  return renodx::tonemap::config::Apply(renodx::color::bt709::from::AP1(untonemapped_ap1), tm_config);
+}
+
+float3 ApplyACES(float3 untonemapped_ap1) {
+  renodx::color::grade::Config cg_config = renodx::color::grade::config::Create();
+  cg_config.exposure = RENODX_TONE_MAP_EXPOSURE;
+  cg_config.highlights = RENODX_TONE_MAP_HIGHLIGHTS;
+  cg_config.shadows = RENODX_TONE_MAP_SHADOWS;
+  cg_config.contrast = RENODX_TONE_MAP_CONTRAST;
+  cg_config.flare = 0.10f * pow(RENODX_TONE_MAP_FLARE, 10.f);
+  cg_config.saturation = RENODX_TONE_MAP_SATURATION;
+  // cg_config.dechroma = RENODX_TONE_MAP_BLOWOUT;
+  cg_config.hue_correction_strength = 1.f - RENODX_TONE_MAP_HUE_SHIFT;
+  float untonemapped_lum = renodx::color::y::from::AP1(untonemapped_ap1);
+  cg_config.blowout = -1.f * (RENODX_TONE_MAP_HIGHLIGHT_SATURATION - 1.f);
+
+  const float ACES_MIN = 0.0001f;
+  float aces_min = ACES_MIN / RENODX_DIFFUSE_WHITE_NITS;
+  float aces_max = (RENODX_PEAK_WHITE_NITS / RENODX_DIFFUSE_WHITE_NITS);
+
+  if (RENODX_GAMMA_CORRECTION) {
+    aces_max = renodx::color::correct::Gamma(aces_max, true);
+    aces_min = renodx::color::correct::Gamma(aces_min, true);
+  }
+
+  untonemapped_ap1 = ApplyExposureContrastFlareHighlightsShadowsByLuminance(untonemapped_ap1, untonemapped_lum, cg_config);
+  float3 tonemapped_ap1 = renodx::tonemap::aces::ODT(untonemapped_ap1, aces_min * 48.f, aces_max * 48.f, renodx::color::IDENTITY_MAT) / 48.f;
+  float3 tonemapped_bt709 = renodx::color::bt709::from::AP1(tonemapped_ap1);
+  tonemapped_bt709 = ApplySaturationBlowoutHueCorrectionHighlightSaturation(tonemapped_bt709, renodx::color::bt709::from::AP1(untonemapped_ap1), untonemapped_lum, cg_config);
+  // tonemapped_bt709 = renodx::color::correct::Hue(tonemapped_bt709, renodx::color::bt709::from::AP1(untonemapped_ap1), (1.f - RENODX_TONE_MAP_HUE_CORRECTION));
+
+  return tonemapped_bt709;
+}
+
+float4 GenerateOutput(float3 untonemapped_ap1) {
+  float3 tonemapped_bt709;
+  if (RENODX_TONE_MAP_TYPE == 2.f) {
+    tonemapped_bt709 = ApplyACES(untonemapped_ap1);
+  } else {
+    tonemapped_bt709 = ApplyUserToneMap(untonemapped_ap1);
+  }
+
+  float3 pq_color = renodx::color::pq::EncodeSafe(renodx::color::bt2020::from::BT709(tonemapped_bt709), RENODX_DIFFUSE_WHITE_NITS);
+  pq_color /= 1.05f;
+
+  return float4(pq_color, 0.f);
+}
+
+bool HandleUICompositing(float4 ui_color, float4 scene_color, inout float4 output_color) {
+  if (RENODX_TONE_MAP_TYPE == 0.f) return false;
+
+  // linearize and normalize brightness
+  float3 ui_color_linear = renodx::color::pq::DecodeSafe(ui_color.rgb, RENODX_DIFFUSE_WHITE_NITS);
+  float3 scene_color_linear = renodx::color::pq::DecodeSafe(scene_color.rgb, RENODX_DIFFUSE_WHITE_NITS);
+
+  // blend in gamma
+  float3 ui_color_gamma = renodx::color::gamma::EncodeSafe(ui_color_linear);
+  float3 scene_color_gamma = renodx::color::gamma::EncodeSafe(scene_color_linear);
+  float3 composited_color_gamma = ui_color_gamma + scene_color_gamma * (1.0 - ui_color.a);
+
+  // linearize and scale up brightness
+  float3 composited_color_linear = renodx::color::gamma::DecodeSafe(composited_color_gamma);
+  float3 pq_color = renodx::color::pq::EncodeSafe(composited_color_linear, RENODX_DIFFUSE_WHITE_NITS);
+  output_color = float4(pq_color, 1.f);
+
+  return true;
 }
