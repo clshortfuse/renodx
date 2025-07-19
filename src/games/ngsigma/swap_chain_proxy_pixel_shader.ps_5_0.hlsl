40fc7163ac191f3bbb9a3a06f3b28813a3110e2b
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-07-20 01:43:17 +0200

    feat(ngsigma): fix clamping battle fx, change hue correction

--- src/games/ngsigma/swap_chain_proxy_pixel_shader.ps_5_0.hlsl
+++ src/games/ngsigma/swap_chain_proxy_pixel_shader.ps_5_0.hlsl
@@ -4,7 +4,8 @@ float3 applyRenoDice(float3 color) {
   //const float paperWhite = RENODX_DIFFUSE_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
   const float paperWhite = RENODX_GRAPHICS_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
   const float peakWhite = RENODX_PEAK_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
-  const float highlightsShoulderStart = paperWhite;
+  //const float highlightsShoulderStart = paperWhite;
+  const float highlightsShoulderStart = 1.f;
   return renodx::tonemap::dice::BT709(color.rgb * paperWhite, peakWhite, highlightsShoulderStart) / paperWhite;
 }
 
@@ -17,14 +18,8 @@ float4 main(float4 vpos: SV_POSITION, float2 uv: TEXCOORD0)
   if (RENODX_TONE_MAP_TYPE) {
     SV_Target.rgb = renodx::color::srgb::DecodeSafe(SV_Target.rgb);
 
-    SV_Target.rgb = renodx::color::bt709::from::BT2020(SV_Target.rgb);
-    SV_Target.rgb = renodx::color::correct::Hue(SV_Target.rgb, saturate(renodx::tonemap::dice::BT709(SV_Target.rgb, 1.f, 0.5f)), RENODX_TONE_MAP_HUE_CORRECTION);
-    SV_Target.rgb = renodx::color::bt2020::from::BT709(SV_Target.rgb);
-
     renodx::draw::Config draw_config = renodx::draw::BuildConfig();
     draw_config.peak_white_nits = 10000.f;
-    draw_config.tone_map_hue_correction = 0.f;
-    draw_config.tone_map_hue_shift = 0.f;
     draw_config.diffuse_white_nits = RENODX_GRAPHICS_WHITE_NITS;
     draw_config.graphics_white_nits = RENODX_DIFFUSE_WHITE_NITS;
 
