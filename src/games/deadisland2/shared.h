d0eff1bf94c4f6f92cfa2b9c3211361840e08adc
Musa Haji <mqhaji@vt.edu>
2025-07-20 04:10:56 -0400

    feat(deadisland2): add new and improve grading, do gamma ui composite

--- src/games/deadisland2/shared.h
+++ src/games/deadisland2/shared.h
@@ -27,7 +27,9 @@ struct ShaderInjectData {
   float tone_map_shadows;
   float tone_map_contrast;
   float tone_map_saturation;
+  float tone_map_highlight_saturation;
   float tone_map_blowout;
+  float tone_map_flare;
   float tone_map_hue_correction;
   float gamma_correction;
   float tone_map_per_channel;
@@ -40,20 +42,22 @@ cbuffer shader_injection : register(b13) {
   ShaderInjectData shader_injection : packoffset(c0);
 }
 
-#define RENODX_TONE_MAP_TYPE           shader_injection.tone_map_type
-#define RENODX_PEAK_WHITE_NITS         shader_injection.peak_white_nits
-#define RENODX_DIFFUSE_WHITE_NITS      shader_injection.diffuse_white_nits
-#define RENODX_GRAPHICS_WHITE_NITS     shader_injection.graphics_white_nits
-#define RENODX_GAMMA_CORRECTION        shader_injection.gamma_correction
-#define RENODX_TONE_MAP_PER_CHANNEL    shader_injection.tone_map_per_channel
-#define RENODX_TONE_MAP_HUE_CORRECTION shader_injection.tone_map_hue_correction
-#define RENODX_TONE_MAP_EXPOSURE       shader_injection.tone_map_exposure
-#define RENODX_TONE_MAP_HIGHLIGHTS     shader_injection.tone_map_highlights
-#define RENODX_TONE_MAP_SHADOWS        shader_injection.tone_map_shadows
-#define RENODX_TONE_MAP_CONTRAST       shader_injection.tone_map_contrast
-#define RENODX_TONE_MAP_SATURATION     shader_injection.tone_map_saturation
-#define RENODX_TONE_MAP_BLOWOUT        shader_injection.tone_map_blowout
-#define CUSTOM_BLOOM                   shader_injection.custom_bloom
+#define RENODX_TONE_MAP_TYPE                 shader_injection.tone_map_type
+#define RENODX_PEAK_WHITE_NITS               shader_injection.peak_white_nits
+#define RENODX_DIFFUSE_WHITE_NITS            shader_injection.diffuse_white_nits
+#define RENODX_GRAPHICS_WHITE_NITS           shader_injection.graphics_white_nits
+#define RENODX_GAMMA_CORRECTION              shader_injection.gamma_correction
+#define RENODX_TONE_MAP_PER_CHANNEL          shader_injection.tone_map_per_channel
+#define RENODX_TONE_MAP_HUE_CORRECTION       shader_injection.tone_map_hue_correction
+#define RENODX_TONE_MAP_EXPOSURE             shader_injection.tone_map_exposure
+#define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.tone_map_highlights
+#define RENODX_TONE_MAP_SHADOWS              shader_injection.tone_map_shadows
+#define RENODX_TONE_MAP_CONTRAST             shader_injection.tone_map_contrast
+#define RENODX_TONE_MAP_SATURATION           shader_injection.tone_map_saturation
+#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION shader_injection.tone_map_highlight_saturation
+#define RENODX_TONE_MAP_BLOWOUT              shader_injection.tone_map_blowout
+#define RENODX_TONE_MAP_FLARE                shader_injection.tone_map_flare
+#define CUSTOM_BLOOM                         shader_injection.custom_bloom
 
 #include "../../shaders/renodx.hlsl"
 
