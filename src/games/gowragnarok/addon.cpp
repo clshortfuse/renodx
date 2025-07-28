c12d37891d93e29a136e795a0c8f02b2d945bf67
Musa Haji <mqhaji@vt.edu>
2025-07-28 01:28:19 -0400

    feat(gowragnarok): fix default highlight saturation

--- src/games/gowragnarok/addon.cpp
+++ src/games/gowragnarok/addon.cpp
@@ -135,7 +135,7 @@ renodx::utils::settings::Settings settings = {
     new renodx::utils::settings::Setting{
         .key = "ColorGradeHighlightSaturation",
         .binding = &shader_injection.tone_map_highlight_saturation,
-        .default_value = 55.f,
+        .default_value = 50.f,
         .label = "Highlight Saturation",
         .section = "Color Grading",
         .tooltip = "Adds or removes highlight color.",
