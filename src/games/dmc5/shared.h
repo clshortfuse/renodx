95b9ab70eab640b59f57a6f818631fc995e985cb
Musa Haji <mqhaji@vt.edu>
2025-08-22 05:11:29 -0400

    feat(dmc5): improve tonemap, add luminance gamma correction, skip sharpening, improve lut scaling

--- src/games/dmc5/shared.h
+++ src/games/dmc5/shared.h
@@ -2,6 +2,12 @@
 #define SRC_DMCV_SHARED_H_
 
 #ifndef __cplusplus
+
+#define GAMMA_CORRECTION    1u
+#define CUSTOM_LUT_STRENGTH 1.f
+#define CUSTOM_LUT_SCALING  1.f
+#define CUSTOM_SHARPENING   2.f
+
 #include "../../shaders/renodx.hlsl"
 #endif
 
