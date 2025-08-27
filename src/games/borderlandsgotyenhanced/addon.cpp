4ee90906486703b06394a211c6804b6dcda5561b
Steve161803 <steve.t.cool@gmail.com>
2025-08-27 16:05:09 +0300

    feat(borderlandsgotyenhanced): add recource cloning

--- src/games/borderlandsgotyenhanced/addon.cpp
+++ src/games/borderlandsgotyenhanced/addon.cpp
@@ -375,10 +375,12 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
           .old_format = reshade::api::format::r10g10b10a2_unorm,
           .new_format = reshade::api::format::r16g16b16a16_float,
           .ignore_size = true,
+          .use_resource_view_cloning = true,
       });
       renodx::mods::swapchain::swap_chain_upgrade_targets.push_back({
           .old_format = reshade::api::format::r11g11b10_float,
           .new_format = reshade::api::format::r16g16b16a16_float,
+          .use_resource_view_cloning = true,
       });
 
       break;
