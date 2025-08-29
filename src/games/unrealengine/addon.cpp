8f5b6b65b0211d420f72af2af77da60d0ab564dd
marat569 <marat569@gmail.com>
2025-08-28 20:47:02 -0400

    feat(unrealengine): Add lost soul aside default upgrades

--- src/games/unrealengine/addon.cpp
+++ src/games/unrealengine/addon.cpp
@@ -584,6 +584,13 @@ const std::unordered_map<
                 {"Upgrade_R10G10B10A2_UNORM", UPGRADE_TYPE_OUTPUT_SIZE},
             },
         },
+        {
+            "Lost Soul Aside",
+            {
+                {"Upgrade_B8G8R8A8_TYPELESS", UPGRADE_TYPE_OUTPUT_SIZE},
+                {"Upgrade_R10G10B10A2_UNORM", UPGRADE_TYPE_OUTPUT_SIZE},
+            },
+        },
 
 };
 
