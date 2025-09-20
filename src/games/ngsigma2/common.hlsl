f96b7b3d525f9e5597f949dcab8be0e9c1594fcb
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-09-20 13:49:45 +0200

    feat(ngsigma): replace DICE with exp rolloff

--- src/games/ngsigma2/common.hlsl
+++ src/games/ngsigma2/common.hlsl
@@ -1,14 +1,34 @@
 #include "./shared.h"
 
-float3 applyRenoDice(float3 color) {
-  const float paperWhite = RENODX_DIFFUSE_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
-  const float peakWhite = RENODX_PEAK_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
-  //const float highlightsShoulderStart = paperWhite;
-  const float highlightsShoulderStart = 1.f;
-  return renodx::tonemap::dice::BT709(color.rgb * paperWhite, peakWhite, highlightsShoulderStart) / paperWhite;
+float3 ExponentialRollOffByLum(float3 color, float output_luminance_max, float highlights_shoulder_start = 0.f) {
+  const float source_luminance = renodx::color::y::from::BT709(color);
+
+  [branch]
+  if (source_luminance > 0.0f) {
+    const float compressed_luminance = renodx::tonemap::ExponentialRollOff(source_luminance, highlights_shoulder_start, output_luminance_max);
+    color *= compressed_luminance / source_luminance;
+  }
+
+  return color;
 }
 
-float3 applyToneMap(float3 color) {
+float3 ApplyExponentialRollOff(float3 color) {
+  const float paperWhite = RENODX_DIFFUSE_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
+
+  const float peakWhite = RENODX_PEAK_WHITE_NITS / renodx::color::srgb::REFERENCE_WHITE;
+
+  // const float highlightsShoulderStart = paperWhite;
+  const float highlightsShoulderStart = 1.f;
+
+  [branch]
+  if (RENODX_TONE_MAP_PER_CHANNEL == 0.f) {
+    return ExponentialRollOffByLum(color * paperWhite, peakWhite, highlightsShoulderStart) / paperWhite;
+  } else {
+    return renodx::tonemap::ExponentialRollOff(color * paperWhite, highlightsShoulderStart, peakWhite) / paperWhite;
+  }
+}
+
+float3 ApplyToneMap(float3 color) {
   color = renodx::color::srgb::DecodeSafe(color);
 
   if (RENODX_TONE_MAP_TYPE) {
@@ -17,7 +37,7 @@ float3 applyToneMap(float3 color) {
 
     color = renodx::draw::ToneMapPass(color, draw_config);
 
-    color = applyRenoDice(color);
+    color = ApplyExponentialRollOff(color);
   } else {
     color = saturate(color);
   }
