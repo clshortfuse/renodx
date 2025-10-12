baed6d14cad906bf38f96c6a13ffe73ca78f7d1a
Carlos Lopez <clshortfuse@gmail.com>
2025-10-12 10:25:02 -0400

    feat(hollowknight-silksong): fix gamut compression with hue clip

--- src/games/hollowknight-silksong/shared.h
+++ src/games/hollowknight-silksong/shared.h
@@ -77,37 +77,40 @@ cbuffer shader_injection : register(b13) {
   ShaderInjectData shader_injection : packoffset(c0);
 }
 
-#define RENODX_TONE_MAP_TYPE                       shader_injection.tone_map_type
-#define RENODX_PEAK_WHITE_NITS                     shader_injection.peak_white_nits
-#define RENODX_DIFFUSE_WHITE_NITS                  shader_injection.diffuse_white_nits
-#define RENODX_GRAPHICS_WHITE_NITS                 shader_injection.graphics_white_nits
-#define RENODX_GAMMA_CORRECTION                    shader_injection.gamma_correction
-#define RENODX_TONE_MAP_HUE_PROCESSOR              shader_injection.tone_map_hue_processor
-#define RENODX_TONE_MAP_EXPOSURE                   shader_injection.tone_map_exposure
-#define RENODX_TONE_MAP_HIGHLIGHTS                 shader_injection.tone_map_highlights
-#define RENODX_TONE_MAP_SHADOWS                    shader_injection.tone_map_shadows
-#define RENODX_TONE_MAP_CONTRAST                   shader_injection.tone_map_contrast
-#define RENODX_TONE_MAP_SATURATION                 shader_injection.tone_map_saturation
-#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION       shader_injection.tone_map_highlight_saturation
-#define RENODX_TONE_MAP_BLOWOUT                    shader_injection.tone_map_blowout
-#define RENODX_TONE_MAP_FLARE                      shader_injection.tone_map_flare
-#define RENODX_TONE_MAP_CLAMP_PEAK                 -1.f
-#define RENODX_COLOR_GRADE_STRENGTH                shader_injection.color_grade_strength
-#define RENODX_RENO_DRT_TONE_MAP_METHOD            renodx::tonemap::renodrt::config::tone_map_method::REINHARD
-#define RENODX_TONE_MAP_HUE_CORRECTION             0.f
-#define RENODX_SWAP_CHAIN_OUTPUT_PRESET            shader_injection.swap_chain_output_preset
-#define RENODX_SWAP_CHAIN_OUTPUT_DITHER_BITS       shader_injection.swap_chain_output_dither_bits
-#define RENODX_SWAP_CHAIN_OUTPUT_DITHER_SEED       shader_injection.swap_chain_output_dither_seed
-#define RENODX_RENO_DRT_WHITE_CLIP                 shader_injection.tone_map_white_clip
-#define CUSTOM_GRAIN_STRENGTH                      shader_injection.custom_grain_strength
-#define CUSTOM_BLOOM                               shader_injection.custom_bloom
-#define CUSTOM_RANDOM                              shader_injection.custom_random
-#define CUSTOM_HERO_LIGHT                          shader_injection.custom_hero_light
-#define CUSTOM_HDR_VIDEOS                          shader_injection.custom_hdr_videos
-#define CUSTOM_VIGNETTE                            shader_injection.custom_vignette
-#define CUSTOM_SATURATION_CLIP                     shader_injection.custom_saturation_clip
-#define CUSTOM_BLOOM_CLIP                          shader_injection.custom_bloom_clip
-#define CUSTOM_HUE_CLIP                            shader_injection.custom_hue_clip
+#define RENODX_TONE_MAP_TYPE                          shader_injection.tone_map_type
+#define RENODX_PEAK_WHITE_NITS                        shader_injection.peak_white_nits
+#define RENODX_DIFFUSE_WHITE_NITS                     shader_injection.diffuse_white_nits
+#define RENODX_GRAPHICS_WHITE_NITS                    shader_injection.graphics_white_nits
+#define RENODX_GAMMA_CORRECTION                       shader_injection.gamma_correction
+#define RENODX_TONE_MAP_HUE_PROCESSOR                 shader_injection.tone_map_hue_processor
+#define RENODX_TONE_MAP_EXPOSURE                      shader_injection.tone_map_exposure
+#define RENODX_TONE_MAP_HIGHLIGHTS                    shader_injection.tone_map_highlights
+#define RENODX_TONE_MAP_SHADOWS                       shader_injection.tone_map_shadows
+#define RENODX_TONE_MAP_CONTRAST                      shader_injection.tone_map_contrast
+#define RENODX_TONE_MAP_SATURATION                    shader_injection.tone_map_saturation
+#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION          shader_injection.tone_map_highlight_saturation
+#define RENODX_TONE_MAP_BLOWOUT                       shader_injection.tone_map_blowout
+#define RENODX_TONE_MAP_FLARE                         shader_injection.tone_map_flare
+#define RENODX_TONE_MAP_CLAMP_PEAK                    -1.f
+#define RENODX_COLOR_GRADE_STRENGTH                   shader_injection.color_grade_strength
+#define RENODX_RENO_DRT_TONE_MAP_METHOD               renodx::tonemap::renodrt::config::tone_map_method::REINHARD
+#define RENODX_TONE_MAP_HUE_CORRECTION                0.f
+#define RENODX_SWAP_CHAIN_OUTPUT_PRESET               shader_injection.swap_chain_output_preset
+#define RENODX_SWAP_CHAIN_OUTPUT_DITHER_BITS          shader_injection.swap_chain_output_dither_bits
+#define RENODX_SWAP_CHAIN_OUTPUT_DITHER_SEED          shader_injection.swap_chain_output_dither_seed
+#define RENODX_RENO_DRT_WHITE_CLIP                    shader_injection.tone_map_white_clip
+#define RENODX_RENO_DRT_NEUTRAL_SDR_CLAMP_PEAK        -1
+#define RENODX_RENO_DRT_NEUTRAL_SDR_CLAMP_COLOR_SPACE -1
+#define RENODX_RENO_DRT_NEUTRAL_SDR_TONE_MAP_METHOD   renodx::tonemap::renodrt::config::tone_map_method::REINHARD
+#define CUSTOM_GRAIN_STRENGTH                         shader_injection.custom_grain_strength
+#define CUSTOM_BLOOM                                  shader_injection.custom_bloom
+#define CUSTOM_RANDOM                                 shader_injection.custom_random
+#define CUSTOM_HERO_LIGHT                             shader_injection.custom_hero_light
+#define CUSTOM_HDR_VIDEOS                             shader_injection.custom_hdr_videos
+#define CUSTOM_VIGNETTE                               shader_injection.custom_vignette
+#define CUSTOM_SATURATION_CLIP                        shader_injection.custom_saturation_clip
+#define CUSTOM_BLOOM_CLIP                             shader_injection.custom_bloom_clip
+#define CUSTOM_HUE_CLIP                               shader_injection.custom_hue_clip
 
 #include "../../shaders/renodx.hlsl"
 
