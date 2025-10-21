46ff549db0577b4f36f82a96a602e86260f6ddb0
Carlos Lopez <clshortfuse@gmail.com>
2025-10-21 17:34:18 -0400

    feat(shaders): add float pq encode/decode

--- src/shaders/color.hlsl
+++ src/shaders/color.hlsl
@@ -362,10 +362,22 @@ static const float C1 = 3424.f / 4096.f;            // 0.8359375f;
 static const float C2 = 32.f * (2413.f / 4096.f);   // 18.8515625f;
 static const float C3 = 32.f * (2392.f / 4096.f);   // 18.6875f;
 
+float Encode(float color, float scaling = 10000.f) {
+  color *= (scaling / 10000.f);
+  float y_m1 = pow(color, M1);
+  return pow((C1 + C2 * y_m1) / (1.f + C3 * y_m1), M2);
+}
+
 float3 Encode(float3 color, float scaling = 10000.f) {
   color *= (scaling / 10000.f);
   float3 y_m1 = pow(color, M1);
   return pow((C1 + C2 * y_m1) / (1.f + C3 * y_m1), M2);
+}
+
+float Decode(float color, float scaling = 10000.f) {
+  float e_m12 = pow(color, 1.f / M2);
+  float out_color = pow(max(0, e_m12 - C1) / (C2 - C3 * e_m12), 1.f / M1);
+  return out_color * (10000.f / scaling);
 }
 
 float3 Decode(float3 color, float scaling = 10000.f) {
