551aca2aeb69ee666bbf4dc79f2b06dbf9fd321b
Mohanned Hassan <gailardiagalane@gmail.com>
2025-10-23 13:47:42 +0400

    feat(kingdomcome2): Change tonemapper to Hermite Spline

--- src/games/kingdomcome2/shared.h
+++ src/games/kingdomcome2/shared.h
@@ -30,28 +30,22 @@ cbuffer shader_injection : register(b13) {
   ShaderInjectData shader_injection : packoffset(c0);
 }
 
-#define RENODX_TONE_MAP_TYPE                          shader_injection.tone_map_type
-#define RENODX_PEAK_WHITE_NITS                        shader_injection.peak_white_nits
-#define RENODX_DIFFUSE_WHITE_NITS                     shader_injection.diffuse_white_nits
-#define RENODX_GRAPHICS_WHITE_NITS                    shader_injection.graphics_white_nits
-#define RENODX_TONE_MAP_EXPOSURE                      shader_injection.tone_map_exposure
-#define RENODX_TONE_MAP_HIGHLIGHTS                    shader_injection.tone_map_highlights
-#define RENODX_TONE_MAP_SHADOWS                       shader_injection.tone_map_shadows
-#define RENODX_TONE_MAP_CONTRAST                      shader_injection.tone_map_contrast
-#define RENODX_TONE_MAP_SATURATION                    shader_injection.tone_map_saturation
-#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION          shader_injection.tone_map_highlight_saturation
-#define RENODX_TONE_MAP_BLOWOUT                       shader_injection.tone_map_blowout
-#define RENODX_TONE_MAP_FLARE                         shader_injection.tone_map_flare
-#define CUSTOM_BLOOM                                  shader_injection.fxBloom
-#define RENODX_RENO_DRT_TONE_MAP_METHOD               renodx::tonemap::renodrt::config::tone_map_method::REINHARD
-#define RENODX_SWAP_CHAIN_OUTPUT_PRESET               renodx::draw::SWAP_CHAIN_OUTPUT_PRESET_SCRGB
-#define RENODX_INTERMEDIATE_ENCODING                  renodx::draw::GAMMA_CORRECTION_NONE
-#define RENODX_TONE_MAP_PASS_AUTOCORRECTION           1.f
-#define RENODX_RENO_DRT_WHITE_CLIP                    100.f
-#define RENODX_TONE_MAP_CLAMP_PEAK                    -1.f
-#define RENODX_RENO_DRT_NEUTRAL_SDR_CLAMP_PEAK        -1
-#define RENODX_RENO_DRT_NEUTRAL_SDR_CLAMP_COLOR_SPACE -1
-#define RENODX_RENO_DRT_NEUTRAL_SDR_TONE_MAP_METHOD   renodx::tonemap::renodrt::config::tone_map_method::REINHARD
+#define RENODX_TONE_MAP_TYPE                 shader_injection.tone_map_type
+#define RENODX_PEAK_WHITE_NITS               shader_injection.peak_white_nits
+#define RENODX_DIFFUSE_WHITE_NITS            shader_injection.diffuse_white_nits
+#define RENODX_GRAPHICS_WHITE_NITS           shader_injection.graphics_white_nits
+#define RENODX_TONE_MAP_EXPOSURE             shader_injection.tone_map_exposure
+#define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.tone_map_highlights
+#define RENODX_TONE_MAP_SHADOWS              shader_injection.tone_map_shadows
+#define RENODX_TONE_MAP_CONTRAST             shader_injection.tone_map_contrast
+#define RENODX_TONE_MAP_SATURATION           shader_injection.tone_map_saturation
+#define RENODX_TONE_MAP_HIGHLIGHT_SATURATION shader_injection.tone_map_highlight_saturation
+#define RENODX_TONE_MAP_BLOWOUT              shader_injection.tone_map_blowout
+#define RENODX_TONE_MAP_FLARE                shader_injection.tone_map_flare
+#define CUSTOM_BLOOM                         shader_injection.fxBloom
+#define RENODX_RENO_DRT_TONE_MAP_METHOD      renodx::tonemap::renodrt::config::tone_map_method::HERMITE_SPLINE
+#define RENODX_SWAP_CHAIN_OUTPUT_PRESET      renodx::draw::SWAP_CHAIN_OUTPUT_PRESET_SCRGB
+#define RENODX_INTERMEDIATE_ENCODING         renodx::draw::GAMMA_CORRECTION_NONE
 
 #if (__SHADER_TARGET_MAJOR >= 6)
 #pragma dxc diagnostic ignored "-Wparentheses-equality"
