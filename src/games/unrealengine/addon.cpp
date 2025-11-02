f9519744ddddd467bd901523d1af510a2cfd04d6
Askeladd <niels.germann@outlook.com>
2025-11-02 20:53:27 +0100

    Re-add the b8g8r8a8_typeless upgrade, as removing it break more stuff than it solve...

--- src/games/unrealengine/addon.cpp
+++ src/games/unrealengine/addon.cpp
@@ -620,6 +620,7 @@ const std::unordered_map<
             "EM-Win64-Shipping.exe",
             {
                 {"Upgrade_R10G10B10A2_UNORM", UPGRADE_TYPE_OUTPUT_SIZE},
+                {"Upgrade_B8G8R8A8_TYPELESS", UPGRADE_TYPE_OUTPUT_SIZE},
             },
         },
 
