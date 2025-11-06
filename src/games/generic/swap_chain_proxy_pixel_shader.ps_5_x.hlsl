d7fe84f56c676d86571a3e61a5de9101d11c2aa7
Carlos Lopez <clshortfuse@gmail.com>
2025-11-06 08:44:08 -0500

    feat(generic): add device proxy option

--- src/games/generic/swap_chain_proxy_pixel_shader.ps_5_x.hlsl
+++ src/games/generic/swap_chain_proxy_pixel_shader.ps_5_x.hlsl
@@ -4,5 +4,8 @@ Texture2D t0 : register(t0);
 SamplerState s0 : register(s0);
 float4 main(float4 vpos: SV_POSITION, float2 uv: TEXCOORD0)
     : SV_TARGET {
+  // Used for OpenGL support
+  uv.y = lerp(uv.y, 1.0 - uv.y, shader_injection.custom_flip_uv_y);
+
   return renodx::draw::SwapChainPass(t0.Sample(s0, uv));
 }
