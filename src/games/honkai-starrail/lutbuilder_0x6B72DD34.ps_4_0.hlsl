90f210f804b8b10a38c8eabb1e10308fc776031b
Maple <8961085+MapleHinata@users.noreply.github.com>
2025-11-08 01:02:51 -0300

    feat(honkai-starrail): apply musa changes.
    Co-authored-by: Musa Haji <131800246+mqhaji@users.noreply.github.com>

--- src/games/honkai-starrail/lutbuilder_0x6B72DD34.ps_4_0.hlsl
+++ src/games/honkai-starrail/lutbuilder_0x6B72DD34.ps_4_0.hlsl
@@ -1,12 +1,8 @@
-#include "./common.hlsl"
+#include "./tonemap.hlsli"
 
 // ---- Created with 3Dmigoto v1.4.1 on Thu Jun 19 08:11:22 2025
-cbuffer cb0 : register(b0) {
-  float4 cb0[32];
-}
 
 // 3Dmigoto declarations
-#define cmp -
 
 void main(
     float4 v0: SV_POSITION0,
@@ -95,205 +91,14 @@ void main(
   r1.x = r1.x * 0.5 + -cb0[0].y;
   r0.xyz = r0.xyz / r1.xxx;
   r0.xyz = -r0.xyz * float3(0.5, 0.5, 0.5) + float3(0.5, 0.5, 0.5);
-
-  float3 untonemapped = r0.rgb;
-  
   r0.xyz = max(float3(0, 0, 0), r0.xyz);
   r0.xyz = r0.www ? r1.yzw : r0.xyz;
   r0.xyz = cb0[1].xyz * r0.xyz;
-  r0.w = cmp(0.5 >= cb0[31].x);
-  if (r0.w != 0) {
-    r0.w = cmp(0.5 < cb0[30].z);
-    if (r0.w != 0) {
-      r1.x = dot(float3(0.613189995, 0.339509994, 0.0473700017), r0.xyz);
-      r1.y = dot(float3(0.0702100024, 0.916339993, 0.0134500004), r0.xyz);
-      r1.z = dot(float3(0.0206199996, 0.109569997, 0.869610012), r0.xyz);
-      r0.w = dot(r1.xyz, float3(0.272228986, 0.674081981, 0.0536894985));
-      r2.xyz = r1.xyz / r0.www;
-      r2.xyz = float3(-1, -1, -1) + r2.xyz;
-      r1.w = dot(r2.xyz, r2.xyz);
-      r1.w = -4 * r1.w;
-      r1.w = exp2(r1.w);
-      r1.w = 1 + -r1.w;
-      r0.w = r0.w * r0.w;
-      r0.w = cb0[30].x * r0.w;
-      r0.w = -4 * r0.w;
-      r0.w = exp2(r0.w);
-      r0.w = 1 + -r0.w;
-      r0.w = r1.w * r0.w;
-      r2.x = dot(float3(1.37041104, -0.329291195, -0.0636843145), r1.xyz);
-      r2.y = dot(float3(-0.0834370106, 1.09708822, -0.0108630517), r1.xyz);
-      r2.z = dot(float3(-0.0257899351, -0.0986270159, 1.20369244), r1.xyz);
-      r2.xyz = r2.xyz + -r1.xyz;
-      r0.xyz = r0.www * r2.xyz + r1.xyz;
 
-      r0.w = cmp(0.5 < cb0[30].w);
-      if (r0.w == 0) {
-        r1.xyz = cb0[23].xxx * r0.xyz;
-        r2.xyzw = cmp(r1.xxyy < cb0[23].yzyz);
-        r3.xyzw = r2.yyyy ? cb0[26].xyzw : cb0[28].xyzw;
-        r4.xyzw = r2.yyww ? cb0[27].xyxy : cb0[29].xyxy;
-        r3.xyzw = r2.xxxx ? cb0[24].xyzw : r3.xyzw;
-        r4.xyzw = r2.xxzz ? cb0[25].xyxy : r4.xyzw;
-        r0.w = r0.x * cb0[23].x + -r3.x;
-        r0.w = r0.w * r3.z;
-        r1.x = cmp(0 < r0.w);
-        r0.w = log2(r0.w);
-        r0.w = r4.y * r0.w;
-        r0.w = r0.w * 0.693147182 + r4.x;
-        r0.w = 1.44269502 * r0.w;
-        r0.w = exp2(r0.w);
-        r0.w = r1.x ? r0.w : 0;
-        r3.x = r0.w * r3.w + r3.y;
-        r5.xyzw = r2.wwww ? cb0[26].xyzw : cb0[28].xyzw;
-        r5.xyzw = r2.zzzz ? cb0[24].xyzw : r5.xyzw;
-        r0.w = r0.y * cb0[23].x + -r5.x;
-        r0.w = r0.w * r5.z;
-        r1.x = cmp(0 < r0.w);
-        r0.w = log2(r0.w);
-        r0.w = r4.w * r0.w;
-        r0.w = r0.w * 0.693147182 + r4.z;
-        r0.w = 1.44269502 * r0.w;
-        r0.w = exp2(r0.w);
-        r0.w = r1.x ? r0.w : 0;
-        r3.y = r0.w * r5.w + r5.y;
-        r1.xy = cmp(r1.zz < cb0[23].yz);
-        r4.xyzw = r1.yyyy ? cb0[26].xyzw : cb0[28].xyzw;
-        r1.yz = r1.yy ? cb0[27].xy : cb0[29].xy;
-        r4.xyzw = r1.xxxx ? cb0[24].xyzw : r4.xyzw;
-        r1.yz = r1.xx ? cb0[25].xy : r1.yz;
-        r0.w = r0.z * cb0[23].x + -r4.x;
-        r0.w = r0.w * r4.z;
-        r1.w = cmp(0 < r0.w);
-        r0.w = log2(r0.w);
-        r0.w = r1.z * r0.w;
-        r0.w = r0.w * 0.693147182 + r1.y;
-        r0.w = 1.44269502 * r0.w;
-        r0.w = exp2(r0.w);
-        r0.w = r1.w ? r0.w : 0;
-        r3.z = r0.w * r4.w + r4.y;
-        r4.xyzw = r2.xxxx ? cb0[24].xyzw : cb0[26].xyzw;
-        r5.xyzw = r2.xxzz ? cb0[25].xyxy : cb0[27].xyxy;
-        r0.w = r0.x * cb0[23].x + -r4.x;
-        r0.w = r0.w * r4.z;
-        r1.y = cmp(0 < r0.w);
-        r0.w = log2(r0.w);
-        r0.w = r5.y * r0.w;
-        r0.w = r0.w * 0.693147182 + r5.x;
-        r0.w = 1.44269502 * r0.w;
-        r0.w = exp2(r0.w);
-        r0.w = r1.y ? r0.w : 0;
-        r0.w = r0.w * r4.w + r4.y;
-        r0.w = r0.w / cb0[30].y;
-        r1.yz = float2(-0.899999976, -0.699999988) + r0.ww;
-        r1.w = cmp(0.200000003 >= abs(r1.y));
-        r1.z = r1.z * r1.z;
-        r1.z = r1.z * -1.171875 + r0.w;
-        r2.x = cmp(0.200000003 < r1.y);
-        r1.y = r1.y * 0.0625 + 0.899999976;
-        r0.w = r2.x ? r1.y : r0.w;
-        r0.w = r1.w ? r1.z : r0.w;
-        r4.x = cb0[30].y * r0.w;
-        r2.xyzw = r2.zzzz ? cb0[24].xyzw : cb0[26].xyzw;
-        r0.w = r0.y * cb0[23].x + -r2.x;
-        r0.w = r0.w * r2.z;
-        r1.y = cmp(0 < r0.w);
-        r0.w = log2(r0.w);
-        r0.w = r5.w * r0.w;
-        r0.w = r0.w * 0.693147182 + r5.z;
-        r0.w = 1.44269502 * r0.w;
-        r0.w = exp2(r0.w);
-        r0.w = r1.y ? r0.w : 0;
-        r0.w = r0.w * r2.w + r2.y;
-        r0.w = r0.w / cb0[30].y;
-        r1.yz = float2(-0.899999976, -0.699999988) + r0.ww;
-        r1.w = cmp(0.200000003 >= abs(r1.y));
-        r1.z = r1.z * r1.z;
-        r1.z = r1.z * -1.171875 + r0.w;
-        r2.x = cmp(0.200000003 < r1.y);
-        r1.y = r1.y * 0.0625 + 0.899999976;
-        r0.w = r2.x ? r1.y : r0.w;
-        r0.w = r1.w ? r1.z : r0.w;
-        r4.y = cb0[30].y * r0.w;
-        r2.xyzw = r1.xxxx ? cb0[24].xyzw : cb0[26].xyzw;
-        r1.xy = r1.xx ? cb0[25].xy : cb0[27].xy;
-        r0.w = r0.z * cb0[23].x + -r2.x;
-        r0.w = r0.w * r2.z;
-        r1.z = cmp(0 < r0.w);
-        r0.w = log2(r0.w);
-        r0.w = r1.y * r0.w;
-        r0.w = r0.w * 0.693147182 + r1.x;
-        r0.w = 1.44269502 * r0.w;
-        r0.w = exp2(r0.w);
-        r0.w = r1.z ? r0.w : 0;
-        r0.w = r0.w * r2.w + r2.y;
-        r0.w = r0.w / cb0[30].y;
-        r1.xy = float2(-0.899999976, -0.699999988) + r0.ww;
-        r1.z = cmp(0.200000003 >= abs(r1.x));
-        r1.y = r1.y * r1.y;
-        r1.y = r1.y * -1.171875 + r0.w;
-        r1.w = cmp(0.200000003 < r1.x);
-        r1.x = r1.x * 0.0625 + 0.899999976;
-        r0.w = r1.w ? r1.x : r0.w;
-        r0.w = r1.z ? r1.y : r0.w;
-        r4.z = cb0[30].y * r0.w;
-        r0.w = dot(r4.xyz, float3(0.272228986, 0.674081981, 0.0536894985));
-        r1.x = dot(r3.xyz, float3(0.272228986, 0.674081981, 0.0536894985));
-        r1.y = cmp(r1.x < 9.99999997e-07);
-        r0.w = r0.w / r1.x;
-        r0.w = r1.y ? 0 : r0.w;
-        r1.xyz = r3.xyz * r0.www;
-        r0.x = dot(float3(1.70504999, -0.621789992, -0.0832599998), r1.xyz);
-        r0.y = dot(float3(-0.130260006, 1.1408, -0.0105499998), r1.xyz);
-        r0.z = dot(float3(-0.0240000002, -0.128969997, 1.15296996), r1.xyz);
-      }
-    } else {
-      r1.xyz = cb0[23].xxx * r0.xyz;
-      r2.xyzw = cmp(r1.xxyy < cb0[23].yzyz);
-      r3.xyzw = r2.yyyy ? cb0[26].xyzw : cb0[28].xyzw;
-      r4.xyzw = r2.yyww ? cb0[27].xyxy : cb0[29].xyxy;
-      r3.xyzw = r2.xxxx ? cb0[24].xyzw : r3.xyzw;
-      r4.xyzw = r2.xxzz ? cb0[25].xyxy : r4.xyzw;
-      r0.w = r0.x * cb0[23].x + -r3.x;
-      r0.w = r0.w * r3.z;
-      r1.x = cmp(0 < r0.w);
-      r0.w = log2(r0.w);
-      r0.w = r4.y * r0.w;
-      r0.w = r0.w * 0.693147182 + r4.x;
-      r0.w = 1.44269502 * r0.w;
-      r0.w = exp2(r0.w);
-      r0.w = r1.x ? r0.w : 0;
-      r0.x = r0.w * r3.w + r3.y;
-      r3.xyzw = r2.wwww ? cb0[26].xyzw : cb0[28].xyzw;
-      r2.xyzw = r2.zzzz ? cb0[24].xyzw : r3.xyzw;
-      r0.w = r0.y * cb0[23].x + -r2.x;
-      r0.w = r0.w * r2.z;
-      r1.x = cmp(0 < r0.w);
-      r0.w = log2(r0.w);
-      r0.w = r4.w * r0.w;
-      r0.w = r0.w * 0.693147182 + r4.z;
-      r0.w = 1.44269502 * r0.w;
-      r0.w = exp2(r0.w);
-      r0.w = r1.x ? r0.w : 0;
-      r0.y = r0.w * r2.w + r2.y;
-      r1.xy = cmp(r1.zz < cb0[23].yz);
-      r2.xyzw = r1.yyyy ? cb0[26].xyzw : cb0[28].xyzw;
-      r1.yz = r1.yy ? cb0[27].xy : cb0[29].xy;
-      r2.xyzw = r1.xxxx ? cb0[24].xyzw : r2.xyzw;
-      r1.xy = r1.xx ? cb0[25].xy : r1.yz;
-      r0.w = r0.z * cb0[23].x + -r2.x;
-      r0.w = r0.w * r2.z;
-      r1.z = cmp(0 < r0.w);
-      r0.w = log2(r0.w);
-      r0.w = r1.y * r0.w;
-      r0.w = r0.w * 0.693147182 + r1.x;
-      r0.w = 1.44269502 * r0.w;
-      r0.w = exp2(r0.w);
-      r0.w = r1.z ? r0.w : 0;
-      r0.z = r0.w * r2.w + r2.y;
-    }
-  }
-  o0.xyz = applyUserTonemap(untonemapped, r0.xyz);
+  float3 untonemapped_bt709 = r0.rgb;  // moved to after exposure cbuffer
+
+  r0.rgb = ApplyVanillaToneMap(untonemapped_bt709);
+  o0.xyz = applyUserTonemap(untonemapped_bt709, r0.xyz);
   o0.w = 1;
   return;
   r0.w = cmp(0.800000012 < cb0[30].z);
