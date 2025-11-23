57ede62a6c3399f855ebbee4856e82ac29ef7a0a
akuru-q <115506513+akuru-q@users.noreply.github.com>
2025-11-23 01:28:47 +0100

    fix(atelierryzadx): fix dx sat brightness, hue and chrominance correction order

--- src/games/atelierryzadx/common.hlsl
+++ src/games/atelierryzadx/common.hlsl
@@ -52,7 +52,7 @@ void OutColorAdjustments(inout float4 o0, float saturationScale)
   [branch]
   if (CUSTOM_SAT_BRIGHTNESS && CUSTOM_SAT_STRENGTH != 0.f) {
     g_pre_sat = renodx::color::srgb::DecodeSafe(g_pre_sat);
-    o0.rgb = renodx::color::correct::Chrominance(g_pre_sat, o0.rgb, 1.f);
+    o0.rgb = renodx::color::correct::Luminance(o0.rgb, g_pre_sat, 1.f);
   }
 
   o0.rgb = saturate(o0.rgb);
@@ -61,19 +61,19 @@ void OutColorAdjustments(inout float4 o0, float saturationScale)
   if (RENODX_TONE_MAP_TYPE != 0.f) {
     float3 sdr_color = o0.rgb;
 
-    renodx::draw::Config draw_config = renodx::draw::BuildConfig();
-    draw_config.tone_map_hue_correction = 0.f;
-    draw_config.tone_map_hue_shift = 0.f;
-
     g_untonemapped = renodx::math::SignPow(g_untonemapped, g_gamma);
     g_untonemapped = renodx::color::srgb::DecodeSafe(g_untonemapped);
 
     g_untonemapped = lerp(sdr_color, g_untonemapped, saturate(renodx::color::y::from::BT709(g_untonemapped)));
 
+    renodx::draw::Config draw_config = renodx::draw::BuildConfig();
+    draw_config.tone_map_hue_correction = 0.f;
+    draw_config.tone_map_hue_shift = 0.f;
+
     o0.rgb = renodx::draw::ToneMapPass(g_untonemapped, draw_config);
 
-    o0.rgb = renodx::color::correct::Hue(o0.rgb, sdr_color, RENODX_TONE_MAP_HUE_CORRECTION);
     o0.rgb = renodx::color::correct::Chrominance(o0.rgb, sdr_color, RENODX_TONE_MAP_HUE_SHIFT);
+    o0.rgb = renodx::color::correct::Hue(o0.rgb, sdr_color, RENODX_TONE_MAP_HUE_CORRECTION);
   }
 
   o0.rgb = renodx::draw::RenderIntermediatePass(o0.rgb);
