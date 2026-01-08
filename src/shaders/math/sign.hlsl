eb4a67f05c5a471e15b59d97b6c369359bf2e173
Carlos Lopez <clshortfuse@gmail.com>
2026-01-08 10:54:32 -0500

    feat(shaders): add half support to sign functions

--- src/shaders/math/sign.hlsl
+++ src/shaders/math/sign.hlsl
@@ -8,12 +8,14 @@
 START_NAMESPACE(renodx)
 START_NAMESPACE(math)
 
-// CopySign
+static const uint FLT32_SIGN = 0x80000000u;       // 0b10000000000000000000000000000000
+static const uint FLT32_MAGNITUDE = 0x7FFFFFFFu;  // 0b01111111111111111111111111111111
 
 #if __SHADER_TARGET_MAJOR <= 3
-#define SELECT_FUNCTION_GENERATOR(TYPE)          \
-  TYPE CopySign(TYPE mag, TYPE sgn) {            \
-    return Select(sgn < 0, -abs(mag), abs(mag)); \
+#define SELECT_FUNCTION_GENERATOR(TYPE)            \
+  TYPE CopySign(TYPE mag, TYPE sgn) {              \
+    TYPE abs_value = abs(mag);                     \
+    return Select(sgn < 0, -abs_value, abs_value); \
   }
 SELECT_FUNCTION_GENERATOR(float)
 SELECT_FUNCTION_GENERATOR(float2)
@@ -22,15 +24,15 @@ SELECT_FUNCTION_GENERATOR(float4)
 #undef SELECT_FUNCTION_GENERATOR
 #else
 float CopySign(float mag, float sgn) {
-  uint sign_bits = asuint(sgn) & 0x80000000u;
-  uint mag_bits = asuint(mag) & 0x7FFFFFFFu;
+  uint sign_bits = asuint(sgn) & FLT32_SIGN;
+  uint mag_bits = asuint(mag) & FLT32_MAGNITUDE;
   return asfloat(sign_bits | mag_bits);
 }
 
 #define COPYSIGN_FUNCTION_GENERATOR_VECTOR(SIZE)           \
   float##SIZE CopySign(float##SIZE mag, float##SIZE sgn) { \
-    uint##SIZE sign_bits = asuint(sgn) & 0x80000000u;      \
-    uint##SIZE mag_bits = asuint(mag) & 0x7FFFFFFFu;       \
+    uint##SIZE sign_bits = asuint(sgn) & FLT32_SIGN;       \
+    uint##SIZE mag_bits = asuint(mag) & FLT32_MAGNITUDE;   \
     float##SIZE result = asfloat(sign_bits | mag_bits);    \
     return result;                                         \
   }
@@ -41,14 +43,87 @@ COPYSIGN_FUNCTION_GENERATOR_VECTOR(4)
 #undef COPYSIGN_FUNCTION_GENERATOR_VECTOR
 #endif
 
+// Half-precision CopySign
+#if __SHADER_TARGET_MAJOR >= 6 && __SHADER_TARGET_MINOR >= 2
+
+static const uint16_t FLT16_SIGN = 0x8000;       // 0b1000000000000000
+static const uint16_t FLT16_MAGNITUDE = 0x7FFF;  // 0b0111111111111111
+
+half CopySign(half mag, half sgn) {
+  uint16_t sign_u = asuint16(sgn) & FLT16_SIGN;
+  uint16_t mag_u = asuint16(mag) & FLT16_MAGNITUDE;
+  return asfloat16(sign_u | mag_u);
+}
+
+half2 CopySign(half2 mag, half2 sgn) {
+  uint16_t sign_x_u = asuint16(sgn.x) & FLT16_SIGN;
+  uint16_t sign_y_u = asuint16(sgn.y) & FLT16_SIGN;
+
+  uint16_t mag_x_u = asuint16(mag.x) & FLT16_MAGNITUDE;
+  uint16_t mag_y_u = asuint16(mag.y) & FLT16_MAGNITUDE;
+
+  return half2(
+      asfloat16(sign_x_u | mag_x_u),
+      asfloat16(sign_y_u | mag_y_u));
+}
+
+half3 CopySign(half3 mag, half3 sgn) {
+  uint16_t sign_x_u = asuint16(sgn.x) & FLT16_SIGN;
+  uint16_t sign_y_u = asuint16(sgn.y) & FLT16_SIGN;
+  uint16_t sign_z_u = asuint16(sgn.z) & FLT16_SIGN;
+
+  uint16_t mag_x_u = asuint16(mag.x) & FLT16_MAGNITUDE;
+  uint16_t mag_y_u = asuint16(mag.y) & FLT16_MAGNITUDE;
+  uint16_t mag_z_u = asuint16(mag.z) & FLT16_MAGNITUDE;
+
+  return half3(
+      asfloat16(sign_x_u | mag_x_u),
+      asfloat16(sign_y_u | mag_y_u),
+      asfloat16(sign_z_u | mag_z_u));
+}
+
+half4 CopySign(half4 mag, half4 sgn) {
+  uint16_t sign_x_u = asuint16(sgn.x) & FLT16_SIGN;
+  uint16_t sign_y_u = asuint16(sgn.y) & FLT16_SIGN;
+  uint16_t sign_z_u = asuint16(sgn.z) & FLT16_SIGN;
+  uint16_t sign_w_u = asuint16(sgn.w) & FLT16_SIGN;
+
+  uint16_t mag_x_u = asuint16(mag.x) & FLT16_MAGNITUDE;
+  uint16_t mag_y_u = asuint16(mag.y) & FLT16_MAGNITUDE;
+  uint16_t mag_z_u = asuint16(mag.z) & FLT16_MAGNITUDE;
+  uint16_t mag_w_u = asuint16(mag.w) & FLT16_MAGNITUDE;
+
+  return half4(
+      asfloat16(sign_x_u | mag_x_u),
+      asfloat16(sign_y_u | mag_y_u),
+      asfloat16(sign_z_u | mag_z_u),
+      asfloat16(sign_w_u | mag_w_u));
+}
+#else
+#define COPYSIGN_HALF_FUNCTION_GENERATOR(TYPE)     \
+  TYPE CopySign(TYPE mag, TYPE sgn) {              \
+    TYPE abs_value = abs(mag);                     \
+    return Select(sgn < 0, -abs_value, abs_value); \
+  }
+COPYSIGN_HALF_FUNCTION_GENERATOR(half)
+COPYSIGN_HALF_FUNCTION_GENERATOR(half2)
+COPYSIGN_HALF_FUNCTION_GENERATOR(half3)
+COPYSIGN_HALF_FUNCTION_GENERATOR(half4)
+#undef COPYSIGN_HALF_FUNCTION_GENERATOR
+#endif
+
 #define SIGNPOW_FUNCTION_GENERATOR(T)          \
-  T SignPow(T x, float exponent) {             \
+  T SignPow(T x, T exponent) {                 \
     return CopySign(pow(abs(x), exponent), x); \
   }
 SIGNPOW_FUNCTION_GENERATOR(float)
 SIGNPOW_FUNCTION_GENERATOR(float2)
 SIGNPOW_FUNCTION_GENERATOR(float3)
 SIGNPOW_FUNCTION_GENERATOR(float4)
+SIGNPOW_FUNCTION_GENERATOR(half)
+SIGNPOW_FUNCTION_GENERATOR(half2)
+SIGNPOW_FUNCTION_GENERATOR(half3)
+SIGNPOW_FUNCTION_GENERATOR(half4)
 #undef SIGNPOW_FUNCTION_GENERATOR
 
 #define SIGNSQRT_FUNCTION_GENERATOR(T) \
@@ -59,29 +134,31 @@ SIGNSQRT_FUNCTION_GENERATOR(float)
 SIGNSQRT_FUNCTION_GENERATOR(float2)
 SIGNSQRT_FUNCTION_GENERATOR(float3)
 SIGNSQRT_FUNCTION_GENERATOR(float4)
+SIGNSQRT_FUNCTION_GENERATOR(half)
+SIGNSQRT_FUNCTION_GENERATOR(half2)
+SIGNSQRT_FUNCTION_GENERATOR(half3)
+SIGNSQRT_FUNCTION_GENERATOR(half4)
 #undef SIGNSQRT_FUNCTION_GENERATOR
 
 #define CBRT_FUNCTION_GENERATOR(T) \
   T Cbrt(T x) {                    \
-    return SignPow(x, 1.f / 3.f);  \
+    return SignPow(x, 1.0 / 3.0);  \
   }
 CBRT_FUNCTION_GENERATOR(float)
 CBRT_FUNCTION_GENERATOR(float2)
 CBRT_FUNCTION_GENERATOR(float3)
 CBRT_FUNCTION_GENERATOR(float4)
+CBRT_FUNCTION_GENERATOR(half)
+CBRT_FUNCTION_GENERATOR(half2)
+CBRT_FUNCTION_GENERATOR(half3)
+CBRT_FUNCTION_GENERATOR(half4)
 #undef CBRT_FUNCTION_GENERATOR
 
-#if __SHADER_TARGET_MAJOR >= 6 || defined(VULKAN)
+// IEEE Float (0, +/-1, NaN) Sign function
 #define SIGN_FUNCTION_GENERATOR(T) \
   T Sign(T x) {                    \
     return sign(x);                \
   }
-#else
-#define SIGN_FUNCTION_GENERATOR(T)                          \
-  T Sign(T x) {                                             \
-    return mad(saturate(mad(x, FLT_MAX, 0.5f)), 2.f, -1.f); \
-  }
-#endif
 
 SIGN_FUNCTION_GENERATOR(float)
 SIGN_FUNCTION_GENERATOR(float2)
