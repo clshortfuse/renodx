2fde4035567966ff6b17fede7aa37c4799646b95
spiwar <92001312+spiwar@users.noreply.github.com>
2026-01-22 01:58:47 -0700

    feat(endfield): add hide UID (hardcoded) for now, add additional ubers, more settings

--- src/games/endfield/common.hlsl
+++ src/games/endfield/common.hlsl
@@ -6,8 +6,9 @@ static inline float3 AutoHDRVideo(float3 sdr_video) {
   }
   renodx::draw::Config config = renodx::draw::BuildConfig();
   config.peak_white_nits = RENODX_VIDEO_NITS;
-  
+
   float3 hdr_video = renodx::draw::UpscaleVideoPass(saturate(sdr_video), config);
   hdr_video = renodx::color::srgb::DecodeSafe(hdr_video);
-  return renodx::draw::RenderIntermediatePass(hdr_video);
+  hdr_video = renodx::draw::RenderIntermediatePass(hdr_video);
+  return hdr_video = max(0, hdr_video);  // bt709 clamp ;
 }
--- src/games/endfield/uberpost/uberpost_0x57737D9F.ps_5_0.hlsl
+++ src/games/endfield/uberpost/uberpost_0x57737D9F.ps_5_0.hlsl
@@ -1,4 +1,4 @@
-// ---- Created with 3Dmigoto v1.3.16 on Wed Jan 21 18:05:04 2026
+// ---- Created with 3Dmigoto v1.3.16 on Wed Jan 21 21:30:43 2026
 #include "../shared.h"
 Texture2D<float4> t2 : register(t2);
 
@@ -30,7 +30,7 @@ void main(
   float2 v1 : TEXCOORD0,
   out float4 o0 : SV_Target0)
 {
-  float4 r0,r1,r2,r3,r4,r5,r6;
+  float4 r0,r1,r2,r3,r4,r5;
   uint4 bitmask, uiDest;
   float4 fDest;
 
@@ -111,52 +111,20 @@ void main(
   r5.xy = -cb1[10].yx + r0.ww;
   r2.w = max(0, r5.x);
   r2.w = min(cb1[10].z, r2.w);
-  r5.xz = -cb1[1].xy + v1.xy;
-  r3.w = -cb1[2].x + 1;
-  r3.w = cb1[1].w * r3.w + cb1[2].x;
-  r5.xz = abs(r5.xz) * r3.ww;
-  r3.w = saturate(cb1[2].x * 1.04999995);
-  r3.w = r3.w * 1.5 + -1;
-  r3.w = cb1[1].w * r3.w + 1;
-  r3.w = r5.x * r3.w;
-  r4.w = cb1[2].x * 2 + -1;
-  r4.w = cb1[1].w * r4.w + 1;
-  r5.x = saturate(cb1[2].x + -2.79999995);
-  r5.x = 5 * r5.x;
-  r6.y = saturate(r5.z * r4.w + r5.x);
-  r4.w = cb0[82].x / cb0[82].y;
-  r5.x = -1 + r4.w;
-  r5.x = cb1[2].w * r5.x + 1;
-  r4.w = r4.w * 0.5625 + -r5.x;
-  r4.w = cb1[1].w * r4.w + r5.x;
-  r6.x = saturate(r4.w * r3.w);
-  r5.xz = log2(r6.xy);
-  r5.xz = cb1[2].zz * r5.xz;
-  r5.xz = exp2(r5.xz);
   r2.w = r2.w * r2.w;
   r2.w = cb1[10].w * r2.w;
   r2.w = max(r2.w, r5.y);
   r0.w = max(9.99999975e-005, r0.w);
   r0.w = r2.w / r0.w;
-  r6.xyz = r0.xyz * r0.www;
-  r6.xyz = -r6.xyz * cb1[9].zzz + r0.xyz;
+  r5.xyz = r0.xyz * r0.www;
+  r5.xyz = -r5.xyz * cb1[9].zzz + r0.xyz;
   r1.xyz = r3.xyz ? r4.xyz : r1.zxy;
-  r1.xyz = r1.xyz * cb1[11].zxy + r6.xyz;
+  r1.xyz = r1.xyz * cb1[11].zxy + r5.xyz;
   r1.xyz = -r2.zxy * cb0[109].xxx + r1.xyz;
   r0.xyz = cb1[9].xxx * r1.xyz + r0.xyz;
-  r0.w = dot(r5.xz, r5.xz);
-  r0.w = 1 + -r0.w;
-  r0.w = max(0, r0.w);
-  r0.w = log2(r0.w);
-  r0.w = cb1[2].y * r0.w;
-  r0.w = exp2(r0.w);
-  r1.xyz = -cb1[4].zxy + float3(1,1,1);
-  r1.xyz = r0.www * r1.xyz + cb1[4].zxy;
-  r0.xyz = r1.xyz * r0.xyz;
   r0.xyz = cb1[7].www * r0.xyz;
-  [branch]
-  if (shader_injection.tone_map_type == 0.f) {
-
+  // Define untonemapped
+  float3 untonemapped = r0.yzx;
   r0.xyz = r0.xyz * float3(5.55555582,5.55555582,5.55555582) + float3(0.0479959995,0.0479959995,0.0479959995);
   r0.xyz = max(float3(0,0,0), r0.xyz);
   r0.xyz = log2(r0.xyz);
@@ -187,21 +155,10 @@ void main(
   r1.xyz = frac(r1.xyz);
   r1.xyz = float3(-0.5,-0.5,-0.5) + r1.xyz;
   o0.xyz = r1.xyz * float3(0.0013725491, 0.0013725491, 0.0013725491) + r0.xyz;
-  o0.xyz = renodx::color::srgb::DecodeSafe(o0.xyz);
+  // sRGB Decode and output
+  o0.xyz = renodx::color::srgb::Decode(o0.xyz);
+  o0.xyz = renodx::draw::ToneMapPass(untonemapped, o0.xyz);
   o0.xyz = renodx::draw::RenderIntermediatePass(o0.xyz);
-  } else {
-    renodx::lut::Config lut_config = renodx::lut::config::Create(
-        s0_s,
-        1.0f,
-        0.f,
-        renodx::lut::config::type::ARRI_C1000_NO_CUT,
-        renodx::lut::config::type::LINEAR
-    );
-
-    float3 graded = renodx::lut::Sample(t2, lut_config, r0.yzx);
-    o0.xyz = renodx::draw::ToneMapPass(r0.yzx,graded.xyz);
-    o0.xyz = renodx::draw::RenderIntermediatePass(o0.xyz);
-  }
   o0.w = min(1, r1.w);
   return;
 }
