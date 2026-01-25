3fe5ed8b1c9a16e12273a540452d3b2cf237a399
MaxG2D <maksgenewicz@gmail.com>
2026-01-25 15:38:56 +0100

    Basic support for Call of Juarez (DX10). Sadly, the game crashes when quick saving, autosaving, or pressing ESC, because it tries to read framebuffer for screenshot, which does not handle float16, which makes the mod pretty much unplayable for now.

---
+++ src/games/callofjuarez/DOFBlend_0x6AFD019B.ps_4_0.hlsl
@@ -0,0 +1,41 @@
+#include "./shared.h"
+
+// ---- Created with 3Dmigoto v1.2.45 on Sat Jan 24 00:40:17 2026
+
+SamplerState samScreen_s : register(s0);
+SamplerState samScreen_Blurred_s : register(s1);
+Texture2D<float4> sScreen : register(t0);
+Texture2D<float4> sScreen_Blurred : register(t1);
+
+
+// 3Dmigoto declarations
+#define cmp -
+Texture1D<float4> IniParams : register(t120);
+Texture2D<float4> StereoParams : register(t125);
+
+
+void main( 
+  float4 v0 : SV_POSITION0,
+  float4 v1 : TEXCOORD0,
+  out float4 o0 : SV_TARGET0)
+{
+  float4 r0,r1;
+  uint4 bitmask, uiDest;
+  float4 fDest;
+
+  r0.xyzw = sScreen_Blurred.Sample(samScreen_Blurred_s, v1.xy).xyzw;
+  //r0.xyz = saturate(r0.xyzw);
+  r1.xyzw = sScreen.Sample(samScreen_s, v1.xy).xyzw;
+  //r1.xyzw = saturate(r1.xyzw);
+  r1.xyzw = max(0, r1.xyzw);
+  r0.xyz = max(0, -r1.xyz + r0.xyz);
+  //r0.w = max(0, r0.w);
+  r0.w = r1.w * 2 + -1;
+  r0.w = max(0, r0.w);
+  //o0.xyz = saturate(r0.www * r0.xyz + r1.xyz);
+  o0.xyz = max(0, r0.www * r0.xyz + r1.xyz);
+  r0.x = r1.w * -2 + 1;
+  o0.w = max(0, r0.x); // Alpha clamp
+
+  return;
+}
---
+++ src/games/callofjuarez/ToneMapper_0x72C84A10.ps_4_0.hlsl
@@ -0,0 +1,46 @@
+#include "./shared.h"
+
+cbuffer _Globals : register(b0)
+{
+  float4 CONST_4 : packoffset(c0);
+  float fhdrglowthreshold : packoffset(c1);
+  float4 RENDER_TARGET_PARAMS : packoffset(c2);
+}
+
+SamplerState samColorAVG_s : register(s0);
+Texture2D<float4> sColorAVG : register(t0);
+Texture2D<float4> sColorHDR : register(t1);
+
+void main(
+  float4 v0 : SV_POSITION0,
+  out float4 o0 : SV_TARGET0)
+{
+  float4 r0, r1, r2, r3;
+  r0.xy = (int2)v0.xy;  // ftoi r0.xy, v0.xyxx
+  r0.zw = float2(0,0);
+  r0.xyzw = sColorHDR.Load(r0.xyz);  // r0 is now HDR Color
+  float3 untonemapped = max(r0.xyz, 0.0);
+  untonemapped = renodx::color::srgb::Decode(untonemapped);
+  r1.xyzw = sColorAVG.Sample(samColorAVG_s, float2(0.5, 0.5)); // Exposure
+  r2.xyz = r0.xyz * r1.xyz; // Apply Exposure
+  r3.xyz = r2.xyz * CONST_4.w + 1.0; // Brightness
+  r2.xyz = r2.xyz * r3.xyz; // Contrast
+  r3.xyz = (r0.xyz * r1.xyz) + 1.0; // For Glow Threshold
+  r0.xyz = (-r1.xyz * fhdrglowthreshold) + r0.xyz; // Subtract Glow Threshold
+  o0.xyz = saturate(r2.xyz / r3.xyz); // Reinhard, my beloved
+  //o0.xyz = (r2.xyz / r3.xyz);
+  r0.xyz = max(r0.xyz, 0.0);
+  o0.w = renodx::color::y::from::BT709(o0.rgb);  // r0.x = dot(float3(0.2125, 0.7154, 0.0721), r0.xyz);
+  o0.w = min(r0.x, 1.0);  // Alpha channel clamp
+  float4 vanilla = o0.xyzw;
+  float3 tonemapped = renodx::tonemap::renodrt::NeutralSDR(untonemapped);
+  //tonemapped = renodx::color::srgb::Decode(tonemapped);
+  if (RENODX_TONE_MAP_TYPE > 0.f) {
+    o0.rgb = renodx::draw::ToneMapPass(untonemapped, vanilla.xyz, tonemapped);;
+  } else {
+    o0 = vanilla;
+  }
+  o0.rgb = renodx::draw::RenderIntermediatePass(o0.rgb);
+
+  return;
+}
