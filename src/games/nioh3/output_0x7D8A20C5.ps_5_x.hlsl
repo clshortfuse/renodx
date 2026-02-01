e37772e9927f8b3a0722cf7bae1591566e94c2c1
Mohanned Hassan <gailardiagalane@gmail.com>
2026-02-01 21:55:13 +0400

    feat(nioh3): Fix vignette, and dramatic luts.

--- src/games/nioh3/output_0x7D8A20C5.ps_5_x.hlsl
+++ src/games/nioh3/output_0x7D8A20C5.ps_5_x.hlsl
@@ -37,20 +37,6 @@ Texture3D<float4> g_tLdrLut : register(t4);
 // 3Dmigoto declarations
 #define cmp -
 
-/// Applies Exponential Roll-Off tonemapping using the maximum channel.
-/// Used to fit the color into a 0–output_max range for SDR LUT compatibility.
-float3 ToneMapMaxCLL(float3 color, float rolloff_start = 0.5f, float output_max = 1.f) {
-  float peak = max(color.r, max(color.g, color.b));
-  peak = min(peak, 100.f);
-  float log_peak = log2(peak);
-
-  // Apply exponential shoulder in log space
-  float log_mapped = renodx::tonemap::ExponentialRollOff(log_peak, log2(rolloff_start), log2(output_max));
-  float scale = exp2(log_mapped - log_peak);  // How much to compress all channels
-
-  return min(output_max, color * scale);
-}
-
 void main(
     float4 v0: SV_Position0,
     float2 v1: TEXCOORD0,
@@ -180,7 +166,6 @@ void main(
     r0.x = r0.x * r0.y;
     r0.y = 1 + -g_vLimbDarkenningInfo.w;
     r0.x = r0.x * g_vLimbDarkenningInfo.w + r0.y;
-    r0.x = lerp(1.f, r0.x, CUSTOM_VIGNETTE);
     r3.xyz = r3.xyz * r0.xxx;
   }
   if (r1.x != 0) {
@@ -220,6 +205,8 @@ void main(
       r0.x = r0.x * r0.x;
       r1.x = 1 + -g_vVerticalLimbDarkenningBottomInfo.x;
       r0.x = r0.x * g_vVerticalLimbDarkenningBottomInfo.x + r1.x;
+      r0.x = lerp(1.f, r0.x, CUSTOM_VIGNETTE);
+
       r0.y = r0.z ? r0.x : 1;
     }
     r3.xyz = r3.xyz * r0.yyy;
