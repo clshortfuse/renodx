8623f66365d8dcc3c1f15618c170c7e37a7cf106
Musa Haji <mqhaji@vt.edu>
2026-02-07 02:02:42 -0500

    feat(re4remake): use neutwo, fix FXAA, adjust grading, add lum 2.2, compress gamut, add auto peak

--- src/games/re4remake/shared.h
+++ src/games/re4remake/shared.h
@@ -1,6 +1,8 @@
 #ifndef SRC_RE4REMAKE_SHARED_H_
 #define SRC_RE4REMAKE_SHARED_H_
 
+#define GAMUT_COMPRESS 1
+
 // Must be 32bit aligned
 // Should be 4x32
 struct ShaderInjectData {
@@ -10,7 +12,7 @@ struct ShaderInjectData {
   float diffuse_white_nits;
   float graphics_white_nits;
   float gamma_adjust;
-  float tone_map_per_channel_blowout_restoration;
+  float tone_map_blowout;
   float tone_map_hue_shift;
   float tone_map_highlight_contrast;
   float tone_map_toe_adjustment_type;
@@ -22,7 +24,7 @@ struct ShaderInjectData {
   float tone_map_contrast;
   float tone_map_saturation;
   float tone_map_highlight_saturation;
-  float tone_map_blowout;
+  float tone_map_dechroma;
   float tone_map_flare;
   float color_grade_lut_strength;
   float color_grade_lut_scaling;
@@ -36,17 +38,17 @@ cbuffer cb13 : register(b0, space50) {
   ShaderInjectData shader_injection : packoffset(c0);
 }
 
-#define TONE_MAP_TYPE                          shader_injection.tone_map_type
-#define RENODX_PEAK_WHITE_NITS                 shader_injection.peak_white_nits
-#define RENODX_DIFFUSE_WHITE_NITS              shader_injection.diffuse_white_nits
-#define RENODX_GRAPHICS_WHITE_NITS             shader_injection.graphics_white_nits
-#define RENODX_PER_CHANNEL_BLOWOUT_RESTORATION shader_injection.tone_map_per_channel_blowout_restoration
-#define RENODX_TONE_MAP_HUE_SHIFT              shader_injection.tone_map_hue_shift
-#define RENODX_GAMMA_CORRECTION                shader_injection.gamma_correction
-#define RENODX_GAMMA_ADJUST                    shader_injection.gamma_adjust
-#define RENODX_TONE_MAP_HIGHLIGHT_CONTRAST     shader_injection.tone_map_highlight_contrast
-#define RENODX_TONE_MAP_TOE_ADJUSTMENT_TYPE    shader_injection.tone_map_toe_adjustment_type
-#define RENODX_TONE_MAP_SHADOW_TOE             shader_injection.tone_map_shadow_toe
+#define TONE_MAP_TYPE                       shader_injection.tone_map_type
+#define RENODX_PEAK_WHITE_NITS              shader_injection.peak_white_nits
+#define RENODX_DIFFUSE_WHITE_NITS           shader_injection.diffuse_white_nits
+#define RENODX_GRAPHICS_WHITE_NITS          shader_injection.graphics_white_nits
+#define RENODX_TONE_MAP_BLOWOUT             shader_injection.tone_map_blowout
+#define RENODX_TONE_MAP_HUE_SHIFT           shader_injection.tone_map_hue_shift
+#define RENODX_GAMMA_CORRECTION             shader_injection.gamma_correction
+#define RENODX_GAMMA_ADJUST                 shader_injection.gamma_adjust
+#define RENODX_TONE_MAP_HIGHLIGHT_CONTRAST  shader_injection.tone_map_highlight_contrast
+#define RENODX_TONE_MAP_TOE_ADJUSTMENT_TYPE shader_injection.tone_map_toe_adjustment_type
+#define RENODX_TONE_MAP_SHADOW_TOE          shader_injection.tone_map_shadow_toe
 
 #define RENODX_TONE_MAP_EXPOSURE             shader_injection.tone_map_exposure
 #define RENODX_TONE_MAP_HIGHLIGHTS           shader_injection.tone_map_highlights
@@ -54,7 +56,7 @@ cbuffer cb13 : register(b0, space50) {
 #define RENODX_TONE_MAP_CONTRAST             shader_injection.tone_map_contrast
 #define RENODX_TONE_MAP_SATURATION           shader_injection.tone_map_saturation
 #define RENODX_TONE_MAP_HIGHLIGHT_SATURATION shader_injection.tone_map_highlight_saturation
-#define RENODX_TONE_MAP_BLOWOUT              shader_injection.tone_map_blowout
+#define RENODX_TONE_MAP_DECHROMA             shader_injection.tone_map_dechroma
 #define RENODX_TONE_MAP_FLARE                shader_injection.tone_map_flare
 #define COLOR_GRADE_LUT_STRENGTH             shader_injection.color_grade_lut_strength
 #define COLOR_GRADE_LUT_SCALING              shader_injection.color_grade_lut_scaling
