faef486ceda3ec652ce8449c8a8176362b5b8f1e
marat569 <marat569@gmail.com>
2026-03-06 18:36:15 -0500

    feat(nioh3): Add bt1886 as an EOTF emulation

--- src/games/nioh3/output_0x7D8A20C5.ps_5_1.hlsl
+++ src/games/nioh3/output_0x7D8A20C5.ps_5_1.hlsl
@@ -241,9 +241,12 @@ void main(
   // r1.xyz = g_vGammaCorrection.xxx * r1.xyz;
   // r1.xyz = exp2(r1.xyz);
   // r0.xyz = r0.www ? r1.xyz : r0.xyz;
+
   o0.xyz = g_vRadialBlurCenter.zzz * r0.xyz;
+
   o0.w = 1;
 
   o0 = ProcessColor(o0);
+
   return;
 }
