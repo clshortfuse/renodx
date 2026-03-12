ff5924b848ae082c6cbac2a4c2f6966f8c351933
Musa Haji <mqhaji@vt.edu>
2026-03-12 12:31:38 -0400

    feat(re7-2r-3r-village): use aspect ratio rg11b10 upgrades

--- src/games/re7-2r-3r-village/addon.cpp
+++ src/games/re7-2r-3r-village/addon.cpp
@@ -49,8 +49,7 @@ void OnInitDevice(reshade::api::device* device) {
       upgrade_infos.push_back({
           .old_format = reshade::api::format::r11g11b10_float,
           .new_format = reshade::api::format::r16g16b16a16_float,
-          .dimensions = {.width = renodx::utils::resource::ResourceUpgradeInfo::BACK_BUFFER,
-                         .height = renodx::utils::resource::ResourceUpgradeInfo::BACK_BUFFER},
+          .aspect_ratio = renodx::utils::resource::ResourceUpgradeInfo::BACK_BUFFER,
           .usage_include = reshade::api::resource_usage::render_target,
       });
     }
