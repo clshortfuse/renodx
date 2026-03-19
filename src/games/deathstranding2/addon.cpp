4ce8dd6a4254d6940beb689320d7b076f99eba5c
Musa Haji <mqhaji@vt.edu>
2026-03-19 14:19:59 -0400

    fix(deathstranding2): fix game name in description

--- src/games/deathstranding2/addon.cpp
+++ src/games/deathstranding2/addon.cpp
@@ -360,7 +360,7 @@ bool initialized = false;
 }  // namespace
 
 extern "C" __declspec(dllexport) constexpr const char* NAME = "RenoDX";
-extern "C" __declspec(dllexport) constexpr const char* DESCRIPTION = "RenoDX for Death Stranding 2";
+extern "C" __declspec(dllexport) constexpr const char* DESCRIPTION = "RenoDX for DEATH STRANDING 2: ON THE BEACH";
 
 BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
   switch (fdw_reason) {
