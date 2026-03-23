db1933e5db0f3f5f929c24eafed88176755fcaf0
Musa Haji <mqhaji@vt.edu>
2026-03-23 15:59:40 -0400

    feat(spiderman_2018_miles): add shader toggle

--- src/games/spiderman_2018_miles/addon.cpp
+++ src/games/spiderman_2018_miles/addon.cpp
@@ -21,7 +21,74 @@ namespace {
 
 renodx::mods::shader::CustomShaders custom_shaders = {__ALL_CUSTOM_SHADERS};
 
+#if ENABLE_SHADER_TOGGLE
+namespace shader_toggle {
+float g_use_shaders = 1.f;          // Controlled by slider
+float g_current_use_shaders = 1.f;  // Will be overridden on startup
+
+void OnPresent(
+    reshade::api::command_queue*,
+    reshade::api::swapchain* swapchain,
+    const reshade::api::rect*,
+    const reshade::api::rect*,
+    uint32_t,
+    const reshade::api::rect*) {
+  if (g_use_shaders != g_current_use_shaders) {
+    reshade::log::message(
+        reshade::log::level::info,
+        (g_use_shaders != 0.f) ? "Enabling shaders (toggle)" : "Disabling shaders (toggle)");
+
+    auto* device = swapchain->get_device();
+    if (device == nullptr) {
+      reshade::log::message(reshade::log::level::error, "Device is null in OnPresent");
+      g_current_use_shaders = g_use_shaders;
+      return;
+    }
+
+    if (g_use_shaders != 0.f) {
+      for (const auto& [hash, shader] : custom_shaders) {
+        renodx::utils::shader::AddRuntimeReplacement(device, hash, shader.code);
+      }
+      reshade::log::message(
+          reshade::log::level::info,
+          ("Injected " + std::to_string(custom_shaders.size()) + " shaders").c_str());
+    } else {
+      renodx::utils::shader::RemoveRuntimeReplacements(device);
+      reshade::log::message(reshade::log::level::info, "Removed all shader replacements.");
+    }
+
+    g_current_use_shaders = g_use_shaders;
+  }
+}
+
+void Initialize() {
+  g_current_use_shaders = -1.0f;
+  reshade::register_event<reshade::addon_event::present>(OnPresent);
+}
+
+void Cleanup() {
+  reshade::unregister_event<reshade::addon_event::present>(OnPresent);
+}
+
+renodx::utils::settings::Setting* GetSetting() {
+  return new renodx::utils::settings::Setting{
+      .binding = &g_use_shaders,
+      .value_type = renodx::utils::settings::SettingValueType::BOOLEAN,
+      .default_value = 1.f,
+      .label = "Enable Mod",
+      .section = "Options",
+      .on_change = []() {
+        g_current_use_shaders = -1.f;
+      },
+  };
+}
+}  // namespace shader_toggle
+#endif
+
 renodx::utils::settings::Settings settings = {
+#if ENABLE_SHADER_TOGGLE
+    shader_toggle::GetSetting(),
+#endif
     new renodx::utils::settings::Setting{
         .value_type = renodx::utils::settings::SettingValueType::BUTTON,
         .label = "RenoDX Discord",
@@ -90,6 +157,8 @@ renodx::utils::settings::Settings settings = {
     },
 };
 
+bool initialized = false;
+
 }  // namespace
 
 extern "C" __declspec(dllexport) constexpr const char* NAME = "RenoDX";
@@ -99,12 +168,21 @@ BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
   switch (fdw_reason) {
     case DLL_PROCESS_ATTACH:
 
-      renodx::mods::shader::expected_constant_buffer_space = 50;
-      renodx::utils::settings::use_presets = false;
+      if (!initialized) {
+        renodx::mods::shader::expected_constant_buffer_space = 50;
+        renodx::utils::settings::use_presets = false;
+        renodx::utils::shader::use_replace_async = true;
+      }
 
       if (!reshade::register_addon(h_module)) return FALSE;
+#if ENABLE_SHADER_TOGGLE
+      shader_toggle::Initialize();
+#endif
       break;
     case DLL_PROCESS_DETACH:
+#if ENABLE_SHADER_TOGGLE
+      shader_toggle::Cleanup();
+#endif
       reshade::unregister_addon(h_module);
       break;
   }
