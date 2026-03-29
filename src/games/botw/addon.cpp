9828a93af128bb7199859a2e3fc5ecdd6fb027b0
Souperman9 <35243266+Souperman9@users.noreply.github.com>
2026-03-30 03:02:06 +1100

    botw: initial commit

---
+++ src/games/botw/addon.cpp
@@ -0,0 +1,223 @@
+/*
+ * Copyright (C) 2024 Carlos Lopez
+ * SPDX-License-Identifier: MIT
+ */
+
+#define ImTextureID ImU64
+#define RENODX_MODS_SWAPCHAIN_VERSION 2
+
+
+#include <embed/shaders.h>
+
+#include <deps/imgui/imgui.h>
+#include <include/reshade.hpp>
+
+#include "../../mods/shader.hpp"
+#include "../../mods/swapchain.hpp"
+#include "../../templates/settings.hpp"
+#include "../../utils/date.hpp"
+#include "../../utils/random.hpp"
+#include "../../utils/settings.hpp"
+#include "./shared.h"
+
+namespace {
+
+renodx::mods::shader::CustomShaders custom_shaders = {__ALL_CUSTOM_SHADERS};
+
+ShaderInjectData shader_injection;
+
+
+renodx::utils::settings::Settings settings = renodx::templates::settings::JoinSettings({
+    renodx::templates::settings::CreateDefaultSettings({
+        {"ToneMapType", &shader_injection.tone_map_type},
+        {"ToneMapPeakNits", &shader_injection.peak_white_nits},
+        {"ToneMapGameNits", &shader_injection.diffuse_white_nits},
+        {"ToneMapUINits", &shader_injection.graphics_white_nits},
+        {"ToneMapGammaCorrection", &shader_injection.gamma_correction},
+        {"ColorGradeExposure", &shader_injection.tone_map_exposure},
+        {"ColorGradeHighlights", &shader_injection.tone_map_highlights},
+        {"ColorGradeShadows", &shader_injection.tone_map_shadows},
+        {"ColorGradeContrast", &shader_injection.tone_map_contrast},
+        {"ColorGradeSaturation", &shader_injection.tone_map_saturation},
+        {"ColorGradeHighlightSaturation", &shader_injection.tone_map_highlight_saturation},
+        {"ColorGradeBlowout", &shader_injection.tone_map_blowout},
+        {"ColorGradeFlare", &shader_injection.tone_map_flare},
+    }),
+    {
+        // new renodx::utils::settings::Setting{
+        //     .key = "FxBloom",
+        //     .binding = &shader_injection.custom_bloom,
+        //     .default_value = 25.f,
+        //     .label = "Bloom Strength",
+        //     .section = "Color Grading",
+        //     .max = 100.f,
+        //     .parse = [](float value) { return value * 0.01f; },
+        //     .is_visible = []() { return settings[0]->GetValue() >= 2.f; },
+        // },
+        new renodx::utils::settings::Setting{
+            .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+            .label = "RenoDX Discord",
+            .section = "Links",
+            .group = "button-line-1",
+            .tint = 0x5865F2,
+            .on_change = []() {
+              renodx::utils::platform::LaunchURL("https://discord.gg/kSTf", "EbcCpC");
+            },
+        },
+        new renodx::utils::settings::Setting{
+            .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+            .label = "HDR Den Discord",
+            .section = "Links",
+            .group = "button-line-1",
+            .tint = 0x5865F2,
+            .on_change = []() {
+              renodx::utils::platform::LaunchURL("https://discord.gg/XUhv", "tR54yc");
+            },
+        },
+        new renodx::utils::settings::Setting{
+            .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+            .label = "Github",
+            .section = "Links",
+            .group = "button-line-1",
+            .on_change = []() {
+              renodx::utils::platform::LaunchURL("https://github.com/clshortfuse/renodx");
+            },
+        },
+        new renodx::utils::settings::Setting{
+            .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+            .label = "Ritsu's Ko-Fi",
+            .section = "Links",
+            .group = "button-line-1",
+            .tint = 0xFF5F5F,
+            .on_change = []() {
+              renodx::utils::platform::LaunchURL("https://ko-fi.com/ritsucecil");
+            },
+        },
+        new renodx::utils::settings::Setting{
+            .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+            .label = "ShortFuse's Ko-Fi",
+            .section = "Links",
+            .group = "button-line-1",
+            .tint = 0xFF5F5F,
+            .on_change = []() {
+              renodx::utils::platform::LaunchURL("https://ko-fi.com/shortfuse");
+            },
+        },
+        new renodx::utils::settings::Setting{
+            .value_type = renodx::utils::settings::SettingValueType::BUTTON,
+            .label = "HDR Den's Ko-Fi",
+            .section = "Links",
+            .group = "button-line-1",
+            .tint = 0xFF5F5F,
+            .on_change = []() {
+              renodx::utils::platform::LaunchURL("https://ko-fi.com/hdrden");
+            },
+        },
+        new renodx::utils::settings::Setting{
+            .value_type = renodx::utils::settings::SettingValueType::TEXT,
+            .label = "Game mod by souperman9, RenoDX Framework by ShortFuse, RenoVK fork by Ritsu.",
+            .section = "About",
+        },
+        new renodx::utils::settings::Setting{
+            .value_type = renodx::utils::settings::SettingValueType::TEXT,
+            .label = std::string("Build: ") + renodx::utils::date::ISO_DATE_TIME,
+            .section = "About",
+        },
+    },
+    
+});
+
+
+
+void OnPresent(
+    reshade::api::command_queue* queue,
+    reshade::api::swapchain* swapchain,
+    const reshade::api::rect* source_rect,
+    const reshade::api::rect* dest_rect,
+    uint32_t dirty_rect_count,
+    const reshade::api::rect* dirty_rects) {
+  // Reset frame state
+}
+
+
+
+
+
+void OnPresetOff() {
+  renodx::utils::settings::UpdateSettings({
+      {"ToneMapType", 0.f},
+      {"ToneMapPeakNits", 203.f},
+      {"ToneMapGameNits", 203.f},
+      {"ToneMapUINits", 203.f},
+      {"ToneMapGammaCorrection", 0.f},
+      {"ColorGradeExposure", 1.f},
+      {"ColorGradeHighlights", 50.f},
+      {"ColorGradeShadows", 50.f},
+      {"ColorGradeContrast", 50.f},
+      {"ColorGradeSaturation", 50.f},
+      {"ColorGradeHighlightSaturation", 50.f},
+      {"ColorGradeBlowout", 0.f},
+      {"ColorGradeFlare", 0.f},
+  });
+}
+
+bool initialized = false;
+
+}  // namespace
+
+extern "C" __declspec(dllexport) constexpr const char* NAME = "RenoDX";
+extern "C" __declspec(dllexport) constexpr const char* DESCRIPTION = "RenoDX for Breath of the Wild";
+
+BOOL APIENTRY DllMain(HMODULE h_module, DWORD fdw_reason, LPVOID lpv_reserved) {
+  const auto target_format = reshade::api::format::r16g16b16a16_float;
+  const auto view_upgrades = renodx::utils::resource::VIEW_UPGRADES_RGBA16F;
+  switch (fdw_reason) {
+    case DLL_PROCESS_ATTACH:
+      if (!reshade::register_addon(h_module)) return FALSE;
+
+      // Always set to true for Vulkan
+      renodx::mods::shader::allow_multiple_push_constants = true;
+      renodx::mods::swapchain::use_resource_cloning = false;
+      renodx::mods::swapchain::target_format = target_format;
+
+      /*
+        True means it'll attempt to expand current cbuffer definitions instead of adding a new push constant
+        entry. You'll have to experiment with this if cbuffer injection doesn't work
+      */
+      renodx::mods::shader::expand_existing_constant_buffer = true;
+
+      /*
+        If expand_existing_constant_buffer is set to false renoDX will add new cbuffer range (instead of reusing the game's).
+        This behaviour is overridden if renoDX finds a cbuffer that targets all shader_stages in minimum_constant_buffer_stages.
+        e.g. If a game's cbuffer range targets all stages, renoDX will expand it regardless of expand_existing_constant_buffer value.
+        Remove the stages you're not injecting to.
+      */
+      renodx::mods::shader::minimum_constant_buffer_stages = reshade::api::shader_stage::pixel;
+
+        renodx::mods::swapchain::resource_upgrade_infos.push_back({
+            .old_format = reshade::api::format::r10g10b10a2_typeless,
+            .new_format = target_format,
+        });
+
+      if (!initialized) {
+        // renodx::utils::random::binds.push_back(&shader_injection.swap_chain_output_dither_seed);
+        initialized = true;
+      }
+
+      // Register event handlers
+      reshade::register_event<reshade::addon_event::present>(OnPresent);
+
+      break;
+    case DLL_PROCESS_DETACH:
+      reshade::unregister_event<reshade::addon_event::present>(OnPresent);
+      reshade::unregister_addon(h_module);
+      break;
+  }
+
+  // renodx::utils::random::Use(DLL_PROCESS_ATTACH);
+  renodx::mods::swapchain::Use(fdw_reason);
+  renodx::utils::settings::Use(fdw_reason, &settings, &OnPresetOff);
+  renodx::mods::shader::Use(fdw_reason, custom_shaders, &shader_injection);
+
+  return TRUE;
+}
