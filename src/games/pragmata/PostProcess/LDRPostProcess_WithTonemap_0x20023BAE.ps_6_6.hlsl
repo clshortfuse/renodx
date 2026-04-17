c7d0e53885986ca249a7a89448e53db9f31f5ca6
Musa Haji <mqhaji@vt.edu>
2026-04-17 01:04:19 -0400

    feat(pragmata): enable `Apply Pre Tone Map Curve` slider in every post process shader

--- src/games/pragmata/PostProcess/LDRPostProcess_WithTonemap_0x20023BAE.ps_6_6.hlsl
+++ src/games/pragmata/PostProcess/LDRPostProcess_WithTonemap_0x20023BAE.ps_6_6.hlsl
@@ -1,4 +1,5 @@
-#include "../common.hlsli"
+#define TONEMAP_PARAM_REGISTER b1
+#include "./PostProcess.hlsli"
 
 Texture2D<float> ReadonlyDepth : register(t0);
 
@@ -10,7 +11,7 @@ Texture3D<float> tVolumeMap : register(t3);
 
 Texture2D<float2> HazeNoiseResult : register(t4);
 
-struct RadialBlurComputeResult {
+struct RadialBlurComputeResult {  // decomp missed this
   float computeAlpha;
 };
 StructuredBuffer<RadialBlurComputeResult> ComputeResultSRV : register(t5);
@@ -50,38 +51,37 @@ cbuffer SceneInfo : register(b0) {
   uint4 rayTracingParams : packoffset(c036.x);
   float4 sceneExtendedData : packoffset(c037.x);
   float2 projectionSpaceJitterOffset : packoffset(c038.x);
-  uint blueNoiseJitterIndex : packoffset(c038.z);
-  float tessellationParam : packoffset(c038.w);
-  uint sceneInfoAdditionalFlags : packoffset(c039.x);
+  float tessellationParam : packoffset(c038.z);
+  uint sceneInfoAdditionalFlags : packoffset(c038.w);
 };
 
-cbuffer TonemapParam : register(b1) {
-  float contrast : packoffset(c000.x);
-  float linearBegin : packoffset(c000.y);
-  float linearLength : packoffset(c000.z);
-  float toe : packoffset(c000.w);
-  float maxNit : packoffset(c001.x);
-  float linearStart : packoffset(c001.y);
-  float displayMaxNitSubContrastFactor : packoffset(c001.z);
-  float contrastFactor : packoffset(c001.w);
-  float mulLinearStartContrastFactor : packoffset(c002.x);
-  float invLinearBegin : packoffset(c002.y);
-  float madLinearStartContrastFactor : packoffset(c002.z);
-  float tonemapParam_isHDRMode : packoffset(c002.w);
-  float useDynamicRangeConversion : packoffset(c003.x);
-  float useHuePreserve : packoffset(c003.y);
-  float exposureScale : packoffset(c003.z);
-  float kneeStartNit : packoffset(c003.w);
-  float knee : packoffset(c004.x);
-  float curve_HDRip : packoffset(c004.y);
-  float curve_k2 : packoffset(c004.z);
-  float curve_k4 : packoffset(c004.w);
-  row_major float4x4 RGBToXYZViaCrosstalkMatrix : packoffset(c005.x);
-  row_major float4x4 XYZToRGBViaCrosstalkMatrix : packoffset(c009.x);
-  float tonemapGraphScale : packoffset(c013.x);
-  float offsetEVCurveStart : packoffset(c013.y);
-  float offsetEVCurveRange : packoffset(c013.z);
-};
+// cbuffer TonemapParam : register(b1) {
+//   float contrast : packoffset(c000.x);
+//   float linearBegin : packoffset(c000.y);
+//   float linearLength : packoffset(c000.z);
+//   float toe : packoffset(c000.w);
+//   float maxNit : packoffset(c001.x);
+//   float linearStart : packoffset(c001.y);
+//   float displayMaxNitSubContrastFactor : packoffset(c001.z);
+//   float contrastFactor : packoffset(c001.w);
+//   float mulLinearStartContrastFactor : packoffset(c002.x);
+//   float invLinearBegin : packoffset(c002.y);
+//   float madLinearStartContrastFactor : packoffset(c002.z);
+//   float tonemapParam_isHDRMode : packoffset(c002.w);
+//   float useDynamicRangeConversion : packoffset(c003.x);
+//   float useHuePreserve : packoffset(c003.y);
+//   float exposureScale : packoffset(c003.z);
+//   float kneeStartNit : packoffset(c003.w);
+//   float knee : packoffset(c004.x);
+//   float curve_HDRip : packoffset(c004.y);
+//   float curve_k2 : packoffset(c004.z);
+//   float curve_k4 : packoffset(c004.w);
+//   row_major float4x4 RGBToXYZViaCrosstalkMatrix : packoffset(c005.x);
+//   row_major float4x4 XYZToRGBViaCrosstalkMatrix : packoffset(c009.x);
+//   float tonemapGraphScale : packoffset(c013.x);
+//   float offsetEVCurveStart : packoffset(c013.y);
+//   float offsetEVCurveRange : packoffset(c013.z);
+// };
 
 cbuffer LDRPostProcessParam : register(b2) {
   float fHazeFilterStart : packoffset(c000.x);
@@ -165,7 +165,6 @@ cbuffer CBControl : register(b3) {
   } cbControlRGCParam: packoffset(c005.x);
 };
 // clang-format on
-
 
 SamplerState PointClamp : register(s1, space32);
 
@@ -1342,3 +1341,4 @@ float4 main(
   SV_Target.w = 0.0f;
   return SV_Target;
 }
+
--- src/games/pragmata/PostProcess/LDRPostProcess_WithTonemap_0xB4456C38.ps_6_6.hlsl
+++ src/games/pragmata/PostProcess/LDRPostProcess_WithTonemap_0xB4456C38.ps_6_6.hlsl
@@ -1,4 +1,5 @@
-#include "../common.hlsli"
+#define TONEMAP_PARAM_REGISTER b2
+#include "./PostProcess.hlsli"
 
 Texture2D<float> ReadonlyDepth : register(t0);
 
@@ -62,33 +63,33 @@ cbuffer CameraKerare : register(b1) {
   float film_aspect : packoffset(c000.w);
 };
 
-cbuffer TonemapParam : register(b2) {
-  float contrast : packoffset(c000.x);
-  float linearBegin : packoffset(c000.y);
-  float linearLength : packoffset(c000.z);
-  float toe : packoffset(c000.w);
-  float maxNit : packoffset(c001.x);
-  float linearStart : packoffset(c001.y);
-  float displayMaxNitSubContrastFactor : packoffset(c001.z);
-  float contrastFactor : packoffset(c001.w);
-  float mulLinearStartContrastFactor : packoffset(c002.x);
-  float invLinearBegin : packoffset(c002.y);
-  float madLinearStartContrastFactor : packoffset(c002.z);
-  float tonemapParam_isHDRMode : packoffset(c002.w);
-  float useDynamicRangeConversion : packoffset(c003.x);
-  float useHuePreserve : packoffset(c003.y);
-  float exposureScale : packoffset(c003.z);
-  float kneeStartNit : packoffset(c003.w);
-  float knee : packoffset(c004.x);
-  float curve_HDRip : packoffset(c004.y);
-  float curve_k2 : packoffset(c004.z);
-  float curve_k4 : packoffset(c004.w);
-  row_major float4x4 RGBToXYZViaCrosstalkMatrix : packoffset(c005.x);
-  row_major float4x4 XYZToRGBViaCrosstalkMatrix : packoffset(c009.x);
-  float tonemapGraphScale : packoffset(c013.x);
-  float offsetEVCurveStart : packoffset(c013.y);
-  float offsetEVCurveRange : packoffset(c013.z);
-};
+// cbuffer TonemapParam : register(b2) {
+//   float contrast : packoffset(c000.x);
+//   float linearBegin : packoffset(c000.y);
+//   float linearLength : packoffset(c000.z);
+//   float toe : packoffset(c000.w);
+//   float maxNit : packoffset(c001.x);
+//   float linearStart : packoffset(c001.y);
+//   float displayMaxNitSubContrastFactor : packoffset(c001.z);
+//   float contrastFactor : packoffset(c001.w);
+//   float mulLinearStartContrastFactor : packoffset(c002.x);
+//   float invLinearBegin : packoffset(c002.y);
+//   float madLinearStartContrastFactor : packoffset(c002.z);
+//   float tonemapParam_isHDRMode : packoffset(c002.w);
+//   float useDynamicRangeConversion : packoffset(c003.x);
+//   float useHuePreserve : packoffset(c003.y);
+//   float exposureScale : packoffset(c003.z);
+//   float kneeStartNit : packoffset(c003.w);
+//   float knee : packoffset(c004.x);
+//   float curve_HDRip : packoffset(c004.y);
+//   float curve_k2 : packoffset(c004.z);
+//   float curve_k4 : packoffset(c004.w);
+//   row_major float4x4 RGBToXYZViaCrosstalkMatrix : packoffset(c005.x);
+//   row_major float4x4 XYZToRGBViaCrosstalkMatrix : packoffset(c009.x);
+//   float tonemapGraphScale : packoffset(c013.x);
+//   float offsetEVCurveStart : packoffset(c013.y);
+//   float offsetEVCurveRange : packoffset(c013.z);
+// };
 
 cbuffer LDRPostProcessParam : register(b3) {
   float fHazeFilterStart : packoffset(c000.x);
@@ -1365,3 +1366,4 @@ float4 main(
   SV_Target.w = 0.0f;
   return SV_Target;
 }
+
