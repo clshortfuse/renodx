92d330456bd552e41f0a833dda04117e18d07ee9
Souperman9 <35243266+Souperman9@users.noreply.github.com>
2026-04-20 21:43:31 +1000

    feat(botw): update tonemapping, add more sliders

--- src/games/botw/shared.h
+++ src/games/botw/shared.h
@@ -52,6 +52,8 @@ struct ShaderInjectData {
 
   float custom_saturation_clip;
   float custom_bloom;
+  float custom_tonemap_clamp;
+  float vanilla_saturation;
 };
 
 #define RENODX_PEAK_WHITE_NITS               shader_injection.peak_white_nits
@@ -67,17 +69,18 @@ struct ShaderInjectData {
 #define RENODX_TONE_MAP_BLOWOUT              shader_injection.tone_map_blowout
 #define RENODX_TONE_MAP_FLARE                shader_injection.tone_map_flare
 #define RENODX_TONE_MAP_HUE_CORRECTION       shader_injection.tone_map_hue_correction
+#define VANILLA_SATURATION                   shader_injection.vanilla_saturation
 // #define RENODX_TONE_MAP_HUE_SHIFT            shader_injection.tone_map_hue_shift
 // #define RENODX_TONE_MAP_HUE_SHIFT_METHOD     HUE_SHIFT_METHOD_SDR_MODIFIED
 // #define RENODX_TONE_MAP_HUE_SHIFT_MODIFIER   0.5f
 #define RENODX_COLOR_GRADE_STRENGTH               shader_injection.scene_grade_strength
 // #define RENODX_TONE_MAP_WORKING_COLOR_SPACE       shader_injection.tone_map_working_color_space
 // #define RENODX_TONE_MAP_HUE_PROCESSOR             shader_injection.tone_map_hue_processor
-#define RENODX_RENO_DRT_TONE_MAP_METHOD           renodx::tonemap::renodrt::config::tone_map_method::REINHARD
+#define RENODX_RENO_DRT_TONE_MAP_METHOD           renodx::tonemap::renodrt::config::tone_map_method::HERMITE_SPLINE
 #define RENODX_GAMMA_CORRECTION                   shader_injection.gamma_correction
 #define CUSTOM_SATURATION_CLIP                    shader_injection.custom_saturation_clip
 #define CUSTOM_BLOOM                          shader_injection.custom_bloom
-
+#define CUSTOM_TONEMAP_CLAMP                  shader_injection.custom_tonemap_clamp
 #define RENODX_INTERMEDIATE_SCALING            (RENODX_DIFFUSE_WHITE_NITS / RENODX_GRAPHICS_WHITE_NITS)
 // #define RENODX_INTERMEDIATE_COLOR_SPACE        color::convert::COLOR_SPACE_BT709
 #define RENODX_SWAP_CHAIN_DECODING           0.f // 0 = linear, 1 = srgb, 2 = 2.2, 3 = 2.4, 4 = pq
@@ -132,6 +135,7 @@ cbuffer injected_buffer : register(b13) {
 #endif
 
 #include "../../shaders/renodx.hlsl"
+#include "../../shaders/tonemap/psycho_test17.hlsl"
 
 #endif
 
