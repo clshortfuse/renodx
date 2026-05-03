502e8daa6fd5bc463f1cdfbf06a704620627dd8f
akuru-q <115506513+akuru-q@users.noreply.github.com>
2026-05-03 12:20:55 +0200

    feat(rotsp): initial commit

---
+++ src/games/rotsp/swap_chain_proxy_pixel_shader.ps_5_1.hlsl
@@ -0,0 +1,8 @@
+#include "./shared.h"
+
+Texture2D t0 : register(t0);
+SamplerState s0 : register(s0);
+float4 main(float4 vpos: SV_POSITION, float2 uv: TEXCOORD0)
+    : SV_TARGET {
+  return renodx::draw::SwapChainPass(t0.Sample(s0, uv));
+}
