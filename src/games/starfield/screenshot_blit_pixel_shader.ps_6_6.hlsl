9519fb8b09a93ce08e8171fd5bbd40c18448d338
Carlos Lopez <clshortfuse@gmail.com>
2026-06-01 20:44:49 -0400

    fix(starfield): fix clip instead of gamut compress for photomode

--- src/games/starfield/screenshot_blit_pixel_shader.ps_6_6.hlsl
+++ src/games/starfield/screenshot_blit_pixel_shader.ps_6_6.hlsl
@@ -4,9 +4,9 @@ Texture2D<float4> hdr_source : register(t0);
 
 float4 main(float4 position: SV_Position) : SV_Target {
   float4 source = hdr_source.Load(int3(int2(position.xy), 0));
-  float3 color = max(0.f, renodx::draw::InvertIntermediatePass(source.rgb));
+  float3 color = renodx::draw::InvertIntermediatePass(source.rgb);
   color = renodx::color::gamut::GamutCompressBT709(color);
   color = renodx::tonemap::neutwo::MaxChannel(color);
   color = saturate(color);
-  return float4(renodx::color::srgb::Encode(saturate(color)), source.a);
+  return float4(renodx::color::srgb::Encode(color), source.a);
 }
