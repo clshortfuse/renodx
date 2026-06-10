8c124b3627a2df3f627acc2a89a759e238facea7
Mohanned Hassan <gailardiagalane@gmail.com>
2026-06-10 19:06:43 +0400

    feat(ff7rebirth): Add support for 1.005

---
+++ src/games/ff7rebirth/final_fg_displaymapped/final_displaymapped_fg_0xF0C9AF0F.ps_6_6.hlsl
@@ -0,0 +1,12 @@
+#define USE_DEFAULT
+#define USE_DISPLAY_MAP
+#define USE_FG
+#include "../hdrcomposite.hlsl"
+
+OutputSignature main(
+    noperspective float2 TEXCOORD: TEXCOORD,
+    noperspective float4 TEXCOORD_1: TEXCOORD1,
+    noperspective float4 SV_Position: SV_Position)
+    {
+  return HDRComposite(TEXCOORD, TEXCOORD_1, SV_Position);
+}
