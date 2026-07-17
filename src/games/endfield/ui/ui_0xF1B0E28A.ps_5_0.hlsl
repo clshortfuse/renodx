fb4756a4c1ed6ddc5ff88cd67ee5dc3f223989a3
ItsTheSewerRat <ItsTheSewerRat@users.noreply.github.com>
2026-07-18 01:36:02 +0200

    fix(endfield): update UI shaders

--- src/games/endfield/ui/ui_0xF1B0E28A.ps_5_0.hlsl
+++ src/games/endfield/ui/ui_0xF1B0E28A.ps_5_0.hlsl
@@ -2,45 +2,38 @@
 #include "../shared.h"
 
 Texture2D<float4> t0 : register(t0);
-
 SamplerState s0_s : register(s0);
 
-cbuffer cb0 : register(b0)
-{
-  float4 cb0[11];
+cbuffer cb1 : register(b1) {
+  float4 cb1[13];
 }
 
-
-
+cbuffer cb0 : register(b0) {
+  float4 cb0[6];
+}
 
 // 3Dmigoto declarations
-#define cmp -
-
-
 void main(
-  float4 v0 : SV_Position0,
-  float4 v1 : TEXCOORD0,
-  float4 v2 : TEXCOORD1,
-  float4 v3 : TEXCOORD2,
-  float4 v4 : TEXCOORD3,
-  float4 v5 : TEXCOORD4,
-  out float4 o0 : SV_Target0)
-{
-  float4 r0,r1;
-  uint4 bitmask, uiDest;
-  float4 fDest;
+    float4 v0 : SV_Position0,
+    float4 v1 : TEXCOORD0,
+    float2 v2 : TEXCOORD1,
+    float4 v3 : TEXCOORD2,
+    float4 v4 : TEXCOORD3,
+    float4 v5 : TEXCOORD4,
+    out float4 o0 : SV_Target0) {
+  float4 r0, r1;
 
-  r0.x = cmp(0.000000 != cb0[10].z);
+  r0.x = 0.000000 != cb1[12].x;
   r1.xyzw = t0.Sample(s0_s, v2.xy).xyzw;
   r1.w = r0.x ? 1 : r1.w;
-  r0.xyzw = cb0[3].xyzw + r1.xyzw;
+  r0.xyzw = cb0[5].xyzw + r1.xyzw;
   r1.x = 255 * v1.w;
   r1.x = round(r1.x);
   r1.w = 0.00392156886 * r1.x;
   r1.xyz = v1.xyz;
   r0.xyzw = r1.xyzw * r0.xyzw;
   o0.xyz = r0.xyz * r0.www;
-  o0.w = cb0[9].z * -r0.w + r0.w;
+  o0.w = cb1[11].x * -r0.w + r0.w;
+
   if (UI_VISIBILITY < 0.5f) o0 = 0;
-  return;
 }
