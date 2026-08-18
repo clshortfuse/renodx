9ac0ad2f0e4a7a28fcdd0c0ece912b5ac4772e27
Mohanned Hassan <gailardiagalane@gmail.com>
2026-08-18 14:26:28 +0400

    fix(zelda-eow): Fix initializer order

--- src/games/zelda-eow/addon.cpp
+++ src/games/zelda-eow/addon.cpp
@@ -292,8 +292,8 @@ void OnInitDevice(reshade::api::device* device) {
     renodx::mods::swapchain::resource_upgrade_infos.push_back({
         .old_format = reshade::api::format::r11g11b10_float,
         .new_format = reshade::api::format::r16g16b16a16_float,
-        .ignore_reset = true,
         .use_resource_view_cloning = true,
+        .ignore_reset = true,
         .dimensions = DIMENSIONS,
         .min_dimensions = MIN_DIMENSIONS,
     });
