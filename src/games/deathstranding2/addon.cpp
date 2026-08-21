30e1444600d99547d29efb1d9f7e50ca691e0652
Musa Haji <mqhaji@vt.edu>
2026-08-21 16:00:35 -0400

    feat(deathstranding2): set peak nits max to 10k

--- src/games/deathstranding2/addon.cpp
+++ src/games/deathstranding2/addon.cpp
@@ -43,8 +43,9 @@ renodx::utils::settings::Settings settings = {
         .section = "Tone Mapping",
         .tooltip = "Sets the value of peak white in nits",
         .min = 48.f,
-        .max = 4000.f,
+        .max = 10000.f,
         .is_enabled = []() { return shader_injection.tone_map_type != 0.f; },
+        .is_logarithmic = true,
     },
     new renodx::utils::settings::Setting{
         .key = "ToneMapGameNits",
